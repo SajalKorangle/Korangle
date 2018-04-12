fcf63691bfb756d210ae639097c29fc3dda1b187
harshal <harshalagrawal03@gmail.com>
2018-04-12 15:41:46 +0530

    1. halfway implementation of fee structure

---
+++ src/app/fees-second/fee.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+
+import 'rxjs/add/operator/toPromise';
+
+import { CommonServiceRequirements } from '../services/common-service-requirements';
+
+@Injectable()
+export class FeeService extends CommonServiceRequirements {
+
+
+
+}
