315af548a5dfc1a0047575e2731f54fa5ee5bf13
harshal <harshalagrawal03@gmail.com>
2018-11-04 16:04:47 +0530

    changed script file

--- copydatabasefromserver.sh
+++ copydatabasefromserver.sh
@@ -3,8 +3,8 @@ current_branch=$(git rev-parse --abbrev-ref HEAD)
 if [ ${current_branch} == 'master' ]
 then
     mv db.sqlite3 db.sqlite3.$dateVar
-    scp -i ~/Documents/django_qlib_virginia.pem ubuntu@ec2-54-174-109-85.compute-1.amazonaws.com:/home/ubuntu/djangobeginners/db.sqlite3 ./db.sqlite3
+    scp -i ~/Documents/korangle.pem ubuntu@ec2-18-209-33-171.compute-1.amazonaws.com:/home/ubuntu/django_backend/db.sqlite3 ./db.sqlite3
 else
     mv ${current_branch}_db.sqlite3 ${current_branch}_db.sqlite3.$dateVar
-    scp -i ~/Documents/django_qlib_virginia.pem ubuntu@ec2-54-174-109-85.compute-1.amazonaws.com:/home/ubuntu/djangobeginners/db.sqlite3 ./${current_branch}_db.sqlite3
+    scp -i ~/Documents/korangle.pem ubuntu@ec2-18-209-33-171.compute-1.amazonaws.com:/home/ubuntu/django_backend/db.sqlite3 ./${current_branch}_db.sqlite3
 fi
