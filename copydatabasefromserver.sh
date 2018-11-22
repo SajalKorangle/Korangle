87b6895dcb53cee9fbdb69bf83bc122291bd895b
harshal <harshalagrawal03@gmail.com>
2018-11-22 18:46:48 +0530

    halfway changes of class session refactor

--- copydatabasefromserver.sh
+++ copydatabasefromserver.sh
@@ -2,9 +2,11 @@ dateVar=$(date +"%Y-%m-%d_%H:%M:%S")
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 if [ ${current_branch} == 'master' ]
 then
-    mv db.sqlite3 db.sqlite3.$dateVar
+    mv db.sqlite3 prev_db
     scp -i ~/Documents/korangle.pem ubuntu@ec2-18-209-33-171.compute-1.amazonaws.com:/home/ubuntu/django_backend/db.sqlite3 ./db.sqlite3
+    cp db.sqlite3 backupdb
 else
-    mv ${current_branch}_db.sqlite3 ${current_branch}_db.sqlite3.$dateVar
+    mv ${current_branch}_db.sqlite3 prev_db
     scp -i ~/Documents/korangle.pem ubuntu@ec2-18-209-33-171.compute-1.amazonaws.com:/home/ubuntu/django_backend/db.sqlite3 ./${current_branch}_db.sqlite3
+    cp ${current_branch}_db.sqlite3 backupdb
 fi
