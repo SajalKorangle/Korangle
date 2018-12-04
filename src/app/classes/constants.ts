6f80a8d39e51d261539ce319d10e5d578ddbcb90
harshal <harshalagrawal03@gmail.com>
2018-12-04 19:53:53 +0530

    changed ip address of backend

--- src/app/classes/constants.ts
+++ src/app/classes/constants.ts
@@ -3,9 +3,7 @@ export class Constants {
 
     public static DJANGO_SERVER = 'http://localhost:8000';
 
-    // public static DJANGO_SERVER = 'http://54.174.109.85:8000';
-
-    // public static DJANGO_SERVER = 'http://18.209.33.171:8000';
+    // public static DJANGO_SERVER = 'http://54.210.167.88:8000';
 
     public static api_version = '/v1.4';
 
