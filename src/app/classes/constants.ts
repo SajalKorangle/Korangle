62bdee722766f1a62164d047144846d2b46ba025
harshal <harshalagrawal03@gmail.com>
2019-01-04 18:58:45 +0530

    marksheet formatting

--- src/app/classes/constants.ts
+++ src/app/classes/constants.ts
@@ -1,9 +1,9 @@
 
 export class Constants {
 
-    public static DJANGO_SERVER = 'http://localhost:8000';
+    // public static DJANGO_SERVER = 'http://localhost:8000';
 
-    // public static DJANGO_SERVER = 'http://54.210.167.88:8000';
+    public static DJANGO_SERVER = 'http://54.210.167.88:8000';
 
     public static api_version = '/v1.6';
 
