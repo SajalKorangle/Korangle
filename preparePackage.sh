44e041193764e2799276cc962fc3d09622029946
harshal <harshalagrawal03@gmail.com>
2019-01-29 11:07:01 +0530

    commented the calling of eklavya logo png

--- preparePackage.sh
+++ preparePackage.sh
@@ -1,5 +1,6 @@
-rm -rf brighstar.tar.gz brightstar/
+rm -rf brighstar.tar.gz brightstar.zip brightstar/
 ng build --prod --build-optimizer
 npm run post-build
 mv dist/ brightstar
 tar -zcvf brightstar.tar.gz brightstar/
+zip -r brightstar.zip brightstar/
