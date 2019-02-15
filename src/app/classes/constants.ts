3ba8d37eaa13844c0ee13a59d69ecb5310c01ec8
harshal <harshalagrawal03@gmail.com>
2019-02-15 10:27:20 +0530

    changed ip address

--- src/app/classes/constants.ts
+++ src/app/classes/constants.ts
@@ -3,7 +3,7 @@ export class Constants {
 
     public static DJANGO_SERVER = 'http://localhost:8000';
 
-    // public static DJANGO_SERVER = 'http://54.210.167.88:8000';
+    // public static DJANGO_SERVER = 'http://3.81.157.137:8000';
 
     public static api_version = '/v1.7';
 
