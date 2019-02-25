b16dc43f15190acd555639e3cc4cfa8b9e9f233a
harshal <harshalagrawal03@gmail.com>
2019-02-25 23:30:20 +0530

    changed api version from v1.8 to v1.9

--- src/app/classes/constants.ts
+++ src/app/classes/constants.ts
@@ -5,6 +5,6 @@ export class Constants {
 
     // public static DJANGO_SERVER = 'http://3.81.157.137:8000';
 
-    public static api_version = '/v1.8';
+    public static api_version = '/v1.9';
 
 }
