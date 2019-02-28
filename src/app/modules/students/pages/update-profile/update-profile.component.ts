208439c5121f6cd205b7b157254117d3d1f6b4f2
harshal <harshalagrawal03@gmail.com>
2019-02-28 13:10:05 +0530

    1. Student address formatting in i-card. 2. Showing thumb image in student update profile image.

--- src/app/modules/students/pages/update-profile/update-profile.component.ts
+++ src/app/modules/students/pages/update-profile/update-profile.component.ts
@@ -334,4 +334,16 @@ export class UpdateProfileComponent implements OnInit {
         });
     }
 
+    getThumbImage(row: any): string {
+        if (row.profileImage) {
+            let url = row.profileImage;
+            if (url.substr(url.length-4) === "main") {
+                return url + "_thumb";
+            }
+            return url.substr(0, url.length-4) + "_thumb" + url.substr(url.length-4);
+        } else {
+            return '';
+        }
+    }
+
 }
