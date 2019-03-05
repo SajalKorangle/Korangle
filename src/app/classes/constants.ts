efa8f5f2268c0593325d9340bbf330c7b7d34bff
harshal <harshalagrawal03@gmail.com>
2019-03-06 00:47:33 +0530

    added nginx ip address in production

--- src/app/classes/constants.ts
+++ src/app/classes/constants.ts
@@ -3,7 +3,7 @@ export class Constants {
 
     public static DJANGO_SERVER = 'http://localhost:8000';
 
-    // public static DJANGO_SERVER = 'http://52.55.52.136:8000';
+    // public static DJANGO_SERVER = 'http://52.55.52.136';
 
     public static api_version = '/v1.9';
 
