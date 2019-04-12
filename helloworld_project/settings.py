1f03eab384c90e8f3127565498b5cd35432c90cd
harshal <harshalagrawal03@gmail.com>
2019-04-12 19:15:47 +0530

    added sentry to settings

--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -14,6 +14,14 @@ import os
 
 import datetime
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://eaa952693a9545d787e92662a961ea25@sentry.io/1437770",
+    integrations=[DjangoIntegration()]
+)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
