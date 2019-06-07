528e9328cd6718b0d6cb9e2bdb4a887cb1f17129
harshal <harshalagrawal03@gmail.com>
2019-06-08 01:42:16 +0530

    change api version from 2.0 to 2.1

--- helloworld_project/urls.py
+++ helloworld_project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-api_version = 'v2.0/'
+api_version = 'v2.1/'
 
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
