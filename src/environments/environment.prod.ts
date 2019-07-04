5d4d084a44452767c2585303ac3df31572234036
harshal <harshalagrawal03@gmail.com>
2019-07-04 16:55:34 +0530

    changed aws ip address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     versionCheckURL: 'www.korangle.com/version.json',
-    DJANGO_SERVER: 'http://52.55.52.136',
+    DJANGO_SERVER: 'http://54.166.241.97',
     api_version: '/v2.0',
 };
