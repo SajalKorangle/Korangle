a8662f85a46cea0ace2fc1c8a7a652bde8fcd858
dilip gupta <dilip98914@gmail.com>
2019-07-13 12:00:19 +0530

    idk just trying

--- src/app/basic-components/sidebar/sidebar.component.ts
+++ src/app/basic-components/sidebar/sidebar.component.ts
@@ -7,6 +7,9 @@ import { EmitterService } from '../../services/emitter.service';
 import {User} from '../../classes/user';
 import {style, state, trigger, animate, transition} from "@angular/animations";
 import {Student} from '../../classes/student';
+import {TeamService} from "../../modules/team/team.service";
+import {SchoolService} from "../../services/school.service";
+import {logger} from "codelyzer/util/logger";
 
 declare const $: any;
 
@@ -28,12 +31,15 @@ declare const $: any;
             transition('false => true', animate('800ms ease-in'))
         ]),
     ],
+    providers: [ TeamService ],
+
 
 })
 export class SidebarComponent implements OnInit {
 
     @Input() user: User;
-
+    sessionChangePermission=false;
+    moduleList=[];
     green = 'green';
     warning = 'warning';
 
@@ -62,7 +68,8 @@ export class SidebarComponent implements OnInit {
         ],
     };
 
-    constructor(private router: Router) {
+    constructor(private router: Router,
+                private teamService:TeamService) {
     }
 
     ngOnInit() {
@@ -84,6 +91,8 @@ export class SidebarComponent implements OnInit {
         if (this.user.section) {
             this.router.navigateByUrl(this.user.section.route);
         }
+
+        this.handleChangeSession();
     }
 
     isMobileMenu() {
@@ -92,6 +101,48 @@ export class SidebarComponent implements OnInit {
         }
         return true;
     };
+
+    handleChangeSession(){
+        console.log("usre",this.user['activeSchool']);
+            const request_module_data = {
+                schoolDbId: this.user.activeSchool.dbId,
+            };
+            console.log('hrere');
+            Promise.all([
+                this.teamService.getSchoolModuleList(request_module_data, this.user.jwt),
+            ]).then(value => {
+                // console.log(value);
+                console.log('hrere22');
+
+                this.sessionChangePermission=true;
+                this.moduleList=value[0];
+                //no need for checkPermission??
+                this.checkPermission(this.moduleList)
+            }, error => {
+                console.log('error changing session');
+            });
+
+
+    }
+
+    checkPermission(moduleArray):void{
+        moduleArray.forEach(module=>{
+            if(module.path=="school"){
+                let taskList=module.taskList;
+                taskList.forEach(task=>{
+                    if(task.path=="change_session"){
+                        console.log('permission is granted!');
+                        this.sessionChangePermission=true;
+                        // this.permissionEmitter.emit(this.sessionChangePermission);
+                    }else{
+                        console.log('permission is not granted!');
+                    }
+
+                });
+
+            }
+        });
+    }
 
     changePage(task: any, module: any) {
         this.user.populateSection(task, module);
