0e9cf268779d010061f85d97ff6459f8ec6f17a8
harshal <harshalagrawal03@gmail.com>
2019-07-21 00:09:49 +0530

    removing unnecessary files

--- src/app/modules/expenses/expense-list/expense-list.component.ts
+++ src/app/modules/expenses/expense-list/expense-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-// import * as Chartist from 'chartist';
 
 import { ExpenseService} from '../expense.service';
 import { Expense } from '../../../classes/expense';
@@ -68,7 +67,6 @@ export class ExpenseListComponent {
 
       this.expenseService.getExpenseList(data, this.user.jwt).then(
           expenseList => {
-              // for (let i = 0; i < 10000; ++i ) { }
               this.totalExpenses = 0;
               this.isLoading = false;
               this.expenseList = expenseList;
@@ -105,29 +103,11 @@ export class ExpenseListComponent {
               if (this.dateLabels.length > 1 && this.displayEndDate !== this.displayStartDate) {
                   setTimeout( () => {
                       const dataExpenseChart: any = {
-                          /*labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-                          series: [
-                              [12, 17, 7, 17, 23, 18, 38]
-                          ]*/
                           labels: this.dateLabels,
                           series: [
                               this.totalExpensesDatesWise
                           ]
                       };
-
-                      /* const optionsExpenseChart: any = {
-                          lineSmooth: Chartist.Interpolation.cardinal({
-                              tension: 0
-                          }),
-                          low: 0,
-                          high: this.maxTotalExpenseInADate, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-                          chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
-                      };*/
-
-
-                      // const expenseChart = new Chartist.Line('#expenseChart', dataExpenseChart, optionsExpenseChart);
-
-                      // this.startAnimationForLineChart(expenseChart);
                   } );
               }
 
@@ -144,40 +124,6 @@ export class ExpenseListComponent {
             this.maxTotalExpenseDate = date;
         }
     }
-
-    /*startAnimationForLineChart(chart) {
-        let seq: any, delays: any, durations: any;
-        seq = 0;
-        delays = 80;
-        durations = 500;
-
-        chart.on('draw', function(data) {
-            if (data.type === 'line' || data.type === 'area') {
-                data.element.animate({
-                    d: {
-                        begin: 600,
-                        dur: 700,
-                        from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
-                        to: data.path.clone().stringify(),
-                        easing: Chartist.Svg.Easing.easeOutQuint
-                    }
-                });
-            } else if (data.type === 'point') {
-                seq++;
-                data.element.animate({
-                    opacity: {
-                        begin: seq * delays,
-                        dur: durations,
-                        from: 0,
-                        to: 1,
-                        easing: 'ease'
-                    }
-                });
-            }
-        });
-
-        seq = 0;
-    };*/
 
 
     printExpenses(): void {
