59944b142f1383ebbc84b38336a212d7e37f16eb
harshal <harshalagrawal03@gmail.com>
2019-12-01 00:55:20 +0530

    changed api version from 2.4 to 2.5

--- helloworld_project/urls.py
+++ helloworld_project/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-api_version = 'v2.4/'
+api_version = 'v2.5/'
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
