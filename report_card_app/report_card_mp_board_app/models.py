b93b53224cc7ce1ef950026270e4de866a13b0ff
Harry Chhabra <harrychhabra1998@gmail.com>
2019-12-20 20:07:32 +0530

    t3-c2; Created StudentRemark table in report card mp board app

--- report_card_app/report_card_mp_board_app/models.py
+++ report_card_app/report_card_mp_board_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from student_app.models import Student
+from school_app.model.models import Session
+
+
+
+class StudentRemark(models.Model):
+
+    remark = models.TextField(null=False, default='-', verbose_name='remark', blank=True)
+    parentStudent = models.ForeignKey(Student, models.CASCADE, null=False, default=0, verbose_name='parentStudent', related_name='%(class)s_parent_student')
+    parentSession = models.ForeignKey(Session, models.PROTECT, null=False, default=0, verbose_name='parentSession', related_name='%(class)s_parent_session')
+
+    class Meta:
+        unique_together = ('parentStudent', 'parentSession')
