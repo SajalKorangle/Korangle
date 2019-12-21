a1d5484d1af5094f9eae7d16a5334c23ed9f18ed
Harry Chhabra <harrychhabra1998@gmail.com>
2019-12-21 19:12:06 +0530

    Modified employee permission table in add student remark task

--- team_app/db_script/add_student_remark_row_mp_board.py
+++ team_app/db_script/add_student_remark_row_mp_board.py
@@ -3,6 +3,7 @@ def add_student_remark_row_mp_board(apps, schema_editor):
 
     Module = apps.get_model('team_app', 'Module')
     Board = apps.get_model('school_app', 'Board')
+    EmployeePermission = apps.get_model('employee_app', 'EmployeePermission')
     Task = apps.get_model('team_app', 'Task')
 
     mp_board_object = Board.objects.get(name='M.P. Board')
@@ -15,7 +16,7 @@ def add_student_remark_row_mp_board(apps, schema_editor):
     
     # To find existing count
     count = Task.objects.filter(parentModule = module_object).count()
-    count = count + 1
+    count = 11 # As observed from database
     
     task_title_list = [
       {'path':'add_student_remarks',
@@ -31,3 +32,16 @@ def add_student_remark_row_mp_board(apps, schema_editor):
                            orderNumber=count,
                            parentBoard=mp_board_object)
         task_object.save()
+        print('Task Added')
+        print(task_object.id)
+        # Adding in employee permission
+        emp_added_list = [] # To get unique employee id
+        emp_per_list = EmployeePermission.objects.filter(parentEmployee__parentSchool__parentBoard__name='M.P. Board')
+        for employee_permission_object in emp_per_list:
+            if employee_permission_object.parentEmployee.id in emp_added_list:
+                continue
+            emp_added_list.append(employee_permission_object.parentEmployee.id)
+            new_emp = EmployeePermission()
+            new_emp.parentEmployee = employee_permission_object.parentEmployee
+            new_emp.parentTask = task_object
+            new_emp.save()
