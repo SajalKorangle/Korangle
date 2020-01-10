d16d0e38b3d6dd0505bbe6dd88349c1b2f1529d2
grvsharma1810 <gs8280601@gmail.com>
2020-01-10 19:25:04 +0530

    did changes according to latest comments

--- school_app/migrations/0039_auto_20200104_2202.py
+++ school_app/migrations/0039_auto_20200104_2202.py
@@ -3,13 +3,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations
-
-def add_session(apps,schema_editor):
-    Session = apps.get_model('school_app','Session')
-    session_20_21 = Session(startDate='2020-04-01',endDate='2021-03-31',name='Session 2020-21',orderNumber=4)
-    session_21_22 = Session(startDate='2021-04-01',endDate='2022-03-31',name='Session 2021-22',orderNumber=5)
-    session_20_21.save()
-    session_21_22.save()
+from school_app.db_script.add_session import add_session
 
 class Migration(migrations.Migration):
 
