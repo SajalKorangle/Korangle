568c116633e5ef3c97d64783358c63faf1bbfb5d
grvsharma1810 <gs8280601@gmail.com>
2020-01-11 20:38:20 +0530

    some minor change

--- src/app/services/modules/school/models/session.ts
+++ src/app/services/modules/school/models/session.ts
@@ -6,9 +6,9 @@ export class Session{
 }
 
 export const SESSION_CONSTANT = [
-    { 'id':1,'startDate' : '2017-04-01', 'endDate' : '2018-03-31', 'name':'Session 2017-2018', 'orderNumber' : 1 },
-    { 'id':2,'startDate' : '2018-04-01', 'endDate' : '2019-03-31', 'name':'Session 2018-2019', 'orderNumber' : 2 },
-    { 'id':3,'startDate' : '2019-04-01', 'endDate' : '2020-03-31', 'name':'Session 2019-2020', 'orderNumber' : 3 },
-    { 'id':4,'startDate' : '2020-04-01', 'endDate' : '2021-03-31', 'name':'Session 2020-2021', 'orderNumber' : 4 },
-    { 'id':5,'startDate' : '2021-04-01', 'endDate' : '2022-03-31', 'name':'Session 2021-2022', 'orderNumber' : 5 }
+    { 'id':1,'startDate' : '2017-04-01', 'endDate' : '2018-03-31', 'name': 'Session 2017-2018', 'orderNumber' : 1 },
+    { 'id':2,'startDate' : '2018-04-01', 'endDate' : '2019-03-31', 'name': 'Session 2018-2019', 'orderNumber' : 2 },
+    { 'id':3,'startDate' : '2019-04-01', 'endDate' : '2020-03-31', 'name': 'Session 2019-2020', 'orderNumber' : 3 },
+    { 'id':4,'startDate' : '2020-04-01', 'endDate' : '2021-03-31', 'name': 'Session 2020-2021', 'orderNumber' : 4 },
+    { 'id':5,'startDate' : '2021-04-01', 'endDate' : '2022-03-31', 'name': 'Session 2021-2022', 'orderNumber' : 5 }
 ]
