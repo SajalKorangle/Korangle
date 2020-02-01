922e320bc7fa59ea240b574cf1598e1fdb5ad9dd
harshal <harshalagrawal03@gmail.com>
2020-02-01 11:24:33 +0530

    modularizing test

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,19 +1,33 @@
 import { browser, by, element } from 'protractor';
 
-export class AppPage {  
-  navigateToURL(url) {
-    return browser.get(browser.baseUrl + url) as Promise<any>;
-  }
+var scrollIntoView = function (element) {
+    arguments[0].scrollIntoView();
+};
+
+export class AppPage {
 
   navigateTo() {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText() {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  beforeEach() {
+
+      browser.driver.manage().window().maximize();
+      browser.waitForAngularEnabled(false);
+      this.navigateTo();
+
+      element(by.name('username')).sendKeys('6264439636');
+      element(by.name('password')).sendKeys('6264439636');
+      element(by.buttonText('Login')).click();
+      browser.sleep(5000);
+      console.log('Logged In');
+
   }
 
-  scrollView(position){
-    
+  afterEach() {
+      browser.executeScript(scrollIntoView,element(by.id('Logout')));
+      element(by.id('Logout')).click();
+      browser.sleep(2000);
   }
+
 }
