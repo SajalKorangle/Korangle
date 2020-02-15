3a6ecdde1fbf8d499537ddefdd5a9ba5e563cc91
grvsharma1810 <gs8280601@gmail.com>
2020-02-15 18:16:40 +0530

    written migration

---
+++ student_app/db_script/delete_unknown_rte.py
@@ -0,0 +1,7 @@
+
+def delete_unknown_rte(apps,schema_editor):
+    Student = apps.get_model('student_app', 'Student')
+    for student in Student.objects.all():
+        if student.rte is None or student.rte == 'UNKNOWN':
+            student.rte = 'NO'
+            student.save()
