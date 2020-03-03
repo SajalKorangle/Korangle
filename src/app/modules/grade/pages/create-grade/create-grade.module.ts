e8450fb8b8b7d5b404ed71bdf7090050f296afc2
Harry Chhabra <harrychhabra1998@gmail.com>
2020-03-04 00:08:25 +0530

    Maximum call stack exceded error resolved

--- src/app/modules/grade/pages/create-grade/create-grade.module.ts
+++ src/app/modules/grade/pages/create-grade/create-grade.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { CommonModule } from '@angular/common';
 import { CreateGradeComponent } from './create-grade.component';
+import { CreateGradeRouting } from './create-grade.routing';
 
 
 
 @NgModule({
   declarations: [CreateGradeComponent],
   imports: [
-    CommonModule
-  ]
+      CreateGradeRouting,
+  ],
+  exports: [
+  ],
+  providers: [],
+  bootstrap: [CreateGradeComponent]
 })
 export class CreateGradeModule { }
