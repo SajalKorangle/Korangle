c00711951989ee43277091fd45a828fc1314f617
grvsharma1810 <gs8280601@gmail.com>
2020-03-04 17:05:15 +0530

    Migration moved

--- team_app/db_script/add_create_grade_task.py
+++ team_app/db_script/add_create_grade_task.py
@@ -1,4 +1,3 @@
-from team_app.db_script.add_create_grade_permission import add_create_grade_permission
 
 def add_create_grade_task_and_permission(apps,schema_editor):
     Task = apps.get_model('team_app', 'Task')
@@ -8,4 +7,12 @@ def add_create_grade_task_and_permission(apps,schema_editor):
             grade_module = module
     create_grade = Task(path='create_grade',title='Create Grade',orderNumber=1,parentModule=grade_module)
     create_grade.save()
-    add_create_grade_permission(apps,schema_editor)
+    
+    EmployeePermission = apps.get_model('employee_app','EmployeePermission')
+    Task = apps.get_model('team_app','Task')
+
+    parentTask = Task.objects.get(path='create_grade')
+
+    for employee_permission in EmployeePermission.objects.filter(parentTask__path='assign_task'):
+            permissionObject = EmployeePermission(parentTask=parentTask, parentEmployee=employee_permission.parentEmployee)
+            permissionObject.save()
