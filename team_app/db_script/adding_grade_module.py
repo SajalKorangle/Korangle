de4ffaeaf1eb3d8634e360652a85279c8c532be1
grvsharma1810 <gs8280601@gmail.com>
2020-03-06 19:24:51 +0530

    order number changed

--- team_app/db_script/adding_grade_module.py
+++ team_app/db_script/adding_grade_module.py
@@ -1,5 +1,5 @@
 
 def add_grade_module(apps,schema_editor):
     Module = apps.get_model('team_app', 'Module')
-    grade_module = Module(path='grade',title='Grade',icon='book',orderNumber=15,parentBoard=None)
+    grade_module = Module(path='grade',title='Grade',icon='book',orderNumber=8,parentBoard=None)
     grade_module.save()
