4eb74d9c12ff2ca3daf2b9b2565eb947033e269b
harshal <harshalagrawal03@gmail.com>
2020-03-21 03:17:34 +0530

    made changes in data migration

--- student_app/db_script/delete_unknown_rte.py
+++ student_app/db_script/delete_unknown_rte.py
@@ -1,7 +1,6 @@
 
 def delete_unknown_rte(apps,schema_editor):
     Student = apps.get_model('student_app', 'Student')
-    for student in Student.objects.all():
-        if student.rte == 'UNKNOWN':
-            student.rte = 'NO'
-            student.save()
+    for student in Student.objects.filter(rte='UNKNOWN'):
+        student.rte = 'NO'
+        student.save()
