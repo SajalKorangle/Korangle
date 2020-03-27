301890e6e8ec895e3066de0fb747974ee4e5a5ff
harshal <harshalagrawal03@gmail.com>
2020-03-27 16:09:02 +0530

    reset migration commit 2

--- reset_migration.sh
+++ reset_migration.sh
@@ -3,6 +3,7 @@ git checkout -b reset_migration
 cp db.sqlite3 reset_migration_db.sqlite3
 ./manage.py makemigrations
 ./manage.py migrate --fake school_app zero
+./manage.py migrate --fake user_app zero
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc"  -delete
 find . -path "*/db_script/*.py"  -delete
