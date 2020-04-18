48fd087190a4ecb3cddfff6acd3ec6c2496d9e2f
harshal <harshalagrawal03@gmail.com>
2020-04-18 14:07:34 +0530

    added create school test

---
+++ frontend/e2e/src/modules/settings/create-school.e2e-spec.ts
@@ -0,0 +1,34 @@
+import { browser, logging, element, by } from 'protractor';
+import {AppPage} from "../../app.po";
+
+var scrollIntoView = function (element) {
+    arguments[0].scrollIntoView();
+};
+
+
+describe('workspace-project App', () => {
+
+    let page = new AppPage();
+
+    beforeEach(() => {
+        page.beforeEach();
+    });
+
+    it('check',()=>{
+
+        console.log('Opening Settings');
+        browser.executeScript(scrollIntoView,element(by.id('Settings')));
+        element(by.id('Settings')).click();
+        browser.sleep(2000);
+
+        browser.executeScript(scrollIntoView,element(by.id('Settings-Create School')));
+        element(by.id('Settings-Create School')).click();
+        browser.sleep(2000);
+
+    });
+
+    afterEach(() => {
+        page.afterEach();
+    });
+
+});
