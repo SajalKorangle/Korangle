d65ade206640ada6421976f8a98bd794bc8a742f
Rahul Singh <rs280599@gmail.com>
2020-04-26 21:20:13 +0530

    Tested and working

--- frontend/e2e/src/app.e2e-spec.ts
+++ frontend/e2e/src/app.e2e-spec.ts
@@ -5,7 +5,21 @@ describe('workspace-project App', () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('http://localhost:4200');
-    await page.screenshot({ path: 'example.png' });
+    await page.type('#username', '1234567890'); // Types instantly
+    await page.type('#pass', '1234567890', {delay: 100}); // Types slower, like a user
+    await page.screenshot({ path: 'before-login.png' });
+    await page.click('button[type="submit"]');
+    await page.waitForNavigation();
+    // await page.waitFor(10000)
+    await page.click('button.navbar-toggle');
+    await page.waitFor('a#Logout')
+    // await page.waitFor(10000)
+    await page.screenshot({ path: 'after-login.png' });
+    await page.click('a#Logout')
+    // await page.waitForNavigation();
+    // await page.waitFor(10000)
+    await page.screenshot({ path: 'click-logout.png' });
+    
 
     await browser.close();
   });
