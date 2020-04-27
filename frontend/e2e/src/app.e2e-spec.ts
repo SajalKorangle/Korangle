e449529fcab546ac21ec2e4f85576150c00b5073
Rahul Singh <rs280599@gmail.com>
2020-04-27 21:31:37 +0530

    updated window size

--- frontend/e2e/src/app.e2e-spec.ts
+++ frontend/e2e/src/app.e2e-spec.ts
@@ -4,6 +4,10 @@ describe('workspace-project App', () => {
   it('Test Puppeteer screenshot', async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    await page.setViewport({
+        width: 1920,
+        height: 1080
+    })
     await page.goto('http://localhost:4200');
     await page.type('#username', '1234567890'); // Types instantly
     await page.type('#pass', '1234567890', {delay: 100}); // Types slower, like a user
@@ -11,7 +15,6 @@ describe('workspace-project App', () => {
     await page.click('button[type="submit"]');
     await page.waitForNavigation();
     // await page.waitFor(10000)
-    await page.click('button.navbar-toggle');
     await page.waitFor('a#Logout')
     // await page.waitFor(10000)
     await page.screenshot({ path: 'after-login.png' });
