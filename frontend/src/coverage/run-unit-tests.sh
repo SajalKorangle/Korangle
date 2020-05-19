0c8698a2b6732845677f975555c20f2ec5b790cb
harshal <harshalagrawal03@gmail.com>
2020-05-19 13:06:32 +0530

    testing

--- frontend/src/coverage/run-unit-tests.sh
+++ frontend/src/coverage/run-unit-tests.sh
@@ -1,8 +1,8 @@
 
 # Create Dummy Spec file to include all modules and their files
 find src/app/ -name '*.module.ts' > src/dummy.spec.ts
-sed -i "" "s/^src\/app\//import '.\/app/g" src/dummy.spec.ts
-sed -i "" "s/\.ts$/\\';/g" src/dummy.spec.ts
+sed -i "s/^src\/app\//import '.\/app/g" src/dummy.spec.ts
+sed -i "s/\.ts$/\\';/g" src/dummy.spec.ts
 
 # Running unit tests
 npm run unit-tests
