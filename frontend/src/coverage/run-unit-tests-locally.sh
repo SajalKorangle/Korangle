4680a1f4b93d9376459a81152c84bc6e7eac59ea
harshal <harshalagrawal03@gmail.com>
2020-05-23 10:11:44 +0530

    rectified the error of copying environment.ts file to environment.prod.ts file while running the unit tests locally

--- frontend/src/coverage/run-unit-tests-locally.sh
+++ frontend/src/coverage/run-unit-tests-locally.sh
@@ -1,7 +1,4 @@
 
-# Create a fake environment.prod.ts
-cp src/environments/environment.ts src/environments/environment.prod.ts
-
 # Create Dummy Spec file to include all modules and their files
 find src/app/ -name '*.module.ts' > src/dummy.spec.ts
 sed -i "" "s/^src\/app/import '.\/app/g" src/dummy.spec.ts
