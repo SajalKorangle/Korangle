5824b79408e75a8bde0c64c4d35105cb7c28a9cd
harshal <harshalagrawal03@gmail.com>
2020-10-10 22:10:44 +0530

    bug fix

--- frontend/scripts/protractor/run-tests-e2e-github.sh
+++ frontend/scripts/protractor/run-tests-e2e-github.sh
@@ -3,8 +3,18 @@ start=$SECONDS
 
 mkdir -p tmp
 
+kill -9 `ps aux | grep testserver | awk '{ print $2 }'`
+
 # Running tests (using protractor)
-npm run e2e
+npm start &
+while ! echo exit | nc localhost 4200; do sleep 10; done
+
+protractor tests/protractor/protractor.conf.js
+if [ "$?" -ne 0 ];
+then
+    echo 'Protractor Test failed'
+    exit 1
+fi
 
 # Total Run time
 echo "Total Run Time: $((SECONDS - start))"
