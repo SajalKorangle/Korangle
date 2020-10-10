3f1f3746b594d6ca4e32d2adec5a51e2971560cf
harshal <harshalagrawal03@gmail.com>
2020-10-10 22:47:06 +0530

    testing

--- frontend/scripts/protractor/run-tests-e2e-github.sh
+++ frontend/scripts/protractor/run-tests-e2e-github.sh
@@ -1,20 +1,16 @@
 
-start=$SECONDS
-
 mkdir -p tmp
 
-kill -9 `ps aux | grep testserver | awk '{ print $2 }'`
+# kill -9 `sudo lsof -t -i:4200`
 
 # Running tests (using protractor)
 npm start &
 while ! echo exit | nc localhost 4200; do sleep 10; done
 
+node ./node_modules/protractor/node_modules/webdriver-manager/bin/webdriver-manager update
 ./node_modules/.bin/protractor tests/protractor/protractor.conf.js
 if [ "$?" -ne 0 ];
 then
     echo 'Protractor Test failed'
     exit 1
 fi
-
-# Total Run time
-echo "Total Run Time: $((SECONDS - start))"
