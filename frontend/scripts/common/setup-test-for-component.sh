4050b4d2bf37db7baa8b28fda59b95bfd4e40496
harshal <harshalagrawal03@gmail.com>
2020-10-14 18:58:26 +0530

    setup a script for creating directories to save user some time for creating directories for each component

--- frontend/scripts/common/setup-test-for-component.sh
+++ frontend/scripts/common/setup-test-for-component.sh
@@ -1,8 +1,25 @@
 
+createDirectory()
+{
+    if [ ! -f "$1" ]; then
+        directoryName=$(dirname "$1")
+        mkdir -p $directoryName
+        echo $directoryName
+    fi
+}
+
 for i in `find src/app -name '*.component.ts'`;
 do
-    featureFile="tests/features${i:7}"
-    featureFile="tests/features${i:7}"
-    echo $featureFile
-    # echo ${i:7}
+    trimmed=${i:7}
+    featureFile="tests/features${trimmed/component.ts/json}"
+    fixtureFile="tests/fixtures${trimmed/component.ts/json}"
+    protractorTestFile="tests/protractor/src${trimmed/component.ts/e2e-spec.ts}"
+    karmaTestFile="tests/karma/test${trimmed/component.ts/spec.ts}"
+
+    createDirectory $featureFile
+    createDirectory $fixtureFile
+    createDirectory $protractorTestFile
+    createDirectory $karmaTestFile
+
 done
+
