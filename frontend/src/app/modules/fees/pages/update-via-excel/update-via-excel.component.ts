ccad050c749074fc34739d80a3ea6e9e311f07d7
aditya <kumar.93@iitj.ac.in>
2020-11-22 01:05:01 +0530

    sheet read write done using xlsx package, upload sheet and dowanload template done

--- frontend/src/app/modules/fees/pages/update-via-excel/update-via-excel.component.ts
+++ frontend/src/app/modules/fees/pages/update-via-excel/update-via-excel.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
 
-import { UpdateViaExcelServiceAdapter } from './update-via-excel.adapter';
+import { UpdateViaExcelServiceAdapter } from './update-via-excel.service.adapter';
 
 import {DataStorage} from '../../../../classes/data-storage';
 import { StudentService } from './../../../../services/modules/student/student.service';
@@ -90,8 +91,47 @@ export class UpdateViaExcelComponent implements OnInit {
     return Object.keys(this.structuredStudent[Class.id]).length > 1;
   }
 
-  downloadSheet(): any {
+  downloadSheetTemplate(): void {
+    let data = [['ID', 'Name', 'Father’s Name', 'Class', 'Section']];
 
+    this.classList.forEach(Class => {
+      this.divisionList.forEach(Division => {
+        if (this.structuredSelection[Class.id][Division.id]) {
+          this.structuredStudent[Class.id][Division.id].forEach(({student}) => {
+            let row = [student.id, student.name, student.fathersName, Class.name, this.showSection(Class)?Division.name:null]
+            data.push(row);
+          });
+        }
+      })
+    });
+    console.log(data);
+
+    let ws = XLSX.utils.aoa_to_sheet(data);
+    let wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    XLSX.writeFile(wb, 'Sheet.xlsx');
+  }
+
+  uploadSheet(event: any): void{
+    if (event.target.files.length>0) {
+      let file = event.target.files[0];
+      const reader: FileReader = new FileReader();
+
+      reader.onload = (e: any) => {
+        /* read workbook */
+        const bstr: string = e.target.result;
+        const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
+  
+        /* grab first sheet */
+        const wsname: string = wb.SheetNames[0];
+        const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+  
+        /* save data */
+        let data = XLSX.utils.sheet_to_json(ws, { header: 1 });
+        console.log('read data: ', data);
+      };
+      reader.readAsBinaryString(file);
+    }
   }
 
   logMessage(log: any): void{
