387f91f95f0128b10050d460c15892ecd40fc02b
aditya <kumar.93@iitj.ac.in>
2020-11-23 23:37:23 +0530

    sanity check of uploaded data in place. Warnings and errors identified

--- frontend/src/app/modules/fees/pages/update-via-excel/update-via-excel.component.ts
+++ frontend/src/app/modules/fees/pages/update-via-excel/update-via-excel.component.ts
@@ -6,12 +6,14 @@ import { UpdateViaExcelServiceAdapter } from './update-via-excel.service.adapter
 import {DataStorage} from '../../../../classes/data-storage';
 import { StudentService } from './../../../../services/modules/student/student.service';
 import { ClassService } from './../../../../services/modules/class/class.service';
+import { FeeService} from './../../../../services/modules/fees/fee.service'
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-update-via-excel',
   templateUrl: './update-via-excel.component.html',
   styleUrls: ['./update-via-excel.component.css'],
-  providers: [StudentService, ClassService]
+  providers: [StudentService, ClassService, FeeService]
 })
 export class UpdateViaExcelComponent implements OnInit {
 
@@ -19,23 +21,36 @@ export class UpdateViaExcelComponent implements OnInit {
 
   classList = [];
   divisionList = [];
+  feeTypeList = [];
+  studentList = [];
 
   structuredStudent = {};  // structure = {classId: {classSecions: [Students], ...}, ...}
   structuredSelection = {};
   studentsCount = 0;
   selectionCount = 0;
 
+  uploadedData = [];  //  Array of array
+  errorRows = []; //  format: [rowNumber,Error Mesage]
+  errorCells = [];  //  format: [row, colums, Error Message]
+  warningRows = []; //  format: [rowNumber,Warning Mesage]
+  warningCells = []; //  format: [row, colums, Warning Message]
+
+  dataIsUploadable = false;
   isLoading = false;
 
   serviceAdapter: UpdateViaExcelServiceAdapter;
 
-  constructor(public studentService: StudentService, public classService: ClassService) { }
+  constructor(public studentService: StudentService, public classService: ClassService, public feeService: FeeService) { }
 
   ngOnInit() {
     this.user = DataStorage.getInstance().getUser();
     this.serviceAdapter = new UpdateViaExcelServiceAdapter();
     this.serviceAdapter.initializeAdapter(this);
     this.serviceAdapter.initializeData();
+  }
+
+  populateFeeType(feeTypeList): void{
+    this.feeTypeList = feeTypeList.sort((a, b) => a.orderNumber - b.orderNumber);
   }
 
   structuringForStudents(classIds: any, divisionIds: any): void {
@@ -47,8 +62,6 @@ export class UpdateViaExcelComponent implements OnInit {
         this.structuredSelection[classId][divisionId] = false;
       });
     });
-    console.log('students structured: ', this.structuredStudent);
-    console.log('selection students: ', this.structuredSelection);
   }
 
   populateStudents(studentSectionList: any): void {
@@ -92,19 +105,22 @@ export class UpdateViaExcelComponent implements OnInit {
   }
 
   downloadSheetTemplate(): void {
-    let data = [['ID', 'Name', 'Father’s Name', 'Class', 'Section']];
+    let data = [];
+    
+    let headersRow = ['Software ID', 'Scholar No.', 'Name', 'Father’s Name', 'Class'];
+    this.feeTypeList.forEach(feeType => headersRow.push(feeType.name));
+    data.push(headersRow);
 
     this.classList.forEach(Class => {
       this.divisionList.forEach(Division => {
         if (this.structuredSelection[Class.id][Division.id]) {
           this.structuredStudent[Class.id][Division.id].forEach(({student}) => {
-            let row = [student.id, student.name, student.fathersName, Class.name, this.showSection(Class)?Division.name:null]
+            let row = [student.id, student.scholarNumber, student.name, student.fathersName, `${Class.name} ${this.showSection(Class) ? ','+Division.name : ''}`]
             data.push(row);
           });
         }
       })
     });
-    console.log(data);
 
     let ws = XLSX.utils.aoa_to_sheet(data);
     let wb = XLSX.utils.book_new();
@@ -126,12 +142,114 @@ export class UpdateViaExcelComponent implements OnInit {
         const wsname: string = wb.SheetNames[0];
         const ws: XLSX.WorkSheet = wb.Sheets[wsname];
   
-        /* save data */
-        let data = XLSX.utils.sheet_to_json(ws, { header: 1 });
-        console.log('read data: ', data);
+      /* save data */
+        this.clearExcelData();
+        this.uploadedData = XLSX.utils.sheet_to_json(ws, { header: 1 });
+        this.uploadedData.pop();
+        console.log('uploaded Data: ', this.uploadedData);
+        //Sanity Checks
+        this.headersSanityCheck();
+        this.studentDataCorrespondenceSanityCheck();
+        this.feeAmountSanityCheck();
       };
       reader.readAsBinaryString(file);
     }
+  }
+
+  headersSanityCheck(): void {
+    const headers = this.uploadedData[0];
+    let actualHeader = ['Software ID', 'Scholar No.', 'Name', 'Father’s Name', 'Class'];
+    this.feeTypeList.forEach(feeType => actualHeader.push(feeType.name));
+    const len = actualHeader.length;
+    for (let i = 0; i < len; i += 1){
+      if (actualHeader[i] !== headers[i])
+        this.errorCells.push([0, i, `Header Mismatch: Expected ${actualHeader[i]}`]);
+    }
+  }
+
+  studentDataCorrespondenceSanityCheck(): void {
+    let i, id, providedStudent, student, len = this.uploadedData.length, Class, Division;
+
+    for (i = 1; i < len; i += 1){ // for every row in uploaded data
+      [student, Class, Division] = [undefined, undefined, undefined];
+      providedStudent = this.uploadedData[i];
+      id = parseInt(providedStudent[0]);
+
+      const [C, D] = providedStudent[4].split(','); //extracting class and divison as C and D
+      Class = this.classList.find(c => c.name.trim() === C.trim()); 
+
+      // Extracting student from class and section
+      if (Class) {
+        if (D) {
+          Division = this.divisionList.find(d => d.name.trim() === D.trim());
+          if (Division)
+            student = this.structuredStudent[Class.id][Division.id].find(ss => ss.student.id === id).student;
+        }
+        else {
+          student = Reflect.ownKeys(this.structuredStudent[Class.id]).map(k=>this.structuredStudent[Class.id][k])[0].find(ss => ss.student.id === id).student;  // till map function is the implementation of Object.values() since it's not supported
+        }
+      }
+  
+      if(student === undefined){
+        this.errorCells.push([i, 4, 'Class/Section Mismatch']);
+        console.log('Class Section Mismatch');
+        student = this.studentList.find(s => s.id === id);
+      }
+
+      if (student === undefined)  // not found case
+        this.errorRows.push([i, 'Student Not Found'])
+      else {
+
+        if (student.scholarNumber != providedStudent[1] && student.scholarNumber !== "" && providedStudent[1] !== undefined) {
+          this.errorCells.push([i, 1, 'Scholar Number Mismatch']);
+          console.log(`Scholar No. data mismatch at row ${i+1}`);
+        }
+
+        if (student.name.trim() !== providedStudent[2].trim()) {
+          this.errorCells.push([i, 2, 'Name Mismatch']);
+          console.log(`Name data mismatch at row ${i+1}`);
+        }
+
+        if (student.fathersName.trim() !== providedStudent[3].trim()) {
+          this.errorCells.push([i, 3, 'Father’s Name Mismatch']);
+          console.log(`Father’s Name data mismatch at row ${i+1}`)
+        }
+      }
+    }
+  }
+
+  feeAmountSanityCheck(): void {
+    let i, j, len = this.uploadedData.length, currData, parsedAmount;
+    for (i = 1; i < len; i++){  //  for Each row
+      currData = this.uploadedData[i];
+      currData.slice(5).forEach((amount, index) => {
+        parsedAmount = parseFloat(amount)
+        if (!isNaN(parsedAmount)) {
+          if (parsedAmount < 0) {
+            this.errorCells.push([i,index+5, 'Negavtive Amount'])
+          }
+          else if (parsedAmount != parseInt(amount)) {
+            currData[index + 5] = Math.round(parsedAmount);
+            this.warningCells.push([i, index + 5, 'Amount Rounded to nearest integer']);
+          }
+        } else {  //  empty cell case
+          // currData[index + 5] = 0;
+          // this.warningCells.push([i, index + 5, 'Empty Amount set to 0']);
+        }
+      });
+    }
+  }
+
+  feeIsNotUploadedSanityCheck(): void {
+    
+  }
+
+  clearExcelData(): void {
+    this.uploadedData = [];
+    this.errorRows = [];
+    this.errorCells = [];
+    this.warningRows = [];
+    this.warningCells = [];
   }
 
   logMessage(log: any): void{
--- frontend/src/app/modules/fees/pages/update-via-excel/update-via-excel.service.adapter.ts
+++ frontend/src/app/modules/fees/pages/update-via-excel/update-via-excel.service.adapter.ts
@@ -18,28 +18,33 @@ export class UpdateViaExcelServiceAdapter{
             'parentSession': this.vm.user.activeSchool.currentSessionDbId,
         };
 
+        const request_fee_type_data = {
+            'parentSchool': this.vm.user.activeSchool.dbId,
+        };
+
         Promise.all([
             this.vm.studentService.getObjectList(this.vm.studentService.student_section, student_section_data),
             this.vm.classService.getObjectList(this.vm.classService.classs,{}),
-            this.vm.classService.getObjectList(this.vm.classService.division,{}),
+            this.vm.classService.getObjectList(this.vm.classService.division, {}),
+            this.vm.feeService.getList(this.vm.feeService.fee_type, request_fee_type_data),
         ]).then(value => {
             console.log('update via excel adapter data: ', value);
             this.vm.classList = value[1];
             this.vm.divisionList = value[2];
+            this.vm.populateFeeType(value[3]);
             
             //structuring for student's class and division
             this.vm.structuringForStudents(value[1].map(el => el.id), value[2].map(el => el.id));
 
             let student_data = {
                 'id__in': value[0].map(ss => ss.parentStudent),
-                'fields__korangle': 'id,name,fathersName',
+                'fields__korangle': 'id,name,fathersName,scholarNumber',
             };
             
             Promise.all([
                 this.vm.studentService.getObjectList(this.vm.studentService.student, student_data)
-            ]).then(value2 => { 
-                console.log('student data: ', value2[0]);
-
+            ]).then(value2 => {
+                this.vm.studentList = value2[0];
                 value[0].forEach(ss => {
                     ss['student'] = value2[0].find(student=> student.id === ss.parentStudent)
                 });
@@ -55,4 +60,5 @@ export class UpdateViaExcelServiceAdapter{
         });
 
     }
+
 }
