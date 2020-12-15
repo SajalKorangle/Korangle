0c00fffe22c4a6ba6c1febe84a6c343825767870
harshal <harshalagrawal03@gmail.com>
2020-12-15 16:01:10 +0530

    reformatted code

--- frontend/tests/protractor/src/modules/parent/pages/view-profile/view-profile.e2e-spec.ts
+++ frontend/tests/protractor/src/modules/parent/pages/view-profile/view-profile.e2e-spec.ts
@@ -11,16 +11,21 @@ describe('Parents -> View Profile', () => {
     let node,prop;
 
     beforeAll(async () => {
+
         startBackendServer(getFixtureFiles('modules/parent/pages/view-profile/view-profile.json'));
         page = await BeforeAfterEach.beforeEach();
         await page.select('select[ng-reflect-model="Employee"]','Parent'); 
         await openModuleAndPage('Profile','');
+
     });
 
     it('View Profile : View Class And Section', async () => {
-    node =await getNode('b','Class');
-    prop = await page.evaluate(el => el.innerHTML, node);
-    expect(prop).toBe("Class - 12 , Section - A");
-    await BeforeAfterEach.afterEach();
+
+        node =await getNode('b','Class');
+        prop = await page.evaluate(el => el.innerHTML, node);
+        expect(prop).toBe("Class - 12 , Section - A");
+
+        await BeforeAfterEach.afterEach();
     });
-    });
+
+});
