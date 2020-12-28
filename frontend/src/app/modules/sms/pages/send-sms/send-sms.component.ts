517ce59d81718e31248d0f4477e37a0f7c393980
avinash0705 <avinash7may@gmail.com>
2020-12-28 10:29:26 +0530

    payment template code for front-end

--- frontend/src/app/modules/sms/pages/send-sms/send-sms.component.ts
+++ frontend/src/app/modules/sms/pages/send-sms/send-sms.component.ts
@@ -12,11 +12,13 @@ import {SendSmsServiceAdapter} from "./send-sms.service.adapter";
 import {NotificationService} from "../../../../services/modules/notification/notification.service";
 import {UserService} from "../../../../services/modules/user/user.service";
 
+import { WindowRefService } from "../../../../services/modules/sms/window-ref.service"
+
 @Component({
     selector: 'send-sms',
     templateUrl: './send-sms.component.html',
     styleUrls: ['./send-sms.component.css'],
-    providers: [ StudentService, ClassService, EmployeeService, NotificationService, UserService, SmsService],
+    providers: [ StudentService, ClassService, EmployeeService, NotificationService, UserService, SmsService, WindowRefService],
 })
 
 export class SendSmsComponent implements OnInit {
@@ -108,7 +110,8 @@ export class SendSmsComponent implements OnInit {
                 public smsService: SmsService,
                 public notificationService: NotificationService,
                 public userService: UserService,
-                private cdRef: ChangeDetectorRef) { }
+                private cdRef: ChangeDetectorRef,
+                private winRef: WindowRefService) { }
 
     onPage(event) {
         clearTimeout(this.timeout);
@@ -406,9 +409,45 @@ export class SendSmsComponent implements OnInit {
     }
 
 
-    initPayment() : void {
-        console.log('Payment initiated for ' + this.selectedSmsPlan.price + ' amount');
+    createRzpayOrder(data) {
+        console.log(data);
+        // call api to create order_id
+        let order_id; // received from backend
+        this.payWithRazor(order_id);
+    }
 
+    payWithRazor(val) {
+        const options: any = {
+          key: 'rzp_test_key',
+          amount: 125500, // amount should be in paise format to display Rs 1255 without decimal point
+          currency: 'INR',
+          name: '', // company name or product name
+          description: '',  // product description
+          image: './assets/logo.png', // company logo or product image
+          order_id: val, // order_id created by you in backend
+          modal: {
+            // We should prevent closing of the form when esc key is pressed.
+            escape: false,
+          },
+          notes: {
+            // include notes if any
+          },
+          theme: {
+            color: '#0c238a'
+          }
+        };
+        options.handler = ((response, error) => {
+          options.response = response;
+          console.log(response);
+          console.log(options);
+          // call your backend api to verify payment signature & capture transaction
+        });
+        options.modal.ondismiss = (() => {
+          // handle the case when user closes the form while transaction is in progress
+          console.log('Transaction cancelled.');
+        });
+        const rzp = new this.winRef.nativeWindow.Razorpay(options);
+        rzp.open();
     }
 
 }
