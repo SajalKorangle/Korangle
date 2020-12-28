13fa0bc7db35d54fa257e5ce02c9d694807451b9
Shubham Agrawal <agrawal152000@gmailcom>
2020-12-29 00:46:18 +0530

    Issue homework page

---
+++ frontend/tests/protractor/src/modules/homework/pages/issue-homework/issue-homework.e2e-spec.ts
@@ -0,0 +1,53 @@
+import {BeforeAfterEach} from '../../../../beforeAterEach';
+import {startBackendServer} from '../../../../backend-server';
+import { getFixtureFiles } from '../../../../../../fixtures/fixture-map';
+import {openModuleAndPage, reClickPage} from '../../../../open-page';
+import {containsFirst} from '../../../../contains';
+
+describe('Homework -> Issue Homework', () => {
+
+    let page: any;
+
+    afterEach( async () => {
+        await BeforeAfterEach.afterEach();
+    });
+
+    it('Creating Homework', async () => {
+
+        // Start Backend Server
+        startBackendServer(getFixtureFiles('modules/homework/pages/issue-homework/issue-homework.json'));
+
+        page = await BeforeAfterEach.beforeEach();
+
+        // Opening Page
+        await openModuleAndPage('Homework', 'Issue Homework');
+
+        // Get Homework Create Template
+        await (await containsFirst('button', 'Get')).click();
+        
+        await BeforeAfterEach.page.waitForXPath('//input[@testId="homeworkName"]');
+        const [inputElement] = await BeforeAfterEach.page.$x('//input[@testId="homeworkName"]');
+        await inputElement.type('Chapter 1, Ex 1');
+
+        // Checking the confirmation message by registering dialog callback
+        page.on('dialog', async dialog => {
+            expect(dialog.message()).toBe('Homework has been successfully created');
+            await dialog.dismiss();
+        });
+
+        //Creating the homework
+        await (await containsFirst('button', 'Create')).click();
+
+        //Verifying Created Homework in Check Homework Page
+        await openModuleAndPage('Homework', 'Create Homework');
+
+        await page.waitForXPath('//td[@testId="homeworkCount"]');
+        const list = await page.$x('//td[@testId="homeworkCount"]');
+        expect(list.length).toBe(1);
+
+        afterAll(async () => {
+            await BeforeAfterEach.afterEach();
+        })
+    });
+
+});
