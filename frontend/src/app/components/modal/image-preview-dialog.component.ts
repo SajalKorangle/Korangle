a1ef70aaab4583d0e14492a69d4e4944274c3bf7
Shubham Agrawal <agrawal152000@gmailcom>
2021-01-01 01:39:17 +0530

    image preview fixed

--- frontend/src/app/components/modal/image-preview-dialog.component.ts
+++ frontend/src/app/components/modal/image-preview-dialog.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Inject} from '@angular/core';
+import {Component, Inject} from '@angular/core';
 import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
 
@@ -27,7 +27,59 @@ export class ImagePreviewDialogComponent {
     }
 
     moveToIndex: any;
-  
+
+    imageStyle = {
+        'display': 'table-cell',
+        'margin': 'auto',
+        'width': '50%',
+        'height': '50%',
+    }
+    maxSize = false;
+    minSize = false;
+
+    increaseSize(): any{
+        this.minSize = false;
+        let currentWidth = this.imageStyle.width;
+        if(currentWidth.length == 3){
+            this.imageStyle.width = '100%';
+            this.imageStyle.height = '100%';
+        }
+        else{
+            if(currentWidth[1] == '5'){
+                this.imageStyle.width = '200%';
+                this.imageStyle.height = '200%';
+            }
+            else if(currentWidth[0] == '1'){
+                this.imageStyle.width = '150%';
+                this.imageStyle.height = '150%';
+            }
+            else{
+                this.maxSize = true;
+            }
+        }
+    }
+    decreaseSize(): any{
+        this.maxSize = false;
+        let currentWidth = this.imageStyle.width;
+        if(currentWidth.length == 3){
+            this.minSize = true;
+        }
+        else{
+            if(currentWidth[1] == '5'){
+                this.imageStyle.width = '100%';
+                this.imageStyle.height = '100%';
+            }
+            else if(currentWidth[0] == '1'){
+                this.imageStyle.width = '50%';
+                this.imageStyle.height = '50%';
+            }
+            else if(currentWidth[0] == '2'){
+                this.imageStyle.width = '150%';
+                this.imageStyle.height = '150%';
+            }
+        }
+    }
+
     onNoClick(): void {
         this.dialogRef.close();
     }
