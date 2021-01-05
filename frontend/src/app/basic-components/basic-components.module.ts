2fff3e0a7327adce2f8b2e95e0f944b96f61e360
rachit <rachit8562@gmail.com>
2021-01-05 21:26:24 +0530

    added DOCUMENT field in manageparameter

--- frontend/src/app/basic-components/basic-components.module.ts
+++ frontend/src/app/basic-components/basic-components.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from "@angular/forms";
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule,MatIconModule} from '@angular/material';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -34,6 +35,8 @@ import {SafePipe} from '../pipes/safe.pipe';
         MatFormFieldModule,
         MatInputModule,
         MatDialogModule,
+        MatButtonModule,
+        MatIconModule,
     ],
     exports: [
 
@@ -50,6 +53,8 @@ import {SafePipe} from '../pipes/safe.pipe';
         MatInputModule,
         MatDialogModule,
         SafePipe,
+        MatButtonModule,
+        MatIconModule,
     ]
 })
 export class BasicComponentsModule { }
