ec828477605ae9e89b5e6acf101106397082a1ff
aditya <kumar.93@iitj.ac.in>
2021-01-25 00:06:08 +0530

    db for sharing complete

--- backend/report_card_app/urls.py
+++ backend/report_card_app/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import url, include
 
 urlpatterns = []
 
-from report_card_app.views import ReportCardLayoutView, ReportCardLayoutListView, ReportCardLayoutNewView, ReportCardLayoutNewListView, LayoutAccessView, LayoutAccessListView, ImageAssetsView, ImageAssetsListView
+from report_card_app.views import ReportCardLayoutView, ReportCardLayoutListView, ReportCardLayoutNewView, ReportCardLayoutNewListView, LayoutSharingView, LayoutSharingListView, ImageAssetsView, ImageAssetsListView
 
 urlpatterns += [
 
     url(r'^report-card-layout-new/batch', ReportCardLayoutNewListView.as_view()),
     url(r'^report-card-layout-new', ReportCardLayoutNewView.as_view()),
-    url(r'^layout-access/batch', LayoutAccessListView.as_view()),
-    url(r'^layout-access', LayoutAccessView.as_view()),
+    url(r'^layout-sharing/batch', LayoutSharingListView.as_view()),
+    url(r'^layout-sharing', LayoutSharingView.as_view()),
     url(r'^image-assets/batch', ImageAssetsListView.as_view()),
     url(r'^image-assets', ImageAssetsView.as_view()),
     url(r'^report-card-layout/batch', ReportCardLayoutListView.as_view()),
--- backend/report_card_app/views.py
+++ backend/report_card_app/views.py
@@ -1,7 +1,7 @@
-from common.common_views_3 import CommonView, CommonListView
-from rest_framework.views import APIView
-from report_card_app.models import ReportCardLayout, ReportCardLayoutNew, LayoutAccess, ImageAssets
-
+from common.common_views_3 import CommonView, CommonListView, APIView
+from common.common_serializer_interface_3 import get_object, get_list
+from report_card_app.models import ReportCardLayout, ReportCardLayoutNew, LayoutSharing, ImageAssets
+from decorators import user_permission_3
 
 # ReportCardLayout
 class ReportCardLayoutView(CommonView, APIView):
@@ -15,15 +15,48 @@ class ReportCardLayoutListView(CommonListView, APIView):
 
 class ReportCardLayoutNewView(CommonView, APIView):
     Model = ReportCardLayoutNew
+    RelationsToSchool = ['parentSchool__id']
+
+    @user_permission_3
+    def get(self, request, activeSchoolId, activeStudentID):
+        layoutSharingList = LayoutSharing.objects.filter(parentSchool=activeSchoolId)
+        sharedLayouts = list(map(lambda o:o.parentLayout, list(layoutSharingList)))
+        filtered_query_set = self.permittedQuerySet(activeSchoolID, activeStudentID)
+        filtered_query_set = filtered_query_set.union(self.Model.objects.filter(Q(public=True) or Q(id__in=sharedLayouts)))   # extending filtered query set
+        return get_object(request.GET, filtered_query_set, self.ModelSerializer)
+            
 
 class ReportCardLayoutNewListView(CommonListView, APIView):
     Model = ReportCardLayoutNew
+    RelationsToSchool = ['parentSchool__id']
 
-class LayoutAccessView(CommonView, APIView):
-    Model = LayoutAccess
+    @user_permission_3
+    def get(self, request, activeSchoolId, activeStudentID):
+        layoutSharingList = LayoutSharing.objects.filter(parentSchool=activeSchoolId)
+        sharedLayouts = list(map(lambda o:o.parentLayout, list(layoutSharingList)))
+        filtered_query_set = self.permittedQuerySet(activeSchoolID, activeStudentID)
+        filtered_query_set = filtered_query_set.union(self.Model.objects.filter(Q(public=True) or Q(id__in=sharedLayouts)))   # extending filtered query set
+        return get_list(request.GET, filtered_query_set, self.ModelSerializer)
 
-class LayoutAccessListView(CommonListView, APIView):
-    Model = LayoutAccess
+class LayoutSharingView(CommonView, APIView):
+    RelationsToSchool = ['parentLayout__parentSchool__id']
+    Model = LayoutSharing
+
+    @user_permission_3
+    def get(self, request, activeSchoolId, activeStudentID):
+        filtered_query_set = self.permittedQuerySet(activeSchoolID, activeStudentID)
+        filtered_query_set = filtered_query_set.union(self.Model.objects.filter(parentSchool=activeSchoolId))   # extending filtered query set
+        return get_object(request.GET, filtered_query_set, self.ModelSerializer)
+
+class LayoutSharingListView(CommonListView, APIView):
+    RelationsToSchool = ['parentLayout__parentSchool__id']
+    Model = LayoutSharing
+
+    @user_permission_3
+    def get(self, request, activeSchoolId, activeStudentID):
+        filtered_query_set = self.permittedQuerySet(activeSchoolID, activeStudentID)
+        filtered_query_set = filtered_query_set.union(self.Model.objects.filter(parentSchool=activeSchoolId))   # extending filtered query set
+        return get_list(request.GET, filtered_query_set, self.ModelSerializer)
 
 class ImageAssetsView(CommonView, APIView):
     Model = ImageAssets
