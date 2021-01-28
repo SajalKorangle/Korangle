8fa77d488f6e19b79f6dde82813b7a95f9ad30b8
SyedAnas26 <ahmed.anas069@gmail.com>
2021-01-28 15:22:02 +0530

    add event ui done

--- backend/event_gallery_app/models.py
+++ backend/event_gallery_app/models.py
@@ -5,6 +5,7 @@ from django.db import models
 # Create your models here.
 from django.utils.timezone import now
 
+from class_app.models import Class
 from school_app.model.models import School
 
 
@@ -18,24 +19,22 @@ class Event(models.Model):
                                      verbose_name='parentSchool')
     eventTitle = models.TextField(null=False, default='', verbose_name='eventTitle')
     eventDescription = models.TextField(null=True, blank=True, verbose_name='eventDescription')
+    heldOn = models.DateTimeField(null=True, verbose_name='heldOn')
     generationDateTime = models.DateTimeField(auto_now_add=True, verbose_name='generationDateTime')
     notifyEmployees = models.BooleanField(null=False, default=False, verbose_name='notifyEmployees')
-    notifyTeachers = models.BooleanField(null=False, default=False, verbose_name='notifyTeachers')
-    notifyClass1 = models.BooleanField(null=False, default=False, verbose_name='notifyClass1')
-    notifyClass2 = models.BooleanField(null=False, default=False, verbose_name='notifyClass2')
-    notifyClass3 = models.BooleanField(null=False, default=False, verbose_name='notifyClass3')
-    notifyClass4 = models.BooleanField(null=False, default=False, verbose_name='notifyClass4')
-    notifyClass5 = models.BooleanField(null=False, default=False, verbose_name='notifyClass5')
-    notifyClass6 = models.BooleanField(null=False, default=False, verbose_name='notifyClass6')
-    notifyClass7 = models.BooleanField(null=False, default=False, verbose_name='notifyClass7')
-    notifyClass8 = models.BooleanField(null=False, default=False, verbose_name='notifyClass8')
-    notifyClass9 = models.BooleanField(null=False, default=False, verbose_name='notifyClass9')
-    notifyClass10 = models.BooleanField(null=False, default=False, verbose_name='notifyClass10')
-    notifyClass11 = models.BooleanField(null=False, default=False, verbose_name='notifyClass11')
-    notifyClass12 = models.BooleanField(null=False, default=False, verbose_name='notifyClass12')
 
     class Meta:
         db_table = 'event'
+
+
+class NotifyClass(models.Model):
+    parentEvent = models.ForeignKey(Event, on_delete=models.CASCADE, null=False, default=0,
+                                    verbose_name='parentEvent')
+    parentClass = models.ForeignKey(Class, on_delete=models.CASCADE, null=False, default=0,
+                                    verbose_name='parentClass')
+
+    class Meta:
+        db_table = 'notify_class'
 
 
 class EventTag(models.Model):
@@ -51,8 +50,18 @@ class EventImage(models.Model):
     parentEvent = models.ForeignKey(Event, on_delete=models.CASCADE, null=False, default=0,
                                     verbose_name='parentEvent')
     eventImage = models.ImageField('event_image', upload_to=upload_event_images_to, blank=True, null=True)
+    imageSize = models.TextField(null=True, blank=True,verbose_name='imageSize')
     orderNumber = models.IntegerField(null=False, default=0, verbose_name='orderNumber')
-    tags = models.ManyToManyField(to=EventTag)
 
     class Meta:
         db_table = 'event_images'
+
+
+class ImageTags(models.Model):
+    parentEventImage = models.ForeignKey(EventImage, on_delete=models.CASCADE, null=False, default=0,
+                                         verbose_name='parentEventImage')
+    parentEventTag = models.ForeignKey(EventTag, on_delete=models.CASCADE, null=False, default=0,
+                                       verbose_name='parentEventTag')
+
+    class Meta:
+        db_table = 'image_tags'
--- frontend/src/app/modules/event-gallery/pages/add-event/add-event.component.ts
+++ frontend/src/app/modules/event-gallery/pages/add-event/add-event.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-add-event',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-event.component.css']
 })
 export class AddEventComponent implements OnInit {
-
+  
+  user:any;
+  
+  selectionList=new FormControl();
+  
+  notifySelectionList:string[]=['None','Select All','Employees','All Students','Class 1','Class 2','Nursery','U.K.G','L.K.G']
+  
   constructor() { }
 
   ngOnInit() {
+    this.selectionList.setValue(this.notifySelectionList.slice(0,1));
   }
 
+  handleSelectionChange(val:any) {
+    console.log(val);
+    if(val=='None'){
+      this.selectionList.setValue(this.notifySelectionList.slice(0,1));
+    }else if(val=='Select All') {
+      this.selectionList.setValue(this.notifySelectionList.slice(1,2));
+    }else if(val=='All Students') {
+      this.selectionList.setValue(this.selectionList.value.filter(value=>{
+        return !(value.startsWith('Class') || value.startsWith('N'))
+      }));
+    }else {
+       this.selectionList.setValue(this.selectionList.value.filter(value=>{
+        return !(value == "None" || value == "Select All");
+      }));
+    }
+    console.log(this.selectionList.value);
+  }
 }
