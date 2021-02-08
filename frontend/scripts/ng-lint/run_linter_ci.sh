3f5b8398104a96ecd69c213cc84ab0e21540670a
Sayan B <bsayan12345@gmail.com>
2021-02-08 19:26:43 +0530

    fix ci linter

--- frontend/scripts/ng-lint/run_linter_ci.sh
+++ frontend/scripts/ng-lint/run_linter_ci.sh
@@ -1,5 +1,9 @@
 # for CI platform only
 
+echo "{
+ \"numberOfSemiColonInTests\": 1
+}" > benchmark-linting-error-count.json
+
 echo $'CI: Testing linting error count...\n'
 node run_linter.js "ci" $(ng lint | grep -c "ERROR: /") || exit
 
