440a3939baed709d182124acddbc9403e3821150
Sayan B <bsayan12345@gmail.com>
2021-02-09 19:38:30 +0530

    QOL fix

--- frontend/scripts/ng-lint/run_linter_ci.sh
+++ frontend/scripts/ng-lint/run_linter_ci.sh
@@ -1,12 +1,15 @@
 # for CI platform only
 
-# gen json file
-echo "{
- \"tslint-errors\": 1
-}" > benchmark-linting-error-count.json
+
+if [[ ! -f benchmark-linting-error-count.json ]]; then
+
+    echo $'{\n\t\"tslint-errors\": 0\n}' > benchmark-linting-error-count.json
+
+fi
 
 echo $'CI: Testing linting error count...\n'
 node run_linter.js "ci" $(ng lint | grep -c "ERROR: /") || exit
 
 # upload to aws
-
+# aws cp
+echo $'\nLinter benchmark updated.\n'
