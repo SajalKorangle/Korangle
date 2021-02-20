a52ff3ffd4f936cbbd05b5ea6ddee8d2113eac49
Sayan B <bsayan12345@gmail.com>
2021-02-20 15:40:48 +0530

    clean up

--- frontend/src/environments/environment.mobile.ts
+++ frontend/src/environments/environment.mobile.ts
@@ -1,18 +1,23 @@
-// This file will be override by environment.prod.ts during production build
+// environment.ts file will be overridden by this file during mobile testing build
 
 /*
+backend/test_mobile_backend.sh      //start backend for mobile testing
+frontend/test_mobile_frontend.sh    //start frontend for mobile testing
+*/
+
+
 var { exec } = require("child_process");
 
 exec("hostname -I | awk '{print $1}'", (error, stdout, stderr) => {
     var IP = `http://:${stdout}:8000`;
     console.log(IP);
 });
-*/
+
 
 export const environment = {
     production: false,
     versionCheckURL: 'www.korangle.com/version.json',
-    //DJANGO_SERVER: 'http://localhost:8000', // normal debugging only (localhost)
-    DJANGO_SERVER: 'http://192.168.29.79:8000', //for mobile + normal debugging (LAN)
+    //DJANGO_SERVER: 'http://localhost:8000',     // normal debugging only (localhost)
+    DJANGO_SERVER: 'http://192.168.29.79:8000', //for mobile + normal debugging (LAN) (Your local IP goes here: <http://IP:8000>)
     api_version: '/v2.0',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,18 +1,8 @@
 // This file will be override by environment.prod.ts during production build
 
-/*
-var { exec } = require("child_process");
-
-exec("hostname -I | awk '{print $1}'", (error, stdout, stderr) => {
-    var IP = `http://:${stdout}:8000`;
-    console.log(IP);
-});
-*/
-
 export const environment = {
     production: false,
     versionCheckURL: 'www.korangle.com/version.json',
     DJANGO_SERVER: 'http://localhost:8000', // normal debugging only (localhost)
-    //DJANGO_SERVER: 'http://192.168.29.79:8000', //for mobile + normal debugging (LAN)
     api_version: '/v2.0',
 };
