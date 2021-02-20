2cf39c61d55967b99f248ddbf9ffd25fd3bd4b2c
Sayan B <bsayan12345@gmail.com>
2021-02-20 16:59:42 +0530

    checkpoint

--- frontend/src/environments/environment.mobile.ts
+++ frontend/src/environments/environment.mobile.ts
@@ -6,18 +6,22 @@ frontend/test_mobile_frontend.sh    //start frontend for mobile testing
 */
 
 
-var { exec } = require("child_process");
+/*
+// npx ts-node environment.mobile.ts
 
-exec("hostname -I | awk '{print $1}'", (error, stdout, stderr) => {
-    var IP = `http://:${stdout}:8000`;
-    console.log(IP);
-});
+const { exec } = require("child_process");
 
+var IP = exec('hostname -I | awk \'{print $1}\'');
+
+IP = `http://:${IP}:8000`;
+
+console.log(IP);
+*/
 
 export const environment = {
     production: false,
     versionCheckURL: 'www.korangle.com/version.json',
-    //DJANGO_SERVER: 'http://localhost:8000',     // normal debugging only (localhost)
+    //DJANGO_SERVER: 'http://localhost:8000',   // normal debugging only (localhost)
     DJANGO_SERVER: 'http://192.168.29.79:8000', //for mobile + normal debugging (LAN) (Your local IP goes here: <http://IP:8000>)
     api_version: '/v2.0',
 };
