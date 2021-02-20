50d57b91d6d016e089050421fd3c1b72d0b44728
Sayan B <bsayan12345@gmail.com>
2021-02-20 17:27:45 +0530

    add auto IP

--- frontend/src/environments/environment.mobile.ts
+++ frontend/src/environments/environment.mobile.ts
@@ -21,7 +21,7 @@ console.log(IP);
 export const environment = {
     production: false,
     versionCheckURL: 'www.korangle.com/version.json',
-    //DJANGO_SERVER: 'http://localhost:8000',   // normal debugging only (localhost)
-    DJANGO_SERVER: 'http://192.168.29.79:8000', //for mobile + normal debugging (LAN) (Your local IP goes here: <http://IP:8000>)
+    //DJANGO_SERVER: 'http://localhost:8000',                      // normal debugging only (localhost)
+    DJANGO_SERVER: 'http://' + window.location.hostname + ':8000', //for mobile + normal debugging (LAN) (Your local IP goes here: <http://IP:8000>)
     api_version: '/v2.0',
 };
