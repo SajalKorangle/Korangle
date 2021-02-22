d955b4cfb890bd49d6781debe5c25a46da271e39
aditya <kumar.93@iitj.ac.in>
2021-02-22 17:25:30 +0530

    migration fix

--- backend/report_card_app/db_script/data_migration_to_ver_3_1.py
+++ backend/report_card_app/db_script/data_migration_to_ver_3_1.py
@@ -10,10 +10,19 @@ def dataMigration(apps, schema_editor):
         content = json.loads(layout.content)
         for page in content:
             for layer in page['layers']:
-                if (layer['LAYER_TYPE'] == 'TEXT'):
+                if (layer['LAYER_TYPE'] in ['TEXT', 'DATE', 'ATTENDANCE', 'GRADE', 'REMARK', 'MARKS', 'FORMULA', 'RESULT']):
                     layer['textAlign'] = 'left'
                     layer['textBaseline'] = 'top'
                     layer['maxWidth'] = 200
+                    if (layer['LAYER_TYPE'] == 'MARKS'):
+                        outOf = 100
+                        if (layer['parentExamination'] and layer['parentSubject'] and layer['testType'] and layer['marksType']):
+                            testFilter = TestSecond.objects.filter(parentExamination=layer['parentExamination'], parentSubject=layer['parentSubject'], testType=layer['testType'])
+                            if (len(testFilter) > 0):
+                                outOf = testFilter.first().maximumMarks
+                        outOf *= layer['factor']
+                        layer['outOf'] = outOf
+                        del layer['factor']
 
                 elif (layer['LAYER_TYPE'] == 'TABLE'):
                     strokeStyle = layer['tableStyle']['strokeStyle']
@@ -51,15 +60,6 @@ def dataMigration(apps, schema_editor):
                             rowCells.append(cellDict)
                         cells.append(rowCells)
                     layer['cells'] = cells
-                elif (layer['LAYER_TYPE'] == 'MARKS'):
-                    outOf = 100
-                    if (layer['parentExamination'] and layer['parentSubject'] and layer['testType'] and layer['marksType']):
-                        testFilter = TestSecond.objects.filter(parentExamination=layer['parentExamination'], parentSubject=layer['parentSubject'], testType=layer['testType'])
-                        if (len(testFilter) > 0):
-                            outOf = testFilter.first().maximumMarks
-                    outOf *= layer[factor]
-                    layer['outOf'] = outOf
-                    del layer['factor']
         layout.content = json.dumps(content)
         layout.save()            
 
