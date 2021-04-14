d22822a515a2f52e52d48d577217ac6cd5b1d10a
harshal <harshalagrawal03@gmail.com>
2021-04-14 16:50:35 +0530

    showing student number for sessions and expired state in django admin panel

--- backend/school_app/admin.py
+++ backend/school_app/admin.py
@@ -6,10 +6,20 @@ from django.contrib import admin
 
 from school_app.model.models import School, Session, SchoolSession
 
-'''admin.site.register(Student)
-admin.site.register(Fee)
-admin.site.register(SubFee)
-admin.site.register(Concession)'''
-admin.site.register(School)
+from student_app.models import StudentSection
+
 admin.site.register(Session)
-admin.site.register(SchoolSession)
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('Name', 'Session_2020_21', 'Session_2021_22', 'expired')
+    list_filter = ('expired',)
+
+    def Name(self,obj):
+        return str(obj.pk) + ' - ' + obj.printName
+
+    def Session_2020_21(self, obj):
+        return str(StudentSection.objects.filter(parentStudent__parentSchool_id=obj.pk, parentSession__name='Session 2020-21').count())
+
+    def Session_2021_22(self, obj):
+        return str(StudentSection.objects.filter(parentStudent__parentSchool_id=obj.pk, parentSession__name='Session 2021-22').count())
