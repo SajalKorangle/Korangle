a8228f2991a23275bbbfc79c7f27a18b48c8998f
harshal <harshalagrawal03@gmail.com>
2021-04-15 13:37:52 +0530

    1. # of Students in schools are seen for all sessions, 2. Schools can be searched based on print name and KID

--- backend/school_app/admin.py
+++ backend/school_app/admin.py
@@ -12,14 +12,25 @@ admin.site.register(Session)
 
 @admin.register(School)
 class SchoolAdmin(admin.ModelAdmin):
-    list_display = ('Name', 'Session_2020_21', 'Session_2021_22', 'expired')
+    search_fields = ('printName', 'id')
+    list_display = ('Name', 'S_2021_22', 'S_2020_21', 'S_2019_20', 'S_2018_19', 'S_2017_18', 'expired')
     list_filter = ('expired',)
 
     def Name(self,obj):
         return str(obj.pk) + ' - ' + obj.printName
 
-    def Session_2020_21(self, obj):
+    def S_2021_22(self, obj):
+        return str(StudentSection.objects.filter(parentStudent__parentSchool_id=obj.pk, parentSession__name='Session 2021-22').count())
+
+    def S_2020_21(self, obj):
         return str(StudentSection.objects.filter(parentStudent__parentSchool_id=obj.pk, parentSession__name='Session 2020-21').count())
 
-    def Session_2021_22(self, obj):
-        return str(StudentSection.objects.filter(parentStudent__parentSchool_id=obj.pk, parentSession__name='Session 2021-22').count())
+    def S_2019_20(self, obj):
+        return str(StudentSection.objects.filter(parentStudent__parentSchool_id=obj.pk, parentSession__name='Session 2019-20').count())
+
+    def S_2018_19(self, obj):
+        return str(StudentSection.objects.filter(parentStudent__parentSchool_id=obj.pk, parentSession__name='Session 2018-19').count())
+
+    def S_2017_18(self, obj):
+        return str(StudentSection.objects.filter(parentStudent__parentSchool_id=obj.pk, parentSession__name='Session 2017-18').count())
+
