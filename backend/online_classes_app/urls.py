c5385523721bf8e958ddb4b2cff3845cbf61b36b
aditya <kumar.93@iitj.ac.in>
2021-04-25 16:28:57 +0530

    view fix

--- backend/online_classes_app/urls.py
+++ backend/online_classes_app/urls.py
@@ -7,8 +7,8 @@ urlpatterns = []
 from online_classes_app import views
 
 urlpatterns += [
-    url(r'^online-class/batch', views.OnlineClassView.as_view()),
-    url(r'^active-class', views.OnlineClassView.as_view()),
+    url(r'^online-class/batch', views.OnlineClassListView.as_view()),
+    url(r'^online-class', views.OnlineClassView.as_view()),
 ]
 
 
