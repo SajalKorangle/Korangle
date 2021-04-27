46e575bcdd7f93a10fe7e530acb0404171e700c6
SyedAnas26 <ahmed.anas069@gmail.com>
2021-04-27 17:22:01 +0530

    added loader in videomodal componenet

--- frontend/src/app/basic-components/modal-video/modal-video.component.ts
+++ frontend/src/app/basic-components/modal-video/modal-video.component.ts
@@ -10,12 +10,23 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 export class ModalVideoComponent implements OnInit {
     user: any;
     videoUrl: string;
+    isIFrameLoading:boolean;
+    youtubeIdMatcher = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|vi|e(?:mbed)?)\/|\S*?[?&]v=|\S*?[?&]vi=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
 
     constructor(public dialogRef: MatDialogRef<ModalVideoComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
         this.videoUrl = this.data.videoUrl;
+        this.isIFrameLoading = true;
     }
 
     ngOnInit(): void {
         this.user = DataStorage.getInstance().getUser();
+    }
+
+    getVideoUrlId() {
+        return this.videoUrl.match(this.youtubeIdMatcher)[1];
+    }
+
+    onReady(event: any) {
+        this.isIFrameLoading = false;
     }
 }
