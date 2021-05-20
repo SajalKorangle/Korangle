7524acd03c1ae4d32cd6640720173b07e875a6a7
Aditya Kumar <adityakumar@Adityas-MacBook-Air.local>
2021-05-20 10:34:28 +0530

    formatting fix

--- backend/employee_app/models.py
+++ backend/employee_app/models.py
@@ -14,8 +14,7 @@ def upload_avatar_to(instance, filename):
 class Employee(models.Model):
 
     # ProfileImage
-    profileImage = models.ImageField(
-        "Avatar", upload_to=upload_avatar_to, blank=True)
+    profileImage = models.ImageField("Avatar", upload_to=upload_avatar_to, blank=True)
 
     # Name
     name = models.CharField(max_length=100)
@@ -87,8 +86,7 @@ class Employee(models.Model):
     dateOfLeaving = models.DateField(null=True)
 
     # School Id
-    parentSchool = models.ForeignKey(
-        School, on_delete=models.PROTECT, default=0)
+    parentSchool = models.ForeignKey(School, on_delete=models.PROTECT, default=0)
 
     class Meta:
         db_table = 'employee'
@@ -97,12 +95,9 @@ class Employee(models.Model):
 
 class EmployeeSessionDetail(models.Model):
 
-    parentEmployee = models.ForeignKey(
-        Employee, on_delete=models.CASCADE, null=False, verbose_name='parentEmployee', default=0)
-    parentSession = models.ForeignKey(
-        Session, on_delete=models.PROTECT, null=False, verbose_name='parentSession', default=0)
-    paidLeaveNumber = models.IntegerField(
-        null=True, verbose_name='paidLeaveNumber')
+    parentEmployee = models.ForeignKey(Employee, on_delete=models.CASCADE, null=False, verbose_name='parentEmployee', default=0)
+    parentSession = models.ForeignKey(Session, on_delete=models.PROTECT, null=False, verbose_name='parentSession', default=0)
+    paidLeaveNumber = models.IntegerField(null=True, verbose_name='paidLeaveNumber')
 
     class Meta:
         db_table = 'employee_session_detail'
@@ -111,10 +106,8 @@ class EmployeeSessionDetail(models.Model):
 
 class EmployeePermission(models.Model):
 
-    parentTask = models.ForeignKey(
-        Task, on_delete=models.PROTECT, null=False, verbose_name='parentTask', default=0)
-    parentEmployee = models.ForeignKey(
-        Employee, on_delete=models.CASCADE, null=False, verbose_name='parentEmployee', default=0)
+    parentTask = models.ForeignKey(Task, on_delete=models.PROTECT, null=False, verbose_name='parentTask', default=0)
+    parentEmployee = models.ForeignKey(Employee, on_delete=models.CASCADE, null=False, verbose_name='parentEmployee', default=0)
     configJSON = models.TextField(default="{}")
 
     def __str__(self):
