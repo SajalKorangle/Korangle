754bef0a024afc60351a4f3acfea9cfbfdb3562d
Aditya Kumar <adityakumar@Adityas-MacBook-Air.local>
2021-06-10 08:00:26 +0530

    formatting

--- frontend/src/app/modules/fees/pages/online-payment-account/online-payment-account.service.adapter.ts
+++ frontend/src/app/modules/fees/pages/online-payment-account/online-payment-account.service.adapter.ts
@@ -5,10 +5,10 @@ export class OnlinePaymentAccountServiceAdapter {
 
     vm: OnlinePaymentAccountComponent;
 
-    constructor() {}
+    constructor() { }
 
     // Data
-    currentSchoolDetails : any;
+    currentSchoolDetails: any;
 
     initializeAdapter(vm: OnlinePaymentAccountComponent): void {
         this.vm = vm;
@@ -16,26 +16,25 @@ export class OnlinePaymentAccountServiceAdapter {
 
     //initialize data
     initializeData(): void {
-        this.vm.feeService.get(this.vm.feeService.online_payment_account,{parentSchool:this.vm.user.activeSchool.dbId}).then(value => {
+        this.vm.feeService.getObject(this.vm.feeService.online_payment_account, { parentSchool: this.vm.user.activeSchool.dbId }).then(value => {
             console.log('School online payment account details ');
-            console.dir(value)
-            console.log(value.data == undefined)
-            if(value.data !=undefined)
-            {   
+            console.dir(value);
+            console.log(value.data == undefined);
+            if (value.data != undefined) {
                 this.vm.schoolExistingAccount = true;
                 this.vm.school.name = value.data.name;
                 this.vm.school.email = value.data.email;
                 this.vm.school.phone = value.data.phone;
-                this.vm.school.aadharNo =value.data.aadharNo;
+                this.vm.school.aadharNo = value.data.aadharNo;
                 this.vm.school.panNo = value.data.panNo;
                 this.vm.school.bankAccount = value.data.bankAccount;
                 this.vm.school.ifsc = value.data.ifsc;
                 this.vm.school.accountHolder = value.data.accountHolder;
                 this.vm.school.gstin = value.data.gstin;
-                this.vm.school.status = value.data.status
+                this.vm.school.status = value.data.status;
             }
-            
-        })
+
+        });
     }
 
 
