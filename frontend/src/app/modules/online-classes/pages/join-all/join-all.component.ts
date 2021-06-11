61a3de218e92854b86ad12585699866fb090a6e9
Ruthwik <ruthwik34@pop-os.localdomain>
2021-06-11 19:31:57 +0530

    set up working page

--- frontend/src/app/modules/online-classes/pages/join-all/join-all.component.ts
+++ frontend/src/app/modules/online-classes/pages/join-all/join-all.component.ts
@@ -5,14 +5,32 @@ import { DataStorage } from "@classes/data-storage";
 import { JoinAllServiceAdapter } from './join-all.service.adapter';
 import { JoinAllHtmlRenderer } from './join-all.html.renderer';
 
+import { AccountInfo } from '@services/modules/online-class/models/account-info';
+import { ClassSubject } from '@services/modules/subject/models/class-subject';
+import { Subject } from '@services/modules/subject/models/subject';
+import {OnlineClass} from '@services/modules/online-class/models/online-class';
+
+import { SubjectService } from '@services/modules/subject/subject.service';
+import { OnlineClassService } from '@services/modules/online-class/online-class.service';
+import { ClassService } from '@services/modules/class/class.service';
+import { SchoolService } from '@services/modules/school/school.service';
+
 @Component({
     selector: 'join-all',
     templateUrl: './join-all.component.html',
     styleUrls: ['./join-all.component.css'],
-    providers: [ ],
+    providers: [SubjectService, OnlineClassService, ClassService, SchoolService ],
 })
 
 export class JoinAllComponent implements OnInit {
+
+    onlineClassList: Array<OnlineClass>;
+    accountInfo: AccountInfo;
+    classSubjectList: Array<ClassSubject>;
+    subjectList: Array<Subject>;
+
+    classList: Array<any>;
+    divisionList: Array<any>;
 
     user: any;
 
@@ -21,10 +39,12 @@ export class JoinAllComponent implements OnInit {
 
     userInput = {};
     backendData = {};
-    stateKeeper = { isLoading: false };
-    
-
-    constructor () { }
+    isLoading: any;
+    constructor ( public subjectService: SubjectService,
+        public onlineClassService: OnlineClassService,
+        public classService: ClassService,
+        public schoolService: SchoolService,
+    ) { }
 
     ngOnInit(): void {
         this.user = DataStorage.getInstance().getUser();
@@ -34,6 +54,17 @@ export class JoinAllComponent implements OnInit {
 
         this.serviceAdapter = new JoinAllServiceAdapter();
         this.serviceAdapter.initialize(this);
-
+        this.serviceAdapter.initializeData();
+        console.log(this);
+        
+    }
+    getAllCurrentClasses():any{
+        this.onlineClassList.filter((onlineClass)=>{
+            return this.IsOngoing(onlineClass) ;
+        });
+    }
+    IsOngoing(onlineClass:any):boolean{
+        let currentTime = new Date();
+        return true;
     }
 }
--- frontend/src/app/modules/online-classes/pages/join-all/join-all.service.adapter.ts
+++ frontend/src/app/modules/online-classes/pages/join-all/join-all.service.adapter.ts
@@ -10,4 +10,38 @@ export class JoinAllServiceAdapter {
         this.vm = vm;
     }
 
+    async initializeData(){
+
+        // const class_subject_request = {
+        //     parentEmployee: this.vm.user.activeSchool.employeeId,
+        //     parentSession: this.vm.user.activeSchool.currentSessionDbId,
+        // };
+
+        const account_info_request = {
+            parentEmployee: this.vm.user.activeSchool.employeeId,
+        };
+
+        this.vm.isLoading = true;
+
+        [
+            this.vm.classSubjectList,
+            this.vm.classList,
+            this.vm.divisionList,
+            this.vm.subjectList,
+            this.vm.accountInfo,
+        ] = await Promise.all([
+            this.vm.subjectService.getObjectList(this.vm.subjectService.class_subject, {}),
+            this.vm.classService.getObjectList(this.vm.classService.classs, {}),
+            this.vm.classService.getObjectList(this.vm.classService.division, {}),
+            this.vm.subjectService.getObjectList(this.vm.subjectService.subject, {}),
+            this.vm.onlineClassService.getObject(this.vm.onlineClassService.account_info, {}),
+        ]);
+        // const online_class_request = {
+        //     parentClassSubject__parentSession: this.vm.user.activeSchool.currentSessionDbId,
+        //     parentClassSubject__in: this.vm.classSubjectList.map(classSubject => classSubject.id),
+        // };
+        this.vm.onlineClassList=await this.vm.onlineClassService.getObjectList(this.vm.onlineClassService.online_class, {});
+        this.vm.isLoading=false;
+    }
+
 }
