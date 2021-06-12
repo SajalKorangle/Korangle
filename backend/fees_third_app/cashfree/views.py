27a28b1ad5d5f924258c7df78c63b96da9656332
Aditya Kumar <adityakumar@Adityas-MacBook-Air.local>
2021-06-12 13:23:26 +0530

    verification api integration

--- backend/fees_third_app/cashfree/views.py
+++ backend/fees_third_app/cashfree/views.py
@@ -1,10 +1,24 @@
 from rest_framework.views import APIView
 from decorators import user_permission_3
-from fees_third_app.cashfree.cashfree import getSettelmentsCycleList
+from fees_third_app.cashfree.cashfree import getSettelmentsCycleList, ifscVerification, bankVerification
 
 
 class SettelmentsCycleListView(APIView):
 
     @user_permission_3
     def get(self, request, *args, **kwargs):
-        return getSettelmentsCycleList()    
+        return getSettelmentsCycleList()   
+
+
+class IFSCVerification(APIView):
+
+    @user_permission_3
+    def get(self, request, *args, **kwargs):
+        return ifscVerification(request.GET)
+
+
+class BankAccountVerification(APIView):
+
+    @user_permission_3
+    def post(self, request, *args, **kwargs):
+        return bankVerification(request.data)
