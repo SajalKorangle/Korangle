dde68f813c6f22d4054aae4639b54ae90a8eef81
Ruthwik <ruthwik34@pop-os.localdomain>
2021-06-12 10:52:18 +0530

    add migration file

---
+++ backend/team_app/db_script/online_class_join_all_task.py
@@ -0,0 +1,34 @@
+def online_class_join_all_task(apps, schema_editor):
+    Module = apps.get_model('team_app', 'Module')
+    Task = apps.get_model('team_app', 'Task')
+    EmployeePermission = apps.get_model('employee_app', 'EmployeePermission')
+
+    online_classes_module = Module.objects.get(path='online_classes')
+
+    # add_account = Task.objects.get(parentModule=online_classes_module, path='add_account')
+    # add_account.title = 'Add Zoom Account'
+    # join_all=Task.objects.get(parentModule=online_classes_module,path='join_all')
+    # join_all.title='Join All Classes'
+
+    # attendance_report_task = Task.objects.create(
+    #     parentModule=online_classes_module,
+    #     path='attendance_report',
+    #     title='Attendance Report',
+    #     orderNumber=5,
+    #     parentBoard=None
+    # )
+    join_all_task=Task.objects.create(
+        parentModule=online_classes_module,
+        path='join_all',
+        title='Join All Classes',
+        orderNumber=6,
+        parentBoard=None
+    )
+
+    # Add the employeePermission
+    for employee_permission in EmployeePermission.objects.filter(parentTask__path='assign_task'):
+        EmployeePermission.objects.create(
+                parentTask=join_all_task,
+                parentEmployee=employee_permission.parentEmployee
+            )
+
