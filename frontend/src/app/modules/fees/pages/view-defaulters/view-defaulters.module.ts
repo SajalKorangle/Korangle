4435766095daf75ea69dfcb793cff5aa7f4ea754
Ruthwik <ruthwik34@pop-os.localdomain>
2021-06-17 11:24:54 +0530

    format changes for module file

--- frontend/src/app/modules/fees/pages/view-defaulters/view-defaulters.module.ts
+++ frontend/src/app/modules/fees/pages/view-defaulters/view-defaulters.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material';
 import { ViewDefaultersRoutingModule } from './view-defaulters.routing';
-import { ComponentsModule } from "../../../../components/components.module";
-import { ViewDefaultersComponent } from "./view-defaulters.component";
+import { ComponentsModule } from '../../../../components/components.module';
+import { ViewDefaultersComponent } from './view-defaulters.component';
 import { MatPaginatorModule } from '@angular/material';
 
 
@@ -10,7 +10,6 @@ import { MatPaginatorModule } from '@angular/material';
     declarations: [ViewDefaultersComponent],
 
     imports: [ViewDefaultersRoutingModule, ComponentsModule, MatTableModule, MatPaginatorModule],
-
     exports: [],
     providers: [],
     bootstrap: [ViewDefaultersComponent],
