06f7678fa40df92188c3ef4ac261a2c6af673931
Aditya Kumar <adityakumar@Adityas-MacBook-Air.local>
2021-06-18 11:41:51 +0530

    fee breakup added in frontend dialog

--- frontend/src/app/modules/parent/pages/view-fee/components/payment-dailog/payment-dailog.component.ts
+++ frontend/src/app/modules/parent/pages/view-fee/components/payment-dailog/payment-dailog.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ViewFeeComponent } from './../../view-fee.component';
+import { SubFeeReceipt } from '@services/modules/fees/models/sub-fee-receipt';
+import { StudentFee } from '@services/modules/fees/models/student-fee';
+import { Student } from '@services/modules/student/models/student';
+import { Session } from '@services/modules/school/models/session';
+
 @Component({
   selector: 'app-payment-dailog',
   templateUrl: './payment-dailog.component.html',
@@ -9,6 +14,7 @@ import { ViewFeeComponent } from './../../view-fee.component';
 export class PaymentDailogComponent implements OnInit {
 
   amountMappedByStudntId: { [key: number]: number; } = {};
+  newSubFeeReceiptListMappedByStudntId: { [key: number]: Array<Partial<SubFeeReceipt>>; } = {};
 
   constructor(public dialogRef: MatDialogRef<PaymentDailogComponent>, @Inject(MAT_DIALOG_DATA) public data: {
     vm: ViewFeeComponent;
@@ -17,10 +23,12 @@ export class PaymentDailogComponent implements OnInit {
   ngOnInit() {
     this.data.vm.selectedStudentList.forEach(student => { // initilizing payment amount to 0
       this.amountMappedByStudntId[student.id] = 0;
+      this.newSubFeeReceiptListMappedByStudntId[student.id] = [];
     });
+    console.log('payment Dialog: ', this);
   }
 
-  amountError(student) {
+  amountError(student: Student) {
     const amountErrorHandler = () => {  // callback error checking function, checkf if amount exceeds the maximum amount
       if (this.amountMappedByStudntId[student.id] < 0)
         return true;
@@ -29,6 +37,114 @@ export class PaymentDailogComponent implements OnInit {
       return false;
     };
     return amountErrorHandler;
+  }
+
+  getOverallTotalPaymntAmount(): number {
+    return Object.values(this.amountMappedByStudntId).reduce((acc, next) => acc + next, 0);
+  }
+
+  handleOverallPaymentChange(student: Student): void {
+    if (this.amountError(student)())
+      return;
+    let paymentLeft = this.amountMappedByStudntId[student.id];
+    this.newSubFeeReceiptListMappedByStudntId[student.id] = []; // start with empty
+
+    this.data.vm.sessionList.forEach((session: Session) => {
+      this.data.vm.installmentList.forEach((installment) => {
+        this.data.vm.getFilteredStudentFeeListBySession(student, session).forEach((studentFee: StudentFee) => {
+          let installmentLateFeesDue = this.data.vm.getStudentFeeInstallmentLateFeesDue(studentFee, installment);
+          if (installmentLateFeesDue > 0) {
+            if (paymentLeft > installmentLateFeesDue) {
+              this.handleStudentFeeInstallmentLateFeePaymentChange(studentFee, installment, installmentLateFeesDue);
+              paymentLeft -= installmentLateFeesDue;
+            } else {
+              this.handleStudentFeeInstallmentLateFeePaymentChange(studentFee, installment, paymentLeft);
+              paymentLeft = 0;
+            }
+          }
+          let installmentFeesDue = this.data.vm.getStudentFeeInstallmentFeesDue(studentFee, installment);
+          if (installmentFeesDue > 0) {
+            if (paymentLeft > installmentFeesDue) {
+              this.handleStudentFeeInstallmentPaymentChange(studentFee, installment, installmentFeesDue);
+              paymentLeft -= installmentFeesDue;
+            } else {
+              this.handleStudentFeeInstallmentPaymentChange(studentFee, installment, paymentLeft);
+              paymentLeft = 0;
+            }
+          }
+        });
+      });
+    });
+  }
+
+  handleStudentFeeInstallmentLateFeePaymentChange(studentFee: StudentFee, installment: string, payment: number): void {
+    if (payment == 0)
+      return;
+    let subFeeReceipt = this.newSubFeeReceiptListMappedByStudntId[studentFee.parentStudent].find((subFeeReceipt) => {
+      return subFeeReceipt.parentStudentFee == studentFee.id;
+    });
+
+    if (subFeeReceipt) {
+      subFeeReceipt[installment + 'LateFee'] = payment;
+
+    } else {
+      this.createNewSubFeeReceipt(studentFee, installment + 'LateFee', payment);
+    }
+  }
+
+  createNewSubFeeReceipt(studentFee: StudentFee, installment: any, payment: any): void {
+    let subFeeReceipt = new SubFeeReceipt();
+    subFeeReceipt.parentStudentFee = studentFee.id;
+    subFeeReceipt.parentFeeType = studentFee.parentFeeType;
+    subFeeReceipt.parentSession = studentFee.parentSession;
+    subFeeReceipt.isAnnually = studentFee.isAnnually;
+    subFeeReceipt[installment] = payment;
+    this.newSubFeeReceiptListMappedByStudntId[studentFee.parentStudent].push(subFeeReceipt);
+  }
+
+  handleStudentFeeInstallmentPaymentChange(studentFee: StudentFee, installment: string, payment: number): void {
+    if (payment == 0)
+      return;
+    let subFeeReceipt = this.newSubFeeReceiptListMappedByStudntId[studentFee.parentStudent].find((subFeeReceipt) => {
+      return subFeeReceipt.parentStudentFee == studentFee.id;
+    });
+
+    if (subFeeReceipt) {
+      subFeeReceipt[installment + 'Amount'] = payment;
+    } else if (payment > 0) {
+      this.createNewSubFeeReceipt(studentFee, installment + 'Amount', payment);
+    }
+  }
+
+  getNewSubFeeReceiptTotalLateFee(student): number {
+    let lateFee = 0;
+
+    const subFeeReceiptList = this.newSubFeeReceiptListMappedByStudntId[student.id];
+    subFeeReceiptList.forEach(subFeeReceipt => {
+      this.data.vm.installmentList.forEach(installment => {
+        if (subFeeReceipt[installment + 'LateFee'])
+          lateFee += subFeeReceipt[installment + 'LateFee'];
+      });
+    });
+    return lateFee;
+  }
+
+  getFeeTypeFromSubFeeReceipt(subFeeReceipt: SubFeeReceipt) {
+    return this.data.vm.feeTypeList.find(feeType => feeType.id == subFeeReceipt.parentFeeType);
+  }
+
+  getTotalInstallmentAmount(subFeeReceipt: SubFeeReceipt) {
+    let totalAmount = 0;
+
+    this.data.vm.installmentList.forEach(installment => {
+      if (subFeeReceipt[installment + 'Amount'])
+        totalAmount += subFeeReceipt[installment + 'Amount'];
+    });
+    return totalAmount;
+  }
+
+  getSessionFilteredSubFeeReceiptList(student, session: Session): Array<Partial<SubFeeReceipt>> {
+    return this.newSubFeeReceiptListMappedByStudntId[student.id].filter(subFeeReceipt => subFeeReceipt.parentSession == session.id);
   }
 
   // apply() {
