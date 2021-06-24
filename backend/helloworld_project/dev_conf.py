6520b5849f3b6c4512bb32f70c4f10eac1c0257c
Aditya Kumar <adityakumar@Adityas-MacBook-Air.local>
2021-06-24 19:48:53 +0530

    prod dev config update

--- backend/helloworld_project/dev_conf.py
+++ backend/helloworld_project/dev_conf.py
@@ -49,3 +49,13 @@ AWS_SECRET_ACCESS_KEY = 'oLYa8rZF9O3DwW/l4HBCFqF5PuEEJxCX0EkUI1gk'
 AWS_STORAGE_BUCKET_NAME = 'korangletesting'
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_SECURE_URLS = False # to use http instead of https
+
+
+################## PAYMENT_GATEWAY ##################
+
+CASHFREE_APP_ID = os.environ['CASHFREE_APP_ID']
+CASHFREE_SECRET_KEY = os.environ['CASHFREE_SECRET_KEY']
+CASHFREE_CLIENT_ID = os.environ['CLIENT_ID']
+CASHFREE_CLIENT_SECRET = os.environ['CLIENT_SECRET']
+
+######################################################
