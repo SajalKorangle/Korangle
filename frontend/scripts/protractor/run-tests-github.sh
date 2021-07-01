682f96b9e6a6b1cfd6fafe310cb1f6ff6feb7011
SyedAnas26 <ahmed.anas069@gmail.com>
2021-07-01 14:29:04 +0530

    resolving

--- frontend/scripts/protractor/run-tests-github.sh
+++ frontend/scripts/protractor/run-tests-github.sh
@@ -7,9 +7,8 @@ mkdir -p tmp
 npm start &
 while ! echo exit | nc localhost 4200; do sleep 10; done
 
-
+node ./node_modules/protractor/node_modules/webdriver-manager/bin/webdriver-manager update
 ./node_modules/.bin/protractor tests/protractor/protractor.conf.js
-
 if [ "$?" -ne 0 ];
 then
     echo "Protractor Test failed"
