16e12fda88c8e1dfb52ed187ebac191673064911
Aditya Kumar <kumar.93@iitj.ac.in>
2021-07-04 13:49:11 +0530

    refnd update

--- backend/payment_app/admin.py
+++ backend/payment_app/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Order
 
 
-class Project(admin.ModelAdmin):
+class OrderAdminTabular(admin.ModelAdmin):
     model = Order
     list_display = ('orderId', 'status', 'dateTime')
     list_filter = ('status', 'dateTime')
 
-admin.site.register(Order, Project)
+admin.site.register(Order, OrderAdminTabular)
--- backend/payment_app/jobs/daily/cashfree_refund.py
+++ backend/payment_app/jobs/daily/cashfree_refund.py
@@ -1,30 +1,29 @@
 from django_extensions.management.jobs import DailyJob
-from datetime import datetime, timedelta
-import pytz
 from payment_app.models import Order
 from payment_app.cashfree.cashfree import getOrderStatus
 
 class Job(DailyJob): # Should be run between 3am to 5am
     help = "Cashfree Refund job."
 
+
     def execute(self):
         print('Processing pending cashfree jobs...')
-        now =datetime.now()
-        timezone = pytz.timezone("Asia/Kolkata")
-        fromDateTime = timezone.localize( datetime(now.year, now.month, now.day) - timedelta(days=1))
-        toCheckOrderList = Order.objects.filter(dateTime__gte=fromDateTime, status = 'Pending')
+        toCheckOrderList = Order.objects.filter(status = 'Pending')
         for orderInstance in toCheckOrderList:
             try:
                 cashfreeOrder = getOrderStatus(orderInstance.orderId)
                 print(cashfreeOrder)
+                if cashfreeOrder['status'] == 'ERROR' and cashfreeOrder['reason'] == 'Order Id does not exist':
+                    orderInstance.status = 'Failed'
+                    orderInstance.save()
+                    continue
                 assert cashfreeOrder['orderStatus'] != "ACTIVE"
-            except:
+            except: # continue in case of any bug or order is active or order is nor registered in cashfree
                 continue
-            print(cashfreeOrder)
             if(cashfreeOrder['txStatus']=='SUCCESS'):
+                orderInstance.status = 'Completed'
+                orderInstance.referenceId = cashfreeOrder['referenceId']
                 try:
-                    orderInstance.status = 'Completed'
-                    orderInstance.referenceId = cashfreeOrder['referenceId']
                     orderInstance.save()
                 except:
                     orderInstance.status = 'Refund Pending'
