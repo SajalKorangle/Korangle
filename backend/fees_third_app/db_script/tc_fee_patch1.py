f5414e65677d3289d748447d6871357d6f1be67e
Aditya Kumar <kumar.93@iitj.ac.in>
2021-07-15 16:23:20 +0530

    migration to set tc fee to 0

---
+++ backend/fees_third_app/db_script/tc_fee_patch1.py
@@ -0,0 +1,6 @@
+def set_tc_school_fee_rule_to_zero(apps, schema_editor):
+    SchoolFeeRule = apps.get_model('fees_third_app', 'SchoolFeeRule')
+    tc_school_fee_rule_list = SchoolFeeRule.objects.filter(name='TC FEE(Software Generated)')
+    for school_fee_rule in tc_school_fee_rule_list:
+        school_fee_rule.aprilAmount = None
+        school_fee_rule.save()
