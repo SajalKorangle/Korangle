94d672da3827aacdd0b6cc2d0e5dd81a629b622d
Aditya Kumar <kumar.93@iitj.ac.in>
2021-07-17 19:39:59 +0530

    migrations folder fix

--- backend/initilize_migration_folder.sh
+++ backend/initilize_migration_folder.sh
@@ -4,3 +4,9 @@ do
     mkdir "${app}migrations/"
     touch "${app}migrations/__init__.py"
 done
+
+#Report Card App
+mkdir report_card_app/report_card_cbse_app/migrations
+touch report_card_app/report_card_cbse_app/migrations/__init__.py
+mkdir report_card_app/report_card_mp_board_app/migrations
+touch report_card_app/report_card_mp_board_app/migrations/__init__.py
