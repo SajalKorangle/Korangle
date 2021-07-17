18fb5f98fed485f0597c73fe959c83d8a86fc0eb
Aditya Kumar <kumar.93@iitj.ac.in>
2021-07-17 19:37:34 +0530

    backup fix

--- backend/backup_migrations.sh
+++ backend/backup_migrations.sh
@@ -8,7 +8,9 @@ do
 done
 
 # RC Migrations
+mkdir old-migrations/report_card_app/report_card_cbse_app/
+mkdir old-migrations/report_card_app/report_card_mp_board_app/
 mv report_card_app/report_card_cbse_app/migrations old-migrations/report_card_app/report_card_cbse_app/migrations
 mv report_card_app/report_card_cbse_app/db_script old-migrations/report_card_app/report_card_cbse_app/db_script
-mv report_card_app/report_card_cbse_app/migrations old-migrations/report_card_app/report_card_cbse_app/migrations
-mv report_card_app/report_card_cbse_app/db_script old-migrations/report_card_app/report_card_cbse_app/db_script
+mv report_card_app/report_card_mp_board_app/migrations old-migrations/report_card_app/report_card_mp_board_app/migrations
+mv report_card_app/report_card_mp_board_app/db_script old-migrations/report_card_app/report_card_mp_board_app/db_script
