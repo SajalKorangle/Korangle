2346f211a0e0488ccf528402f1f204eeaee52a0f
Aditya Kumar <kumar.93@iitj.ac.in>
2021-07-19 08:51:35 +0530

    class filter added in tc and migration

--- backend/fees_third_app/db_script/tc_fee_patch1.py
+++ backend/fees_third_app/db_script/tc_fee_patch1.py
@@ -3,4 +3,5 @@ def set_tc_school_fee_rule_to_zero(apps, schema_editor):
     tc_school_fee_rule_list = SchoolFeeRule.objects.filter(name='TC FEE(Software Generated)')
     for school_fee_rule in tc_school_fee_rule_list:
         school_fee_rule.aprilAmount = None
+        school_fee_rule.isClassFilter = True
         school_fee_rule.save()
