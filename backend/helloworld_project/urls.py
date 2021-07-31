41ffcfe38def937eca34c0a1316e0a513990db57
Aditya Kumar <kumar.93@iitj.ac.in>
2021-07-31 17:24:09 +0530

    added data migration to move rc and tc data to my design

--- backend/helloworld_project/urls.py
+++ backend/helloworld_project/urls.py
@@ -40,6 +40,6 @@ urlpatterns = [
     url(r'^'+api_version+'accounts/', include('accounts_app.urls')),
     url(r'^'+api_version+'event-gallery/', include('event_gallery_app.urls')),
     url(r'^'+api_version+'online-class/', include('online_classes_app.urls')),
-    url(r'^'+api_version+'my-design/', include('my-design.urls')),
+    url(r'^'+api_version+'my-design/', include('my_design.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
---
+++ backend/my_design/db_script/migrate_layouts.py
@@ -0,0 +1,59 @@
+def migrate_rc_layout_data(apps, schema_editor):
+    ReportCardLayoutNew = apps.get_model('report_card_app', 'ReportCardLayoutNew')
+    LayoutSharingReportCard = apps.get_model('report_card_app', 'LayoutSharing')
+    RcImageAssets = apps.get_model('report_card_app', 'ImageAssets')
+
+    Layout = apps.get_model('my_design', 'Layout')
+    LayoutSharing = apps.get_model('my_design', 'LayoutSharing')
+    ImageAssets = apps.get_model('my_design', 'ImageAssets')
+
+    for rc_layout in ReportCardLayoutNew.objects.all():
+        newLayout = Layout.objects.create(
+            type = 'REPORT CARD',
+            parentSchool = rc_layout.parentSchool,
+            name = rc_layout.name,
+            thumbnail = rc_layout.thumbnail,
+            publiclyShared= rc_layout.publiclyShared,
+            content =rc_layout.content
+        )
+
+        for rc_layout_sharing in LayoutSharingReportCard.objects.filter(parentLayout=rc_layout):
+            LayoutSharing.objects.craete(
+                parentLayout = newLayout,
+                parentSchool = rc_layout_sharing.parentSchool
+            )
+
+    for image_assets in RcImageAssets.objects.all():
+        ImageAssets.objects.create(
+            image = image_assets.image,
+        )
+
+def migrate_tc_layout_data(apps, schema_editor):
+    TcLayout = apps.get_model('tc_app', 'TCLayout')
+    LayoutSharingTc = apps.get_model('tc_app', 'TCLayoutSharing')
+    TcImageAssets = apps.get_model('tc_app', 'TCImageAssets')
+
+    Layout = apps.get_model('my_design', 'Layout')
+    LayoutSharing = apps.get_model('my_design', 'LayoutSharing')
+    ImageAssets = apps.get_model('my_design', 'ImageAssets')
+
+    for tc_layout in TcLayout.objects.all():
+        newLayout = Layout.objects.create(
+            type = 'REPORT CARD',
+            parentSchool = tc_layout.parentSchool,
+            name = tc_layout.name,
+            thumbnail = tc_layout.thumbnail,
+            publiclyShared= tc_layout.publiclyShared,
+            content =tc_layout.content
+        )
+
+        for rc_layout_sharing in LayoutSharingTc.objects.filter(parentLayout=tc_layout):
+            LayoutSharing.objects.craete(
+                parentLayout = newLayout,
+                parentSchool = rc_layout_sharing.parentSchool
+            )
+
+    for image_assets in TcImageAssets.objects.all():
+        ImageAssets.objects.create(
+            image = image_assets.image,
+        )
