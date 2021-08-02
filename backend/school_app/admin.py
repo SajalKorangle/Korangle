169d0c81d547e4c66f935fc3a9c67c669fa0ae9f
harshal <harshalagrawal03@gmail.com>
2021-08-02 16:26:43 +0530

    added date of expiration

--- backend/school_app/admin.py
+++ backend/school_app/admin.py
@@ -14,14 +14,40 @@ from examination_app.models import StudentTest
 from employee_app.models import Employee
 from homework_app.models import HomeworkQuestion
 from tutorial_app.models import Tutorial
+from datetime import date
+from django.db.models import Q
 
 admin.site.register(Session)
+
+
+from django.utils.translation import gettext_lazy as _
+
+
+class ActiveListFilter(admin.SimpleListFilter):
+    title = _('Active')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'active'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('Yes', _('Yes')),
+            ('No', _('No')),
+        )
+
+    def queryset(self, request, queryset):
+
+        if self.value() == 'No':
+            return queryset.filter(Q(expired=True) | Q(dateOfExpiry__lt=date.today()))
+        if self.value() == 'Yes':
+            return queryset.filter(Q(dateOfExpiry=None) | Q(dateOfExpiry__gte=date.today()), expired=False)
+
 
 @admin.register(School)
 class SchoolAdmin(admin.ModelAdmin):
     search_fields = ('printName', 'id')
-    list_display = ('Name', 'Stud','Empl', 'Fee_Receipts', 'Subj', 'Stud_Marks', 'Hwk_Ques', 'Tuts', 'expired', 'S_2020_21', 'S_2019_20', 'S_2018_19', 'S_2017_18')
-    list_filter = ('expired',)
+    list_display = ('Name', 'Stud','Empl', 'Fee_Receipts', 'Subj', 'Stud_Marks', 'Hwk_Ques', 'Tuts', 'Active', 'S_2020_21', 'S_2019_20', 'S_2018_19', 'S_2017_18')
+    list_filter = (ActiveListFilter,)
 
     def Name(self,obj):
         return str(obj.pk) + ' - ' + obj.printName
@@ -46,6 +72,16 @@ class SchoolAdmin(admin.ModelAdmin):
 
     def Tuts(self, obj):
         return str(Tutorial.objects.filter(parentClassSubject__parentSchool_id=obj.pk, parentClassSubject__parentSession__name='2021-04-01').count())
+
+    def Active(self, obj):
+        if obj.expired:
+            return 'No'
+        if obj.dateOfExpiry is None:
+            return 'Yes'
+        if obj.dateOfExpiry < date.today():
+            return 'No'
+        else:
+            return 'Yes'
 
     def S_2020_21(self, obj):
         return str(StudentSection.objects.filter(parentStudent__parentSchool_id=obj.pk, parentSession__name='Session 2020-21').count())
