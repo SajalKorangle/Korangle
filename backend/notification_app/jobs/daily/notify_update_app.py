df602720aa1ca8a264378ae9f5f272a0877a87ca
SyedAnas26 <ahmed.anas069@gmail.com>
2021-08-04 17:43:14 +0530

    shifted script and reused query in update_app

--- backend/notification_app/jobs/daily/notify_update_app.py
+++ backend/notification_app/jobs/daily/notify_update_app.py
@@ -11,10 +11,12 @@ class Job(DailyJob):  # Should run at 2.30 am
     def execute(self):
         gcm_devices = GCMDevice.objects.filter(active=True)
         no_of_iterations = math.ceil(len(gcm_devices) / 50)
+        print(no_of_iterations)
         starting_range = 0
         for x in range(0, no_of_iterations):
             sliced_query = gcm_devices[starting_range:starting_range + 50]  # slicing for every 50 numbers
-            sliced_devices = GCMDevice.objects.filter(id__in=[s.id for s in sliced_query])
+            sliced_devices = gcm_devices.filter(id__in=[s.id for s in sliced_query])
+            print(sliced_devices)
             sliced_devices.send_message(None, extra={"data": "UPDATE"})
             starting_range += 50  # increasing the starting range
             time.sleep(120)
