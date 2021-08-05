46717c7022b8521029f3175f660ba13783bf359c
harshal <harshalagrawal03@gmail.com>
2021-08-05 16:04:50 +0530

    changed the time in the commented line for crontab as the server is in U.S. Virginia

--- backend/cron_job_scripts/update_android_app.sh
+++ backend/cron_job_scripts/update_android_app.sh
@@ -1,5 +1,5 @@
 # paste the following line in crontab file to run the script everyday at 2.30 am 
-# 30 2 * * * sh /path/to/korangle/backend/update_app.sh
+# 30 21 * * * sh /path/to/korangle/backend/update_app.sh
 
 # changing directory is required to backend to access the database
 cd ~/code/korangle/backend
