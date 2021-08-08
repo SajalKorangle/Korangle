62a941a199e6018cc513af7b9ec67b47e9c778aa
Aditya Kumar <kumar.93@iitj.ac.in>
2021-08-09 02:07:49 +0530

    added no input

--- frontend/tests/protractor/src/backend-server.ts
+++ frontend/tests/protractor/src/backend-server.ts
@@ -4,13 +4,13 @@ const { exec, execSync } = require('child_process');
 // We are assuming that the tests are run from frontend folder.
 
 export function startBackendServer(fixtureFileList: any) {
-    const command = '../backend/manage.py testserver --nomigrations `find ../backend/fixtures/constants/ -path "*.json"` ';
+    const command = '../backend/manage.py testserver --no-input --nomigrations `find ../backend/fixtures/constants/ -path "*.json"` ';
     console.log(command + fixtureFileList);
     stopBackendServer();
     exec(command + fixtureFileList,
         (err, stdout, stderr) => {
-        // console.log('Backend server ended');
-    });
+            // console.log('Backend server ended');
+        });
 }
 
 export function stopBackendServer() {
