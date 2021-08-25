1922221d0fbef7df9523e5a82dba67d5116d8b48
SyedAnas26 <ahmed.anas069@gmail.com>
2021-08-25 20:43:12 +0530

    changed the notification title to school

--- backend/notification_app/business/send_notification.py
+++ backend/notification_app/business/send_notification.py
@@ -1,12 +1,10 @@
 from push_notifications.models import GCMDevice
-from sms_app.models import SMSIdSchool, SMSId
 
 
 def send_notification(data):
-    sms_id_school = SMSIdSchool.objects.filter(parentSchool_id=data.parentSchool.id)
-    if sms_id_school.exists():
-        sms_id = SMSId.objects.get(id=sms_id_school[0].parentSMSId.id).smsId
-    else:
-        sms_id = 'KORNGL'
+
+    school_name = 'KORANGLE'
+    if data.parentSchool:
+        school_name = data.parentSchool.name
     fcm_devices = GCMDevice.objects.filter(user=data.parentUser)
-    fcm_devices.send_message(data.content, title=sms_id)
+    fcm_devices.send_message(data.content, title=school_name)
