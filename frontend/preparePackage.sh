f9a0264d5139de7f7d6bbf1739276f57d26abd7d
Aditya Kumar <kumar.93@iitj.ac.in>
2021-08-28 14:11:16 +0530

    frontend deployment wokflow

--- frontend/preparePackage.sh
+++ frontend/preparePackage.sh
@@ -1,6 +1,7 @@
 rm -rf korangle.zip korangle/
 node --max_old_space_size=8192 ./node_modules/@angular/cli/bin/ng build --prod --build-optimizer
 mv dist/ korangle
-npm run post-build $1
+npm run post-build mobile
 cp version.json korangle/version.json
 zip -r korangle.zip korangle/
+mv korangle.zip ./korangle/
