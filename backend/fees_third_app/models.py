99b30245818371713392867db9b8977fa63f67a9
Aditya Kumar <kumar.93@iitj.ac.in>
2021-09-14 17:35:42 +0530

    answers

--- backend/fees_third_app/models.py
+++ backend/fees_third_app/models.py
@@ -24,12 +24,10 @@ from student_app.models import Student
 
 from employee_app.models import Employee
 
-from django.contrib.auth.models import User
-
-from accounts_app.models import Accounts, Transaction, AccountSession, TransactionAccountDetails
+from accounts_app.models import Transaction, AccountSession
 from payment_app.models import Order
 
-from datetime import date, datetime
+from datetime import datetime
 from accounts_app.models import Transaction, AccountSession
 
 from django.dispatch import receiver
