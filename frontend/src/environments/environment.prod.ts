f807bdbddd98096e81f413d68e2d6a0ccf88cc15
ABX9801 <you@example.coanurag.bhardwaj9801@gmail.com>
2021-10-14 17:02:43 +0530

    reverted unnecessary changes

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // This file will be override by environment.prod.ts during production build
 
 export const environment = {
-    production: false,
+    production: true,
     versionCheckURL: 'www.korangle.com/version.json',
     DJANGO_SERVER: 'http://localhost:8000', // normal debugging only (localhost)
     api_version: '/v2.0',
