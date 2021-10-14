aa28d9d7afde152a81a2990207c93fea150e5814
ABX9801 <you@example.coanurag.bhardwaj9801@gmail.com>
2021-10-14 11:46:38 +0530

    fixed 2 remark bars in collectfees

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 // This file will be override by environment.prod.ts during production build
 
 export const environment = {
-    production: true,
+    production: false,
     versionCheckURL: 'www.korangle.com/version.json',
-    DJANGO_SERVER: 'https://www.korangle.com:8443',
+    DJANGO_SERVER: 'http://localhost:8000', // normal debugging only (localhost)
     api_version: '/v2.0',
 };
