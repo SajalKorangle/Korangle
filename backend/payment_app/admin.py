0a9238317a091ed3e8a12aa97fd481d415f4e00b
Aditya Kumar <kumar.93@iitj.ac.in>
2021-10-15 11:01:49 +0530

    migrations merge and readonly field in order admin

--- backend/payment_app/admin.py
+++ backend/payment_app/admin.py
@@ -6,8 +6,14 @@ class OrderAdminTabular(admin.ModelAdmin):
     model = Order
     list_display = ('orderId', 'status', 'dateTime')
     list_filter = ('status', 'dateTime')
+    readonly_fields = [field.name for field in Order._meta.concrete_fields if field.name != 'status']
+
+
+class SchoolMerchantAccountAdminTabular(admin.ModelAdmin):
+    list_display = ('parentSchool', 'vendorId')
+    readonly_fields = [field.name for field in SchoolMerchantAccount._meta.concrete_fields]
 
 
 admin.site.register(Order, OrderAdminTabular)
-admin.site.register(SchoolMerchantAccount)
+admin.site.register(SchoolMerchantAccount, SchoolMerchantAccountAdminTabular)
 admin.site.register(CashfreeDailyJobsReport)
