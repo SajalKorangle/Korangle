36dc2cabe36070877fac9f18543fa6200b429fa7
Aditya Kumar <kumar.93@iitj.ac.in>
2021-10-28 19:20:39 +0530

    backend updaed for generic design models and added permissions

--- backend/generic_design_app/models.py
+++ backend/generic_design_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from school_app.model.models import School
 from django.utils.timezone import now
+from common.common import BasePermission
+from django.db.models import Q
 
 
 def upload_thumbnail_to(instance, filename):
@@ -19,6 +21,15 @@ class Layout(models.Model):
     publiclyShared = models.BooleanField(default=False)
     content = models.JSONField()    # Contains the JSON content for the layout
 
+    class Permissions(BasePermission):
+
+        RelationsToSchool = ['parentSchool__id']
+
+        def getPermittedQuerySet(self, activeSchoolId, activeStudentIdList):
+            permittedQuerySet = super().getPermittedQuerySet(activeSchoolId, activeStudentIdList)
+            sharedLayoutList = LayoutShare.objects.filter(parentSchoolSharedWith=activeSchoolId).values_list('parentLayout', flat=True)
+            return permittedQuerySet | Q(id__in=sharedLayoutList)
+
     class Meta:
         unique_together = ('type', 'parentSchool', 'name')
 
@@ -26,6 +37,13 @@ class Layout(models.Model):
 class LayoutShare(models.Model):
     parentSchoolSharedWith = models.ForeignKey(School, on_delete=models.CASCADE, related_name='layoutSharedWithMeList')
     parentLayout = models.ForeignKey(Layout, on_delete=models.CASCADE, related_name='layoutShareList')
+
+    class Permissions(BasePermission):
+        RelationsToSchool = ['parentLayout__parentSchool__id']
+
+        def getPermittedQuerySet(self, activeSchoolId, activeStudentIdList):
+            permittedQuerySet = super().getPermittedQuerySet(activeSchoolId, activeStudentIdList)
+            return permittedQuerySet | Q(parentSchoolSharedWith=activeSchoolId)
 
     class Meta:
         unique_together = ('parentSchoolSharedWith', 'parentLayout')
--- backend/generic_design_app/urls.py
+++ backend/generic_design_app/urls.py
@@ -7,11 +7,4 @@ from . import views
 
 urlpatterns += [
 
-    # url(r'^layout/batch', views.LayoutListView.as_view()),
-    # url(r'^layout-sharing/batch', views.LayoutSharingListView.as_view()),
-    # url(r'^layout-sharing', views.LayoutSharingView.as_view()),
-    # url(r'^layout', views.LayoutView.as_view()),
-    # url(r'^image-assets/batch', views.ImageAssetsListView.as_view()),
-    # url(r'^image-assets', views.ImageAssetsView.as_view()),
-
 ]
