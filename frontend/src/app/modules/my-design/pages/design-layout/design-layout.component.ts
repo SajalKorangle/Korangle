31884a66808d3a5281495a98064fea84829f764b
Aditya Kumar <kumar.93@iitj.ac.in>
2021-10-30 23:04:34 +0530

    removed legecy services

--- frontend/src/app/modules/my-design/pages/design-layout/design-layout.component.ts
+++ frontend/src/app/modules/my-design/pages/design-layout/design-layout.component.ts
@@ -5,15 +5,6 @@ import { DataStorage } from '../../../../classes/data-storage';
 
 // Backend Services
 import { GenericService } from '@services/generic/generic-service';
-import { MyDesignService } from '@services/modules/generic-design/generic-design.service';
-import { ReportCardService } from '@services/modules/report-card/report-card.service';
-import { StudentService } from '@services/modules/student/student.service';
-import { ClassService } from '@services/modules/class/class.service';
-import { ExaminationService } from '@services/modules/examination/examination.service';
-import { SubjectService } from '@services/modules/subject/subject.service';
-import { SchoolService } from '@services/modules/school/school.service';
-import { AttendanceService } from '@services/modules/attendance/attendance.service';
-import { GradeService } from '@services/modules/grade/grade.service';
 
 // For Types
 import { User } from '@classes/user';
@@ -29,15 +20,7 @@ import { DesignLayoutServiceAdapter } from './design-layout.service.adapter';
     styleUrls: ['./design-layout.component.css'],
     providers: [
         GenericService,
-        MyDesignService,
-        ReportCardService,
-        StudentService,
-        ClassService,
-        ExaminationService,
-        SubjectService,
-        SchoolService,
-        AttendanceService,
-        GradeService,
+
         MatDialog,
     ],
 })
@@ -56,15 +39,6 @@ export class DesignLayoutComponent implements OnInit {
 
     constructor(
         public genericService: GenericService,
-        public myDesignService: MyDesignService,
-        public reportCardService: ReportCardService,
-        public studentService: StudentService,
-        public classService: ClassService,
-        public examinationService: ExaminationService,
-        public subjectService: SubjectService,
-        public schoolService: SchoolService,
-        public attendanceService: AttendanceService,
-        public gradeService: GradeService,
         public dialog: MatDialog
     ) { }
 
