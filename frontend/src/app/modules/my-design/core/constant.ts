7ad6fa6165949b54915da29e04cfc5eccf5f7983
Aditya Kumar <kumar.93@iitj.ac.in>
2021-11-08 17:46:14 +0530

    code refactoring, get Data to save update base layer functions fixed

--- frontend/src/app/modules/my-design/core/constant.ts
+++ frontend/src/app/modules/my-design/core/constant.ts
@@ -1,6 +1,11 @@
 import { STANDARD_PAGE_RESOLUTION_MAPPED_BY_NAME, PageResolution } from "./page-resolutions";
 
 export const DEFAULT_BACKGROUND_COLOR = '#ffffff'; // white
+export const DEFAULT_TEXT_COLOR = '#000000'; // black
+export const ACTIVE_LAYER_HIGHLIGHTER_LINE_WIDTH = 2; // in pixels
+export const ACTIVE_LAYER_HIGHLIGHTER_COLOR = 'cyan';
+export const PERMISSIBLE_CLICK_ERROR = 4;    // in pixels
+export const DEFAULT_IMAGE_URL = 'https://korangleplus.s3.amazonaws.com/assets/img/ef3f502028770e76bbeeeea68744c2c3.jpg';
 
 export interface LAYOUT_INTERFACE{
     [key: string]: any;
--- frontend/src/app/modules/my-design/core/layers/base.ts
+++ frontend/src/app/modules/my-design/core/layers/base.ts
@@ -3,17 +3,21 @@ import { PositionPanelComponent } from '@modules/my-design/core/components/panel
 import { SettingsPanelComponent } from '@modules/my-design/core/components/panels/settings-panel/settings-panel.component';
 // Panel List Ends
 
+import { PERMISSIBLE_CLICK_ERROR, ACTIVE_LAYER_HIGHLIGHTER_COLOR, ACTIVE_LAYER_HIGHLIGHTER_LINE_WIDTH } from '@modules/my-design/core/constant';
+
 // Constants Starts
-const DATA_SOURCE_TYPE = [    // used in all canvas layers
-    'N/A',  // no data source, constant element
-    'DATA'  // data source available, get data from the provided data source
-];
+const DATA_SOURCE_TYPE = {    // used in all canvas layers
+    constant: 'N/A',  // no data source, constant element
+    data: 'DATA'  // data source available, get data from the provided data source
+};
 
 // Constants Ends
 
 // Types Starts
-interface BaseCanvasAdapter {
+interface BaseCanvasAdapterInterface {
     pixelToMmFactor: number;
+    scheduleCanvasReDraw: (delay: number) => Promise<any>;
+    parameterList: any[];
 }
 
 type Class = { new(...args: any[]): any; };
@@ -23,7 +27,7 @@ type keyValue = { [key: string | number]: any; };
 
 // Property Decorators Starts
 
-function dataPropertyDecorator(targetPrototype: keyValue, propertyName: string) {   // to subscribe to property changes
+export function dataPropertyDecorator(targetPrototype: keyValue, propertyName: string) {   // to subscribe to property changes
     const targetClass: any = targetPrototype.valueOf().constructor;
     const maxId = targetClass.maxId;
     const defaultObj = new targetClass();
@@ -41,7 +45,7 @@ function dataPropertyDecorator(targetPrototype: keyValue, propertyName: string) 
     });
 }
 
-function designPropertyDecorator(targetPrototype: keyValue, propertyName: string) {   // to subscribe to property changes
+export function designPropertyDecorator(targetPrototype: keyValue, propertyName: string) {   // to subscribe to property changes
     const targetClass: any = targetPrototype.valueOf().constructor;
     const maxId = targetClass.maxId;
     const defaultObj = new targetClass();
@@ -66,7 +70,7 @@ export class BaseLayer {    // this layer is inherited by all canvas layers
     id: number = null;
     static maxID: number = 0;   // for auto incrementing id
 
-    ca: BaseCanvasAdapter;
+    ca: BaseCanvasAdapterInterface;
 
     @designPropertyDecorator error: boolean = false;
     @designPropertyDecorator x: number = 0;
@@ -81,21 +85,21 @@ export class BaseLayer {    // this layer is inherited by all canvas layers
     static panelList: Array<Class> = [PositionPanelComponent, SettingsPanelComponent];  // position right toolbar panel is present in all layers
     static toolBarList: string[] = [];     // change strings to component later
     isPositionLocked: boolean = false;
-    dataSourceType: string = 'N/A';
+    dataSourceType: string = DATA_SOURCE_TYPE.constant;
     source?: { [key: string]: any; };
 
-    constructor(ca: BaseCanvasAdapter) {
+    constructor(ca: BaseCanvasAdapterInterface) {
         this.ca = ca;
         BaseLayer.maxID += 1;
         this.id = BaseLayer.maxID;
     }
 
-    initilizeSelf(attributes: object): void { // initializes all class variables according to provided initial parameters data as object
+    initializeSelf(attributes: object): void { // initializes all class variables according to provided attribute object
         Object.entries(attributes).forEach(([key, value]) => this[key] = value);
         BaseLayer.maxID = Math.max(BaseLayer.maxID, this.id);   // always keeping static maxID maximum of all layers
-        if (this.dataSourceType == DATA_SOUCE_TYPE[1]) {
+        if (this.dataSourceType == DATA_SOURCE_TYPE.data) {
             // The dependence on htmlAdapter should be removed, once custom parameter handling is updated use paramter list insted of htmlAdapter
-            this.source = this.ca.vm.htmlAdapter.parameterList.find(el => el.key == this.source.key
+            this.source = this.ca.parameterList.find(el => el.key == this.source.key
                 && el.field.fieldStructureKey == this.source.field.fieldStructureKey);
             if (!this.source)
                 this.error = true;
@@ -103,29 +107,27 @@ export class BaseLayer {    // this layer is inherited by all canvas layers
     }
 
     updatePosition(dx = 0, dy = 0): void {
-        if (this.isLocked) {
+        if (this.isPositionLocked) 
             return;
-        }
-        else {
-            this.x += dx;
-            this.y += dy;
-        }
+ 
+        this.x += dx;
+        this.y += dy;
     }
 
     highlightLayer(ctx: CanvasRenderingContext2D): void {
         if (this.height && this.width) {
             ctx.strokeStyle = ACTIVE_LAYER_HIGHLIGHTER_COLOR;
             ctx.lineWidth = ACTIVE_LAYER_HIGHLIGHTER_LINE_WIDTH;
-            ctx.strokeRect(this.x - permissibleClickError / 4, this.y - permissibleClickError / 4,
-                this.width + permissibleClickError / 2, this.height + permissibleClickError / 2);
+            ctx.strokeRect(this.x - PERMISSIBLE_CLICK_ERROR / 4, this.y - PERMISSIBLE_CLICK_ERROR / 4,
+                this.width + PERMISSIBLE_CLICK_ERROR / 2, this.height + PERMISSIBLE_CLICK_ERROR / 2);
         }
     }
 
     isClicked(mouseX: number, mouseY: number, shiftKey: boolean = false): boolean {
-        return (mouseX > this.x - permissibleClickError
-            && mouseX < this.x + this.width + permissibleClickError
-            && mouseY > this.y - permissibleClickError
-            && mouseY < this.y + this.height + permissibleClickError);
+        return (mouseX > this.x - PERMISSIBLE_CLICK_ERROR
+            && mouseX < this.x + this.width + PERMISSIBLE_CLICK_ERROR
+            && mouseY > this.y - PERMISSIBLE_CLICK_ERROR
+            && mouseY < this.y + this.height + PERMISSIBLE_CLICK_ERROR);
     }
 
     getDataToSave(): { [object: string]: any; } {   // common data to be saved in database
@@ -133,11 +135,13 @@ export class BaseLayer {    // this layer is inherited by all canvas layers
             id: this.id,
             displayName: this.displayName,
             LAYER_TYPE: this.LAYER_TYPE,
-            x: this.x * this.ca.pixelTommFactor,  // converting pixels to mm
-            y: this.y * this.ca.pixelTommFactor,
+            x: this.x * this.ca.pixelToMmFactor,  // converting pixels to mm
+            y: this.y * this.ca.pixelToMmFactor,
             dataSourceType: this.dataSourceType,
-            isLocked: this.isLocked
+            isLocked: this.isPositionLocked
         };
+        if(this.dataSourceType == DATA_SOURCE_TYPE.data) 
+            Object.assign(savingData, {source: this.source});
         return savingData;
     }
 }
