834bc2d56bca690c6a9eaf5d0853925e24a83255
Aditya Kumar <kumar.93@iitj.ac.in>
2021-11-13 17:57:03 +0530

    removed payment app

--- backend/payment_app/cashfree/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from .views import SettlementsCycleListView, IFSCVerification, BankAccountVerification
-
-urlpatterns = [
-    url(r'^settlement-cycle', SettlementsCycleListView.as_view()),
-    url(r'^ifsc-verification', IFSCVerification.as_view()),
-    url(r'^bank-account-verification', BankAccountVerification.as_view()),
-]
