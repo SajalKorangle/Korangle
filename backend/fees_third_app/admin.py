c0bb4d3a539ae3fc7074705ea1aa18e1214d7186
Aditya Kumar <kumar.93@iitj.ac.in>
2021-11-13 17:44:47 +0530

    removed fee third app changes

--- backend/fees_third_app/admin.py
+++ backend/fees_third_app/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
-from django.db.models.base import Model
-from .models import FeeReceiptOrder
+
 # Register your models here.
-
-
-class FeeReceiptOrderAdmin(admin.ModelAdmin):
-    Model = FeeReceiptOrder
-    readonly_fields = [field.name for field in FeeReceiptOrder._meta.concrete_fields]
-
-
-admin.site.register(FeeReceiptOrder, FeeReceiptOrderAdmin)
--- backend/fees_third_app/urls.py
+++ backend/fees_third_app/urls.py
@@ -1,3 +1,4 @@
+
 from django.conf.urls import url
 
 urlpatterns = []
@@ -74,15 +75,10 @@ urlpatterns += [
 ]
 
 
+
 from .views import FeeSettingsView, FeeSettingsListView
 
 urlpatterns += [
     url(r'^fee-settings/batch', FeeSettingsListView.as_view()),
     url(r'^fee-settings', FeeSettingsView.as_view()),
-]
-
-from .views import FeeReceiptOrderView, FeeReceiptOrderListView
-urlpatterns += [
-    url(r'^fee-receipt-order/batch', FeeReceiptOrderListView.as_view()),
-    url(r'^fee-receipt-order', FeeReceiptOrderView.as_view()),
 ]
