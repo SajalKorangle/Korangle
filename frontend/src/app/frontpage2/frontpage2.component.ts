fb1faa2c97fd94fbcef5a6978e4a61489d714f11
SyedAnas26 <ahmed.anas069@gmail.com>
2021-11-15 19:11:19 +0530

    completed

--- frontend/src/app/frontpage2/frontpage2.component.ts
+++ frontend/src/app/frontpage2/frontpage2.component.ts
@@ -1,6 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {CommonFunctions} from '@classes/common-functions';
-import {Router} from '@angular/router';
+import {VALIDATORS_REGX} from '@classes/regx-validators';
+import {DataStorage} from '@classes/data-storage';
+import {User} from '@classes/user';
+import {registerForNotification} from '@classes/common';
+import {environment} from '../../environments/environment';
+import {Constants} from '@classes/constants';
+import {AuthenticationOldService} from '@services/authentication-old.service';
+import {NotificationService} from '@services/modules/notification/notification.service';
 
 
 @Component({
@@ -10,15 +17,91 @@ import {Router} from '@angular/router';
 })
 export class Frontpage2Component implements OnInit {
 
-    constructor(private router: Router) {
-    }
-
     section = 'normal';
+    user: User;
+
+    loginVariables = {
+        mobileNumber: '',
+        password: '',
+        passwordVisibility: false
+    };
+
+    forgotPasswordVariables = {
+        mobileNumber: null,
+        otp: '',
+        newPassword: null,
+        confirmPassword: null,
+        newPasswordVisibility: false,
+        confirmPasswordVisibility: false
+    };
+
+    signUpVariables = {
+        firstName: null,
+        lastName: null,
+        emailID: null,
+        mobileNumber: null,
+        otp: '',
+        newPassword: null,
+        confirmPassword: null,
+        newPasswordVisibility: false,
+        confirmPasswordVisibility: false
+    };
+
+    validators = VALIDATORS_REGX;
+
+    isLoading = false;
 
     changePassword = false;
     path = window.location.pathname;
 
-    ngOnInit() {
+    constructor(private authenticationService: AuthenticationOldService, private notificationService: NotificationService) {
+    }
+
+
+    ngOnInit(): void {
+        this.user = DataStorage.getInstance().getUser();
+    }
+
+    login() {
+        this.isLoading = true;
+        this.authenticationService.loginUserDetails(this.loginVariables.mobileNumber, this.loginVariables.password).then(
+            (data) => {
+                this.isLoading = false;
+                if (data.username === 'invalidUsername') {
+                    alert('Login failed: Invalid username or password');
+                } else {
+                    localStorage.setItem('schoolJWT', data.token);
+                    this.user.jwt = data.token;
+                    this.user.isAuthenticated = true;
+                    this.user.initializeUserData(data);
+                    //Sending the user-id of user to google-analytics to monitor per user flow
+                    // (<any>window).ga('set', 'userId', 'id: '+data.id+' user-name: '+data.username+ ' name: '+data.first_name+' '+data.last_name);
+                    (<any>window).ga('set', 'userId', 'id: ' + data.id);
+                    (<any>window).ga('send', 'event', 'authentication', 'Login');
+                    registerForNotification({
+                        user: this.user.id,
+                        jwt: this.user.jwt,
+                        url:
+                            environment.DJANGO_SERVER +
+                            Constants.api_version +
+                            this.notificationService.module_url +
+                            this.notificationService.gcm_device,
+                    });
+                }
+            },
+            (error) => {
+                this.isLoading = false;
+            }
+        );
+    }
+
+    submit() {
+        const elem = document.getElementById('username');
+        if (document.activeElement === elem) {
+            document.getElementById('pass').focus();
+        } else {
+            this.login();
+        }
     }
 
     isMobile(): boolean {
@@ -27,6 +110,18 @@ export class Frontpage2Component implements OnInit {
 
     onOtpChange(event) {
         console.log(event);
+    }
+
+    toggleVisibilityMode(visibilityMode): void {
+        visibilityMode = !visibilityMode;
+    }
+
+    getVisibilityMode(visibilityMode): string {
+        if (visibilityMode) {
+            return 'visibility_off';
+        } else {
+            return 'visibility';
+        }
     }
 
 }
