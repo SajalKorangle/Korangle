da26bb9740dd7c8301b0420870d3b36029026f26
Dhruv-Kakadiya <kakadiya.1@iitj.ac.in>
2021-12-23 13:54:03 +0530

    changes in admin.py

--- backend/student_app/admin.py
+++ backend/student_app/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-<<<<<<< Updated upstream
-=======
 from .models import CountAllTable
->>>>>>> Stashed changes
 # Register your models here.
 
 admin.site.register(CountAllTable)
