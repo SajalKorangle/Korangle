b424e3c7dfb550cc891a3ab571df78f788299d37
Dhruv-Kakadiya <kakadiya.1@iitj.ac.in>
2022-01-10 14:31:17 +0530

    updated database

--- backend/parent_support_app/models.py
+++ backend/parent_support_app/models.py
@@ -1,32 +1,40 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
 
 from employee_app.models import Employee
 from student_app.models import Student
 
-# Create your models here.
-class MessageType(models.Model):
 
-    # Type Name
+class Status(models.Model):
+
+    # Status Name
     name = models.CharField(max_length = 20)
 
-    # Default Text
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'complaint_status'
+
+
+class ComplaintType(models.Model):
+
+    # Complaint Type Name
+    name = models.CharField(max_length = 20)
+
+    # Default Complaint Text
     defaultText = models.TextField()
 
-    # Applicable Status
-    statuses = ArrayField(models.CharField(max_length=20))
-
-    # Default Status
+    # Default Complaint Status
     defaultStatus = models.CharField(max_length = 20)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        db_table = 'message_type'
+        db_table = 'complaint_type'
 
 
-class Message(models.Model):
+class Complaint(models.Model):
 
     # Parent Name
     parentEmployee = models.ForeignKey(Employee, on_delete=models.CASCADE)
@@ -34,47 +42,69 @@ class Message(models.Model):
     # Date Sent
     dateSent = models.DateTimeField(auto_now_add = True)
 
-    # Message Type
-    parentMessageType = models.ForeignKey(MessageType, on_delete=models.CASCADE)
+    # Complaint Type
+    parentComplaintType = models.ForeignKey(ComplaintType, on_delete=models.CASCADE)
 
-    # Child Name
+    # Child
     parentStudent = models.ForeignKey(Student, on_delete=models.CASCADE)
 
     # Contact Number
     mobileNumber = models.BigIntegerField(null=True)
 
-    # Title
+    # Complaint Title
     title = models.CharField(max_length = 50)
 
     # Comments
+    # It's an object of objects.
+
+    # comment = {
+    #     "sender's name": name,
+    #     "comment": comment,
+    #     "Date-Time: dateTime",
+    # }
     comments = models.JSONField()
 
-    # Message Status
-    status = models.CharField(max_length = 20)
+    # Complaint Status
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
     class Meta:
-        db_table = 'message'
+        db_table = 'complaint'
+
+
+class StatusComplaintTypeJunction(models.Model):
+
+    # Parent Status
+    parentStatus = models.ForeignKey(Status, on_delete=models.CASCADE)
+
+    # Parent Complaint type
+    parentComplaintType = models.ForeignKey(ComplaintType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (self.parentStatus.name + " & " + self.parentComplaintType.name)
+
+    class Meta:
+        db_table = 'status_complaintType_junction'
 
 
 class EmployeeMessageTypeJuction(models.Model):
 
-    # Employee name
+    # Parent Employee
     parentEmployee = models.ForeignKey(Employee, on_delete=models.CASCADE)
 
-    # Message type
-    parentMessageType = models.ForeignKey(MessageType, on_delete=models.CASCADE)
+    # Parent Complaint type
+    parentComplaintType = models.ForeignKey(ComplaintType, on_delete=models.CASCADE)
 
-    # Notify employee based on message's status
+    # Notify employee based on complaint's status
     notifyStatus = models.CharField(max_length = 20, null = True)
 
     # After how many day he/she should get notified
     notifyDays = models.IntegerField(null = True)
 
     def __str__(self):
-        return (self.parentEmployee.name + " & " + self.parentMessageType.name)
+        return (self.parentEmployee.name + " & " + self.parentComplaintType.name)
 
     class Meta:
-        db_table = 'employee_messageType_junction'
+        db_table = 'employee_complaintType_junction'
