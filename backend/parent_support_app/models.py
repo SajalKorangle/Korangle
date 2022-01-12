c3cc2c708f3540bc65466263f8ed71add3997353
Dhruv-Kakadiya <kakadiya.1@iitj.ac.in>
2022-01-12 16:05:44 +0530

    database updated

--- backend/parent_support_app/models.py
+++ backend/parent_support_app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from employee_app.models import Employee
 from student_app.models import Student
-
+from notification_app.models import Notification
 
 class Status(models.Model):
 
@@ -25,7 +27,7 @@ class ComplaintType(models.Model):
     defaultText = models.TextField()
 
     # Default Complaint Status
-    defaultStatus = models.CharField(max_length = 20)
+    parentStatus_defaultStatus = models.ForeignKey(Status, on_delete = models.SET_NULL, null = True)
 
     def __str__(self):
         return self.name
@@ -36,20 +38,20 @@ class ComplaintType(models.Model):
 
 class Complaint(models.Model):
 
-    # Parent Name
-    parentEmployee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    # Father or Creator
+    parentEmployee = models.ForeignKey(Employee, on_delete = models.CASCADE)
 
     # Date Sent
     dateSent = models.DateTimeField(auto_now_add = True)
 
     # Complaint Type
-    parentComplaintType = models.ForeignKey(ComplaintType, on_delete=models.CASCADE)
+    parentComplaintType = models.ForeignKey(ComplaintType, on_delete = models.CASCADE)
 
     # Child
-    parentStudent = models.ForeignKey(Student, on_delete=models.CASCADE)
+    parentStudent = models.ForeignKey(Student, on_delete = models.CASCADE)
 
     # Contact Number
-    mobileNumber = models.BigIntegerField(null=True)
+    mobileNumber = models.BigIntegerField(null = True)
 
     # Complaint Title
     title = models.CharField(max_length = 50)
@@ -62,10 +64,10 @@ class Complaint(models.Model):
     #     "comment": comment,
     #     "Date-Time: dateTime",
     # }
-    comments = models.JSONField()
+    commentList = models.JSONField()
 
     # Complaint Status
-    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    parentStatus = models.ForeignKey(Status, on_delete = models.SET_NULL, null = True)
 
     def __str__(self):
         return self.title
@@ -74,37 +76,47 @@ class Complaint(models.Model):
         db_table = 'complaint'
 
 
-class StatusComplaintTypeJunction(models.Model):
+class StatusComplaintType(models.Model):
 
     # Parent Status
-    parentStatus = models.ForeignKey(Status, on_delete=models.CASCADE)
+    parentStatus = models.ForeignKey(Status, on_delete = models.CASCADE)
 
     # Parent Complaint type
-    parentComplaintType = models.ForeignKey(ComplaintType, on_delete=models.CASCADE)
+    parentComplaintType = models.ForeignKey(ComplaintType, on_delete = models.CASCADE)
 
     def __str__(self):
         return (self.parentStatus.name + " & " + self.parentComplaintType.name)
 
     class Meta:
-        db_table = 'status_complaintType_junction'
+        db_table = 'status_complaintType'
 
 
-class EmployeeMessageTypeJuction(models.Model):
+class EmployeeComplaintType(models.Model):
 
     # Parent Employee
-    parentEmployee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    parentEmployee = models.ForeignKey(Employee, on_delete = models.CASCADE)
 
     # Parent Complaint type
-    parentComplaintType = models.ForeignKey(ComplaintType, on_delete=models.CASCADE)
+    parentComplaintType = models.ForeignKey(ComplaintType, on_delete = models.CASCADE)
 
-    # Notify employee based on complaint's status
-    notifyStatus = models.CharField(max_length = 20, null = True)
-
-    # After how many day he/she should get notified
-    notifyDays = models.IntegerField(null = True)
+    # User Id
+    # Need at a time of creating a notification.
+    userId = models.IntegerField()
 
     def __str__(self):
         return (self.parentEmployee.name + " & " + self.parentComplaintType.name)
 
     class Meta:
-        db_table = 'employee_complaintType_junction'
+        db_table = 'employee_complaintType'
+
+
+@receiver(post_save, sender = EmployeeComplaintType)
+def notify_employee(sender, instance, created, **kwargs):
+
+    if created:
+        notification = Notification()
+        notification.content = "A new complaint of type " + instance.parentComplaintType.name + " is assigned to you. Kindly respond to it. "
+        notification.SMSEventId = 0
+        notification.parentUser = instance.userId
+        notification.parentSchool = instance.parentEmployee.parentSchool
+        notification.save()
