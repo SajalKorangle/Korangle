8091c273df8b9d42c0467aed18f7e6cc5e8f7eff
Dhruv-Kakadiya <kakadiya.1@iitj.ac.in>
2022-02-01 14:55:14 +0530

    migrations error

--- backend/parent_support_app/admin.py
+++ backend/parent_support_app/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import SchoolComplaintStatus, SchoolComplaintType, Complaint, Comment, StatusComplaintType, EmployeeComplaintType, CountAllParentSupport
-
-admin.site.register(SchoolComplaintStatus)
-admin.site.register(SchoolComplaintType)
-admin.site.register(Complaint)
-admin.site.register(Comment)
-admin.site.register(StatusComplaintType)
-admin.site.register(EmployeeComplaintType)
-admin.site.register(CountAllParentSupport)
