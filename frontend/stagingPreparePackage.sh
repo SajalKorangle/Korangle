e8356914bccf2647c540dda75c6f8abe35989b6e
harshal <harshalagrawal03@gmail.com>
2022-03-16 16:08:24 +0530

    created post build script for staging

--- frontend/stagingPreparePackage.sh
+++ frontend/stagingPreparePackage.sh
@@ -1,6 +1,5 @@
 rm -rf korangle.zip korangle/
 node --max_old_space_size=8192 ./node_modules/@angular/cli/bin/ng build --configuration=staging --build-optimizer
 mv dist/ korangle
-npm run post-build $1
-cp version.json korangle/version.json
+npm run post-build-staging $1
 zip -r korangle.zip korangle/
