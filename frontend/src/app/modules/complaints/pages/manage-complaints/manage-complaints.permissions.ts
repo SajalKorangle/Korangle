2f513648d5b522c44d8568073713863380a46b05
Dhruv-Kakadiya <kakadiya.1@iitj.ac.in>
2022-04-09 21:51:08 +0530

    permission teacher to employee

--- frontend/src/app/modules/complaints/pages/manage-complaints/manage-complaints.permissions.ts
+++ frontend/src/app/modules/complaints/pages/manage-complaints/manage-complaints.permissions.ts
@@ -2,7 +2,7 @@ import { TaskPermissionStructure, InPagePermission } from '@modules/common/in-pa
 
 export const USER_PERMISSION_KEY = 'userType';
 export const ADMIN_PERMISSION = 'admin';
-export const TEACHER_PERMISSION = 'teacher';
+export const EMPLOYEE_PERMISSION = 'employee';
 
 const add_account_in_page_permission = new TaskPermissionStructure("complaints", "manage_complaints");
 
@@ -10,6 +10,6 @@ add_account_in_page_permission.inPagePermissionMappedByKey[USER_PERMISSION_KEY]
     = new InPagePermission(
         'User Type',
         'select',
-        [[ADMIN_PERMISSION, 'Admin'], [TEACHER_PERMISSION, 'Teacher']],
+        [[ADMIN_PERMISSION, 'Admin'], [EMPLOYEE_PERMISSION, 'Employee']],
         { groupName: "User Type Permission" }
     );
