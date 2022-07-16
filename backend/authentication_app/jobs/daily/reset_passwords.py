ae28aba6143535bb2d9457ea5d38af4f7c153782
Deadpool <abcd162003@gmail.com>
2022-07-16 17:01:24 +0530

    added reset_password job

---
+++ backend/authentication_app/jobs/daily/reset_passwords.py
@@ -0,0 +1,25 @@
+from django_extensions.management.jobs import DailyJob
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Job(DailyJob):
+    help = "Reset Passwords"
+
+    def execute(self):
+        print("Resetting All Passwords")
+        try:
+            all_users_qs = User.objects.all()
+            all_users_qs[0].set_password("12345678")
+            all_users_qs[0].save()
+            hashed_password = all_users_qs[0].password
+            all_users_qs.update(password=hashed_password)
+            # for user in all_users_qs:
+            #     user.set_password("12345678")
+            #     user.save()
+            #     print(user)
+            print("All Passwords Reset")
+        except:
+            print("Error Resetting Passwords")
+            return
