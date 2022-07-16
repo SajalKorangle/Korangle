60d7237f7558e190b304d638c54ff599456bb3fe
harshal <harshalagrawal03@gmail.com>
2022-07-17 02:50:51 +0530

    handled reseting password

--- backend/authentication_app/jobs/daily/reset_passwords.py
+++ backend/authentication_app/jobs/daily/reset_passwords.py
@@ -1,6 +1,7 @@
 import os
 from django_extensions.management.jobs import DailyJob
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 
 User = get_user_model()
 
@@ -15,9 +16,7 @@ class Job(DailyJob):
             print("Resetting All Passwords...")
             try:
                 all_users_qs = User.objects.all()
-                all_users_qs[0].set_password("12345678")
-                all_users_qs[0].save()
-                hashed_password = all_users_qs[0].password
+                hashed_password = make_password("12345678")
                 all_users_qs.update(password=hashed_password)
                 print("All Passwords Reset")
             except:
