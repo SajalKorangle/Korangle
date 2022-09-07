6c987760840727dcc853205b5bc00e7a19aee0b0
Shubhank <kul.shubhank207@gmail.com>
2022-09-07 18:21:36 +0530

    minor

--- frontend/src/app/modules/examination/pages/update-marks/update-marks.component.ts
+++ frontend/src/app/modules/examination/pages/update-marks/update-marks.component.ts
@@ -27,8 +27,8 @@ export class UpdateMarksComponent implements OnInit {
     showTestDetails = false;
 
     // --- starts : variables for marks lock status ---
-    MARKS_UPDATION_LOCKED_STATUS_BACKEND='Locked'
-    MARKS_UPDATION_UNLOCKED_STATUS_BACKEND='Unlocked'
+    MARKS_UPDATION_LOCKED_STATUS_BACKEND = 'Locked';
+    MARKS_UPDATION_UNLOCKED_STATUS_BACKEND = 'Unlocked';
     // --- ends : variables for marks lock status ---
 
     selectedExamination: any;
