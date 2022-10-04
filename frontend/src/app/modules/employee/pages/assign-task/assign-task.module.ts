857e878ca4f3eff1d4c9e4dbd62d477d90d8e073
divyanshkorangle <divyansh@korangle.com>
2022-10-04 21:09:16 +0530

    view defaulter permission assign

--- frontend/src/app/modules/employee/pages/assign-task/assign-task.module.ts
+++ frontend/src/app/modules/employee/pages/assign-task/assign-task.module.ts
@@ -6,14 +6,15 @@ import { AssignTaskRoutingModule } from './assign-task.routing';
 import { ComponentsModule } from '../../../../components/components.module';
 import { InPagePermissionDialogComponent } from './../../component/in-page-permission-dialog/in-page-permission-dialog.component';
 import { LocalComponentsModule } from '@modules/employee/component/local-components.module';
+import { ViewDefaulterPermissionModalComponent } from './view-defaulter-permission-modal/view-defaulter-permission-modal.component';
 @NgModule({
-    declarations: [AssignTaskComponent],
+    declarations: [AssignTaskComponent, ViewDefaulterPermissionModalComponent],
     imports: [
         AssignTaskRoutingModule,
         ComponentsModule,
         LocalComponentsModule
     ],
-    entryComponents: [InPagePermissionDialogComponent],
+    entryComponents: [InPagePermissionDialogComponent, ViewDefaulterPermissionModalComponent],
     exports: [],
     providers: [],
     bootstrap: [AssignTaskComponent],
