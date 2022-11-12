68ee6fb0e535cb62926d661db0fddcf7ade98f96
harshal <harshalagrawal03@gmail.com>
2022-11-12 12:45:01 +0530

    added exclaimation before git

--- frontend/scripts/diff_changes/small_changes_check.sh
+++ frontend/scripts/diff_changes/small_changes_check.sh
@@ -1,6 +1,4 @@
-git diff master --shortstat
-
-CHANGE_COUNT=$(git diff master --shortstat | awk '{print $4 + $6}')
+CHANGE_COUNT=$(!git diff master --shortstat | awk '{print $4 + $6}')
 
 if [[ $CHANGE_COUNT -gt 500 ]]
 then
