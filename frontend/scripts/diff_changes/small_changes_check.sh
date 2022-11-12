19f45c6099387ae0cce58efa1750a1d3cad748f7
harshal <harshalagrawal03@gmail.com>
2022-11-12 12:19:54 +0530

    changed maximum line change limit from 600 to 500

--- frontend/scripts/diff_changes/small_changes_check.sh
+++ frontend/scripts/diff_changes/small_changes_check.sh
@@ -1,6 +1,6 @@
 CHANGE_COUNT=$(git diff master --shortstat | awk '{print $4 + $6}')
 
-if [[ $CHANGE_COUNT -gt 600 ]]
+if [[ $CHANGE_COUNT -gt 500 ]]
 then
     echo "ERROR: Number of lines changes ($CHANGE_COUNT) should be less than 500"
     exit 1
