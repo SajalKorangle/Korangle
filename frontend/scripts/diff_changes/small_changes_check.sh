028aee9afb766954f15ac7588ed90f7a793dae9c
harshal <harshalagrawal03@gmail.com>
2022-11-12 12:34:45 +0530

    changed print statement

--- frontend/scripts/diff_changes/small_changes_check.sh
+++ frontend/scripts/diff_changes/small_changes_check.sh
@@ -2,6 +2,6 @@ CHANGE_COUNT=$(git diff master --shortstat | awk '{print $4 + $6}')
 
 if [[ $CHANGE_COUNT -gt 500 ]]
 then
-    echo "ERROR: Number of lines changes ($CHANGE_COUNT) should be less than 500"
+    echo "ERROR: Number of changed lines ($CHANGE_COUNT) should be less than 500"
     exit 1
 fi
