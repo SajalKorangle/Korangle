53025d23741c052065f03a3babf26c22a067f302
harshal <harshalagrawal03@gmail.com>
2022-11-17 14:41:01 +0530

    changed pr size limit from 500 to 600

--- frontend/scripts/diff_changes/small_changes_check.sh
+++ frontend/scripts/diff_changes/small_changes_check.sh
@@ -1,7 +1,7 @@
 CHANGE_COUNT=$(git diff -w --shortstat origin/master | awk '{print $4 + $6}')
 
-if [[ $CHANGE_COUNT -gt 500 ]]
+if [[ $CHANGE_COUNT -gt 600 ]]
 then
-    echo "ERROR: Number of changed lines ($CHANGE_COUNT) should be less than 500"
+    echo "ERROR: Number of changed lines ($CHANGE_COUNT) should be less than 600"
     exit 1
 fi
