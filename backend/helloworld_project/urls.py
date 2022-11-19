567ade8a53cd276099eb535ff3e5f4c2d13b4afa
harshal <harshalagrawal03@gmail.com>
2022-11-20 00:41:26 +0530

    changed api version from 7.7 to 7.8

--- backend/helloworld_project/urls.py
+++ backend/helloworld_project/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from generic.generic_views import GenericView, GenericListView
 
-api_version = 'v7.7/'
+api_version = 'v7.8/'
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
