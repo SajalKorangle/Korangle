5b7e2f3940f9be149b765586e580aaca403ef48c
Dhruv-Kakadiya <kakadiya.1@iitj.ac.in>
2022-12-14 15:41:06 +0530

    css file

--- frontend/src/app/modules/students/pages/update-via-excel/update-via-excel.component.ts
+++ frontend/src/app/modules/students/pages/update-via-excel/update-via-excel.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-update-via-excel',
-  templateUrl: './update-via-excel.component.html'
+  templateUrl: './update-via-excel.component.html',
+  styleUrls: ['./update-via-excel.component.css'],
 })
 export class UpdateViaExcelComponent implements OnInit {
 
