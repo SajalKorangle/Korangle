a2873d7e1a8288e692805208564ca1394006bea6
Dhruv-Kakadiya <kakadiya.1@iitj.ac.in>
2022-12-14 15:38:16 +0530

    Features Flag

--- backend/team_app/db_script/add_task_update_via_excel.py
+++ backend/team_app/db_script/add_task_update_via_excel.py
@@ -2,10 +2,19 @@ def add_task_update_via_excel(apps, schema_editor):
     Module = apps.get_model('team_app', 'Module')
     Task = apps.get_model('team_app', 'Task')
     EmployeePermission = apps.get_model('employee_app', 'EmployeePermission')
+    FeatureFlag = apps.get_model('feature_flag_app', 'FeatureFlag')
+
 
     module_object = Module.objects.get(path='students')
-    task_object = Task(path='update_via_excel', title='Update Via Excel', orderNumber=4, parentModule=module_object)
+    task_object = Task()
+    task_object.parentModule = module_object
+    task_object.path = 'update_via_excel'
+    task_object.title = 'Update Via Excel'
+    task_object.orderNumber = 4
+    task_object.parentBoard = None
+    task_object.parentFeatureFlag = FeatureFlag.objects.get(name = "Update Via Excel")
     task_object.save()
+
 
     for employee_permission in EmployeePermission.objects.filter(parentTask__path='assign_task'):
         employee = EmployeePermission()
--- frontend/src/app/modules/students/pages/update-via-excel/update-via-excel.component.ts
+++ frontend/src/app/modules/students/pages/update-via-excel/update-via-excel.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-update-via-excel',
-  templateUrl: './update-via-excel.component.html',
-  styleUrls: ['./update-via-excel.component.css']
+  templateUrl: './update-via-excel.component.html'
 })
 export class UpdateViaExcelComponent implements OnInit {
 
