d1e3f43fb2ada39554312fcdc160ede72b29f7b9
harshal <harshalagrawal03@gmail.com>
2023-01-18 18:04:04 +0530

    Solved following QA Testing Bugs
    1. "1. Agar ham koi  file choose karte h then uske bad again koi file choose karne k liye dialog kholkar bina file select kiye close kar diya jaye tab jo file pehel se select  uss file k name ki jagha ""No file chosen"" show hota h .
    "
    2. 1. Agar koi file pehle se select h then re-uploading karte h tab ""loss data warning"" Dialog show hota h jisme agare hamne cancel kar diya tab file name ki place par pichli file ka hi name ana chahiye par vaha file name change ho jata h.
    3. (In Mobile Apk ) It accept image format in the place of Excel format

--- frontend/src/app/modules/students/pages/add-via-excel/add-via-excel.html.adapter.ts
+++ frontend/src/app/modules/students/pages/add-via-excel/add-via-excel.html.adapter.ts
@@ -1,17 +1,20 @@
 import { AddViaExcelComponent } from './add-via-excel.component';
+import { isMobile } from '@classes/common';
 
 export class AddViaExcelHtmlAdapter {
 
     vm: AddViaExcelComponent;
 
     isLoading = false;
-    
+
     constructor( ) { }
 
     initializeAdapter(vm: AddViaExcelComponent): void {
-
         this.vm = vm;
+    }
 
+    isMobileDevice(): boolean {
+        return isMobile();
     }
 
 }
