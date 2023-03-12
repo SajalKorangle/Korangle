039715f79a72c21b2098c10cb31f3a932187de7e
Gourav Bidhuri <gourav@korangle.com>
2023-03-12 11:15:36 +0530

    removed unnecessary changes.

--- frontend/src/app/modules/leaves/leaves.component.ts
+++ frontend/src/app/modules/leaves/leaves.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from "@angular/core";
-import { DataStorage } from "../../classes/data-storage";
+import { Component, OnInit } from '@angular/core';
+import { DataStorage } from '../../classes/data-storage';
 
 @Component({
-    template: "<router-outlet></router-outlet>",
+    template: '<router-outlet></router-outlet>',
 })
-export class LeavesComponent {
+export class LeavesComponent{
+
     constructor() {}
 }
