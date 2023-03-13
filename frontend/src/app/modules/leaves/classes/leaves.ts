1b440409180b0c360bf5dd45d44f585048bf744a
Gourav Bidhuri <gourav@korangle.com>
2023-03-13 22:07:39 +0530

    reformatted the code to save number of lines.

--- frontend/src/app/modules/leaves/classes/leaves.ts
+++ frontend/src/app/modules/leaves/classes/leaves.ts
@@ -1,14 +1,7 @@
+// prettier-ignore
 export interface MonthVsLeaves {
-  jan: Array<number>;
-  feb: Array<number>;
-  mar: Array<number>;
-  apr: Array<number>;
-  may: Array<number>;
-  jun: Array<number>;
-  jul: Array<number>;
-  aug: Array<number>;
-  sep: Array<number>;
-  oct: Array<number>;
-  nov: Array<number>;
-  dec: Array<number>;
+    jan: Array<number>; feb: Array<number>; mar: Array<number>;
+    apr: Array<number>; may: Array<number>; jun: Array<number>;
+    jul: Array<number>; aug: Array<number>; sep: Array<number>;
+    oct: Array<number>; nov: Array<number>; dec: Array<number>;
 }
--- frontend/src/app/modules/leaves/components/dialogs/leave-type-dialog/leave-type.component.ts
+++ frontend/src/app/modules/leaves/components/dialogs/leave-type-dialog/leave-type.component.ts
@@ -13,17 +13,17 @@ export class LeaveTypeDialog {
     // dialog variables
     // prettier-ignore
     colorCodeList: string[] = [
-        "#f9ebea", "#f2d7d5", "#e6b0aa", "#d98880", "#cd6155", "#c0392b", "#a93226", "#922b21", "#943126", "#78281f",
-        "#f5eef8", "#ebdef0", "#d7bde2", "#c39bd3", "#af7ac5", "#9b59b6", "#884ea0", "#76448a", "#633974", "#512e5f",
-        "#eaf2f8", "#d4e6f1", "#a9cce3", "#7fb3d5", "#5499c7", "#2980b9", "#2471a3", "#1f618d", "#1a5276", "#154360",
-        "#e8f8f5", "#d1f2eb", "#a3e4d7", "#76d7c4", "#48c9b0", "#1abc9c", "#17a589", "#148f77", "#117864", "#0e6251",
-        "#e9f7ef", "#d4efdf", "#a9dfbf", "#7dcea0", "#52be80", "#27ae60", "#229954", "#1e8449", "#196f3d", "#145a32",
-        "#fef9e7", "#fcf3cf", "#f9e79f", "#f7dc6f", "#f4d03f", "#f1c40f", "#d4ac0d", "#b7950b", "#9a7d0a", "#7d6608",
-        "#fdf2e9", "#fae5d3", "#f5cba7", "#f0b27a", "#eb984e", "#e67e22", "#ca6f1e", "#af601a", "#935116", "#784212",
-        "#fdfefe", "#fbfcfc", "#f7f9f9", "#f4f6f7", "#f0f3f4", "#ecf0f1", "#d0d3d4", "#b3b6b7", "#979a9a", "#7b7d7d",
-        "#f4f6f6", "#eaeded", "#d5dbdb", "#bfc9ca", "#aab7b8", "#95a5a6", "#839192", "#717d7e", "#5f6a6a", "#4d5656",
-        "#ebedef", "#d6dbdf", "#aeb6bf", "#85929e", "#5d6d7e", "#34495e", "#2e4053", "#283747", "#212f3c", "#1b2631",
-        "#eaecee", "#d5d8dc", "#abb2b9", "#808b96", "#566573", "#2c3e50", "#273746", "#212f3d", "#1c2833", "#17202a"
+        '#f9ebea', '#f2d7d5', '#e6b0aa', '#d98880', '#cd6155', '#c0392b', '#a93226', '#922b21', '#943126', '#78281f',
+        '#f5eef8', '#ebdef0', '#d7bde2', '#c39bd3', '#af7ac5', '#9b59b6', '#884ea0', '#76448a', '#633974', '#512e5f',
+        '#eaf2f8', '#d4e6f1', '#a9cce3', '#7fb3d5', '#5499c7', '#2980b9', '#2471a3', '#1f618d', '#1a5276', '#154360',
+        '#e8f8f5', '#d1f2eb', '#a3e4d7', '#76d7c4', '#48c9b0', '#1abc9c', '#17a589', '#148f77', '#117864', '#0e6251',
+        '#e9f7ef', '#d4efdf', '#a9dfbf', '#7dcea0', '#52be80', '#27ae60', '#229954', '#1e8449', '#196f3d', '#145a32',
+        '#fef9e7', '#fcf3cf', '#f9e79f', '#f7dc6f', '#f4d03f', '#f1c40f', '#d4ac0d', '#b7950b', '#9a7d0a', '#7d6608',
+        '#fdf2e9', '#fae5d3', '#f5cba7', '#f0b27a', '#eb984e', '#e67e22', '#ca6f1e', '#af601a', '#935116', '#784212',
+        '#fdfefe', '#fbfcfc', '#f7f9f9', '#f4f6f7', '#f0f3f4', '#ecf0f1', '#d0d3d4', '#b3b6b7', '#979a9a', '#7b7d7d',
+        '#f4f6f6', '#eaeded', '#d5dbdb', '#bfc9ca', '#aab7b8', '#95a5a6', '#839192', '#717d7e', '#5f6a6a', '#4d5656',
+        '#ebedef', '#d6dbdf', '#aeb6bf', '#85929e', '#5d6d7e', '#34495e', '#2e4053', '#283747', '#212f3c', '#1b2631',
+        '#eaecee', '#d5d8dc', '#abb2b9', '#808b96', '#566573', '#2c3e50', '#273746', '#212f3d', '#1c2833', '#17202a'
     ];
     isColorListVisible: boolean = false;
     isNoteVisible: boolean = false;
@@ -38,10 +38,9 @@ export class LeaveTypeDialog {
     leaveType: number = -1;
     color: string = "";
     leavesPerMonth: MonthVsLeaves;
+    // prettier-ignore
     salaryComponentValue: { [id: string]: number } = {
-        "Base Salary": 0,
-        HRA: 0,
-        DA: 0,
+        "Base Salary": 0, HRA: 0, DA: 0,
     };
     dividingFactorType: number = -1;
     dividingFactorValue: number = 30;
@@ -53,24 +52,14 @@ export class LeaveTypeDialog {
             this.color = "";
             this.isColorListVisible = false;
             this.isSalaryComponentSelectorVisible = false;
+            // prettier-ignore
             this.leavesPerMonth = {
-                jan: [0, 0],
-                feb: [0, 0],
-                mar: [0, 0],
-                apr: [0, 0],
-                may: [0, 0],
-                jun: [0, 0],
-                jul: [0, 0],
-                aug: [0, 0],
-                sep: [0, 0],
-                oct: [0, 0],
-                nov: [0, 0],
-                dec: [0, 0],
+                jan: [0, 0], feb: [0, 0], mar: [0, 0], apr: [0, 0], may: [0, 0], jun: [0, 0],
+                jul: [0, 0], aug: [0, 0], sep: [0, 0], oct: [0, 0], nov: [0, 0], dec: [0, 0],
             };
+            // prettier-ignore
             this.salaryComponentValue = {
-                "Base Salary": 0,
-                HRA: 0,
-                DA: 0,
+                "Base Salary": 0, HRA: 0, DA: 0,
             };
             this.dividingFactorType = -1;
             this.dividingFactorValue = 30;
@@ -117,25 +106,17 @@ export class LeaveTypeDialog {
         } else if (this.dividingFactorType === -1 || this.dividingFactorValue === 0) {
             alert("Please select a valid division factor type / value.");
         } else {
+            // prettier-ignore
             event.data = {
-                isNew: this.isNew,
-                leaveTypeName: this.name,
-                leaveType: this.leaveType,
-                color: this.color,
-                assignedLeavesMonthWise: JSON.stringify(this.leavesPerMonth),
-                salaryComponents: JSON.stringify(this.salaryComponentValue),
-                divisionFactor: this.dividingFactorValue,
+                isNew: this.isNew, leaveTypeName: this.name, leaveType: this.leaveType,
+                color: this.color, assignedLeavesMonthWise: JSON.stringify(this.leavesPerMonth),
+                salaryComponents: JSON.stringify(this.salaryComponentValue), divisionFactor: this.dividingFactorValue,
                 divisionFactorType: this.dividingFactorType,
             };
-            if (!this.isNew) {
-                event.data.id = this.data.id;
-            }
+            event.data.id = !this.isNew ? this.data.id : -1;
             await this.save.emit(event.data);
         }
         this.isSaving = false;
-    }
-    closeDialog(event): void {
-        this.close.emit(event);
     }
     updateLeaves(event, month): void {
         this.leavesPerMonth[month][1] = parseInt(event.target.value);
