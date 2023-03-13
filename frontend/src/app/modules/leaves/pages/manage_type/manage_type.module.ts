bd0302223c57adf0755fb347d7aa375e1316bf98
Gourav Bidhuri <gourav@korangle.com>
2023-03-13 22:20:14 +0530

    removed unnecessary changes.

--- frontend/src/app/modules/leaves/pages/manage_type/manage_type.module.ts
+++ frontend/src/app/modules/leaves/pages/manage_type/manage_type.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from "@angular/core";
+
 import { ManageTypeComponent } from "./manage_type.component";
 import { ManageTypeRouting } from "./manage_type.routing";
 import { ComponentsModule } from "@components/components.module";
 import { LeaveTypeDialog } from "@modules/leaves/components/dialogs/leave-type-dialog/leave-type.component";
-
 @NgModule({
     declarations: [ManageTypeComponent, LeaveTypeDialog],
     imports: [ManageTypeRouting, ComponentsModule],
