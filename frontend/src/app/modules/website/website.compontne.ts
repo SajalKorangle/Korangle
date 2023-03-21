9310b792bce6471514b33e8951b171ea581aeffb
Aditya Kumar <adityakumar@korangle.com>
2023-03-21 23:40:44 +0530

    linting fix

--- frontend/src/app/modules/website/website.compontne.ts
+++ frontend/src/app/modules/website/website.compontne.ts
@@ -5,7 +5,7 @@ import { DataStorage } from '../../classes/data-storage';
     template: '<router-outlet></router-outlet>',
 })
 export class WebsiteComponent implements OnInit {
-    
+
     user: any;
 
     constructor() {}
