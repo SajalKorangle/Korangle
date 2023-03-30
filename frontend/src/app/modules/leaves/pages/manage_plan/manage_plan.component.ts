7cdaea42e075073bfd0113bd98f69a09f94085c9
Gourav Bidhuri <gourav@korangle.com>
2023-03-30 23:41:55 +0530

    updated template for manage_plan to display under construction.

--- frontend/src/app/modules/leaves/pages/manage_plan/manage_plan.component.ts
+++ frontend/src/app/modules/leaves/pages/manage_plan/manage_plan.component.ts
@@ -1,15 +1,11 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component} from "@angular/core";
 import { GenericService } from "@services/generic/generic-service";
 
 @Component({
     selector: "manage-plan",
-    templateUrl: "./manage_plan.component.html",
-    styleUrls: ["./manage_plan.component.css"],
+    template: "Under Construction!",
     providers: [GenericService],
 })
 export class ManagePlanComponent {
     constructor(public genericService: GenericService) {}
-    // page manage variables
-    isLoading: boolean = false;
-    isLeavePlanOpen: boolean = false;
 }
