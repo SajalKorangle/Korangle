ad73335c81ca4d3561cc110496c78a81099a127e
BLLatKorangle <125577685+BLLatKorangle@users.noreply.github.com>
2023-03-30 12:01:09 +0530

    Fixed linting

--- frontend/src/app/classes/user.ts
+++ frontend/src/app/classes/user.ts
@@ -212,7 +212,7 @@ export class User {
         let maxSessionID = 0;
         this.session_list.forEach((session) => {
             maxSessionID = Math.max(maxSessionID, session.id);
-        })
+        });
         if (school != undefined && Number(urlParams.get('session')) > 0 && Number(urlParams.get('session')) <= maxSessionID) {
             this.activeSchool = school;
             if (this.activeSchool.currentSessionDbId != Number(urlParams.get('session')) && this.checkChangeSession()) {
