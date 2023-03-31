2e63c40587b610bab62c9f50ae32cd6623ca624c
Dev Aryan Dogra <devdogra1@gmail.com>
2023-03-31 22:12:09 +0530

    Create book name search bar and select columns dropdown

--- frontend/src/app/modules/library/pages/view-all/view-all.component.ts
+++ frontend/src/app/modules/library/pages/view-all/view-all.component.ts
@@ -2,6 +2,25 @@ import { Component, OnInit } from '@angular/core';
 
 import { DataStorage } from "@classes/data-storage";
 
+
+class ColumnFilter {
+    showSerialNumber = true;
+    showName = true;
+    showAuthor = true;
+    showPublisher = true;
+    showDateOfPurchase = true;
+    showBookNumber = true;
+    showEdition = false;
+    showNumberOfPages = false;
+    showPrintedCost = false;
+    showCoverType = false;
+    showLocation = false;
+    showBookType = false;
+    showFrontImage = false;
+    showBackImage = false;
+}
+
+
 @Component({
     selector: 'view-all',
     templateUrl: './view-all.component.html',
@@ -13,11 +32,26 @@ export class ViewAllComponent implements OnInit {
 
     user: any;
 
+    isLoading = false;
+
+    // Col Filter for books
+    columnFilter: ColumnFilter;
+    // Col Filter for documents
+    documentFilter: ColumnFilter
+
+    bookDocumentSelectList = ['Book', 'Documents'];
+    currentBookDocumentFilter;
+
     constructor () { }
 
     ngOnInit(): void {
-
         this.user = DataStorage.getInstance().getUser();
-
+        this.columnFilter = new ColumnFilter();
+        this.documentFilter = new ColumnFilter();
+        this.currentBookDocumentFilter = this.bookDocumentSelectList[0];
     }
+
+    selectAllColumns(): void {}
+    unSelectAllColumns(): void {}
+
 }
