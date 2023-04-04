2eb9ee6744bd7d78a31d7dbc2d1f427a5fe5fff6
Gourav Bidhuri <gourav@korangle.com>
2023-04-04 23:09:17 +0530

    removed unnecessary changes.

--- frontend/src/app/modules/leaves/leaves.routing.ts
+++ frontend/src/app/modules/leaves/leaves.routing.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { LeavesComponent } from './leaves.component';
+import { ManagePlanComponent } from './pages/manage_plan/manage_plan.component';
 
 const routes: Routes = [
     {
@@ -11,7 +12,7 @@ const routes: Routes = [
     },
     {
         path: 'manage_plan',
-        loadChildren: 'app/modules/leaves/pages/manage_plan/manage_plan.module#ManagePlanModule',
+        component: ManagePlanComponent,
         data: { moduleName: 'leaves' },
     },
     {
