132f39be2127a0d8c50609a1df2bf6287acd79b6
Gourav Bidhuri <gourav@korangle.com>
2023-04-04 23:57:34 +0530

    removed manage plan page for now.

--- frontend/src/app/modules/leaves/leaves.routing.ts
+++ frontend/src/app/modules/leaves/leaves.routing.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { LeavesComponent } from './leaves.component';
-import { ManagePlanComponent } from './pages/manage_plan/manage_plan.component';
 
 const routes: Routes = [
     {
         path: 'manage_type',
         loadChildren: 'app/modules/leaves/pages/manage_type/manage_type.module#ManageTypeModule',
-        data: { moduleName: 'leaves' },
-    },
-    {
-        path: 'manage_plan',
-        component: ManagePlanComponent,
         data: { moduleName: 'leaves' },
     },
     {
