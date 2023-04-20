54cc6b77482af52db93d42fc22622f8a87eefcfc
Gourav Bidhuri <gourav@korangle.com>
2023-04-20 18:58:30 +0530

    updated service adaptor.

--- frontend/src/app/modules/leaves/pages/manage_plan/manage_plan.service.adapter.ts
+++ frontend/src/app/modules/leaves/pages/manage_plan/manage_plan.service.adapter.ts
@@ -22,62 +22,34 @@ export default class ManagePlanServiceAdapter {
     }
     // ends :- Initialize Data
 
-    // starts :- Data Change Handler (handle Data changes by making requests to backend)
-    async handleDataChange(Operation: Operation, variableName: string): Promise<any> {
-        this.vm.isLoading = true;
-        if (Operation.operation === "insert" || Operation.operation === "update") {
-            // starts :- check for existing entry in the database by using Operation.check to compare 2 objects
-            this.vm[variableName] = await this.vm.genericService.getObjectList(Operation.database, {});
-            let sameVariableNameMap: { [id: string]: boolean } = {};
-            const similarObjectList = this.vm[variableName].filter((object) => {
-                const variableList = Operation.check(object, Operation.data[0]);
-                variableList.map((variableName) => {
-                    sameVariableNameMap[variableName] = true;
-                });
-                return variableList.length !== 0;
+    // starts :- alert for duplicate entries
+    async compareAndAlert(variableName, database, check, data): Promise<boolean> {
+        // starts :- check for existing entry in the database by using Operation.check to compare 2 objects
+        this.vm[variableName] = await this.vm.genericService.getObjectList(database, {});
+        let sameVariableNameMap: { [id: string]: boolean } = {};
+        const similarObjectList = this.vm[variableName].filter((object) => {
+            const variableList = check(object, data);
+            variableList.map((variableName) => {
+                sameVariableNameMap[variableName] = true;
             });
-            // ends :- check for duplicate entry
-            // starts :- Alert if data is duplicate using list of variables provided in sameVariableNameList
-            if (similarObjectList.length !== 0) {
-                let alertString: string = "";
-                const sameVariableNameList = Object.keys(sameVariableNameMap);
-                sameVariableNameList.map((variableName, index) => {
-                    alertString += `${index === sameVariableNameList.length - 1 && sameVariableNameList.length !== 1 ? "and " : ""}${variableName}${
-                        index === sameVariableNameList.length - 1 ? "" : ", "
-                    }`;
-                });
-                alert(`${alertString} already exists!`);
-                this.vm.isLoading = false;
-                return null;
-            }
-            // ends :- alert for duplicate entry (returns null indicating error else moves ahead.)
-        }
-        let response = null;
-        if (!Operation.operation.endsWith("Batch")) {
-            Operation.data.forEach((data) => {
-                data.parentSchool = this.vm.user.activeSchool.dbId;
+            return variableList.length !== 0;
+        });
+        // ends :- check for duplicate entry
+        // starts :- Alert if data is duplicate using list of variables provided in sameVariableNameList
+        if (similarObjectList.length !== 0) {
+            let alertString: string = "";
+            const sameVariableNameList = Object.keys(sameVariableNameMap);
+            sameVariableNameList.map((variableName, index) => {
+                alertString += `${index === sameVariableNameList.length - 1 && sameVariableNameList.length !== 1 ? "and " : ""}${variableName}${
+                    index === sameVariableNameList.length - 1 ? "" : ", "
+                }`;
             });
+            alert(`${alertString} already exists!`);
+            return true;
         }
-        if (Operation.operation === "insert") {
-            response = await this.vm.genericService.createObject(Operation.database, Operation.data[0]);
-        } else if (Operation.operation === "update") {
-            response = await this.vm.genericService.partiallyUpdateObject(Operation.database, Operation.data[0]);
-        } else if (Operation.operation === "delete") {
-            response = await this.vm.genericService.deleteObjectList(Operation.database, { filter: Operation.data[0] });
-        } else if (Operation.operation === "insertBatch") {
-            response = await this.vm.genericService.createObjectList(Operation.database, Operation.data);
-        } else if (Operation.operation === "updateBatch") {
-            response = await this.vm.genericService.partiallyUpdateObjectList(Operation.database, Operation.data);
-        } else if (Operation.operation === "deleteBatch") {
-            response = await this.vm.genericService.deleteObjectList(Operation.database, { filter: { __or__: Operation.data } });
-        }
-        if (response !== null) {
-            await this.initializeData(Operation.database, variableName);
-        }
-        this.vm.isLoading = false;
-        return response;
+        return false;
     }
-    // ends :- Data Change Handler
+    // ends :- alert for duplicate entries
 
     // starts :- Function to save plan
     async savePlan(data): Promise<any> {
@@ -86,9 +58,8 @@ export default class ManagePlanServiceAdapter {
                 "Please Enter a valid Leave Plan Name. (start with lowercase / uppercase english alphabets and contains only alphabets, numbers, spaces and hyphens.)",
             );
         }
-        let removeLeaveTypeChoiceList: Array<LeavePlanToLeaveType> = [];
-        let addLeaveTypeChoiceList: Array<LeavePlanToLeaveType> = [];
-        let oldLeaveTypeChoiceList: Array<LeavePlanToLeaveType> = [];
+        this.vm.isLoading = true;
+        let removeLeaveTypeChoiceList: Array<LeavePlanToLeaveType> = [], addLeaveTypeChoiceList: Array<LeavePlanToLeaveType> = [], oldLeaveTypeChoiceList: Array<LeavePlanToLeaveType> = [];
         this.vm.leavePlanToLeaveTypeList.map((leavePlanToLeaveTypeItem) => {
             leavePlanToLeaveTypeItem.parentSchoolLeavePlan === this.vm.currentLeavePlan.id ? oldLeaveTypeChoiceList.push(leavePlanToLeaveTypeItem) : null;
         });
@@ -101,43 +72,33 @@ export default class ManagePlanServiceAdapter {
         this.vm.appliedLeaveTypeChoiceList.map((leaveType) => {
             let similarLeaveType = oldLeaveTypeChoiceList.find((oldLeaveTypeChoice) => leaveType.id === oldLeaveTypeChoice.parentSchoolLeaveType);
             !similarLeaveType
-                ? addLeaveTypeChoiceList.push({ id: -1, parentSchoolLeavePlan: this.vm.currentLeavePlan.id, parentSchoolLeaveType: leaveType.id })
-                : null;
+                ? addLeaveTypeChoiceList.push({ id: -1, parentSchoolLeavePlan: this.vm.currentLeavePlan.id, parentSchoolLeaveType: leaveType.id }) : null;
         });
-        await this.handleDataChange({
-                check: (data1, data2) => data2.id != data1.id && data1.leavePlanName.toLowerCase() === data2.leavePlanName.toLowerCase() ? ["Leave Plan Name"] : [],
-                data: [data], database: { leaves_app: "SchoolLeavePlan" }, operation: data.id ? "update" : "insert",
-            }, "leavePlanList",);
+        if(await this.compareAndAlert("leavePlanList", { leaves_app: "SchoolLeavePlan" }, (data1, data2) => data2.id != data1.id && data1.leavePlanName.toLowerCase() === data2.leavePlanName.toLowerCase() ? ["Leave Plan Name"] : [], data)) {
+            this.vm.isLoading = false;
+            return false;
+        }
+        data.parentSchool = this.vm.user.activeSchool.dbId;
+        data.id ? this.vm.genericService.partiallyUpdateObject({ leaves_app: "SchoolLeavePlan" }, data) : await this.vm.genericService.createObject({ leaves_app: "SchoolLeavePlan" }, data);
         removeLeaveTypeChoiceList.length
-            ? await this.handleDataChange({
-                      check: null, data: removeLeaveTypeChoiceList, operation: "deleteBatch",
-                      database: { leaves_app: "SchoolLeavePlanToSchoolLeaveType" },
-                  }, "leavePlanToLeaveTypeList",) : null;
-        addLeaveTypeChoiceList.length ? await this.handleDataChange({
-                      check: null, data: addLeaveTypeChoiceList, operation: "insertBatch",
-                      database: { leaves_app: "SchoolLeavePlanToSchoolLeaveType" },
-                  }, "leavePlanToLeaveTypeList",) : null;
+            ? await this.vm.genericService.deleteObjectList({ leaves_app: "SchoolLeavePlanToSchoolLeaveType" }, { filter: { __or__: removeLeaveTypeChoiceList } }) : null;
+        addLeaveTypeChoiceList.length ? await this.vm.genericService.createObjectList({ leaves_app: "SchoolLeavePlanToSchoolLeaveType" }, addLeaveTypeChoiceList) : null;
         this.vm.resetComponent();
+        this.vm.ngOnInit();
     }
     // ends :- function to save plan
 
     // starts :- Function to delete currently selected plan
     async handleDelete(): Promise<any> {
         let oldLeaveTypeChoiceList: Array<LeavePlanToLeaveType> = [];
+        this.vm.isLoading = true;
         this.vm.leavePlanToLeaveTypeList.map((leavePlanToLeaveTypeItem) => {
             leavePlanToLeaveTypeItem.parentSchoolLeavePlan === this.vm.currentLeavePlan.id ? oldLeaveTypeChoiceList.push(leavePlanToLeaveTypeItem) : null;
         });
-        if (oldLeaveTypeChoiceList.length) {
-            await this.handleDataChange({
-                    check: null, data: oldLeaveTypeChoiceList, operation: "deleteBatch",
-                    database: { leaves_app: "SchoolLeavePlanToSchoolLeaveType" },
-                }, "leavePlanToLeaveTypeList");
-        }
-        await this.handleDataChange({
-                check: null, data: [this.vm.currentLeavePlan], operation: "delete",
-                database: { leaves_app: "SchoolLeavePlan" },
-            }, "leavePlanList");
+        oldLeaveTypeChoiceList.length ? await this.vm.genericService.deleteObjectList({ leaves_app: "SchoolLeavePlanToSchoolLeaveType" }, { filter: { __or__: oldLeaveTypeChoiceList } }) : null;
+        await this.vm.genericService.deleteObjectList({ leaves_app: "SchoolLeavePlan" }, { filter: this.vm.currentLeavePlan });
         this.vm.resetComponent();
+        this.vm.ngOnInit();
     }
     // ends :- Function to delete currently selected plan
 }
