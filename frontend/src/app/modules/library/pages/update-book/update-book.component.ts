cf92520534e93e9c1f751f007f833116ec103a75
Dev Aryan Dogra <devdogra1@gmail.com>
2023-05-22 15:31:04 +0530

    Add search book bar in update book page

--- frontend/src/app/modules/library/pages/update-book/update-book.component.ts
+++ frontend/src/app/modules/library/pages/update-book/update-book.component.ts
@@ -1,23 +1,71 @@
-import { Component, OnInit } from '@angular/core';
-
 import { DataStorage } from "@classes/data-storage";
-
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import { startWith } from 'rxjs/operators/startWith';
+import { map } from 'rxjs/operators/map';
+import { Book } from '@modules/library/models/book';
+import { UpdateBookServiceAdapter } from "./update-book.service.adapter";
+import { GenericService } from '@services/generic/generic-service';
 
 @Component({
-    selector: 'app-update-book',
+    selector: 'update-book',
     templateUrl: './update-book.component.html',
-    styleUrls: ['./update-book.component.css']
+    styleUrls: ['./update-book.component.css'],
+    providers: [GenericService]
 })
 export class UpdateBookComponent implements OnInit {
 
     user: any;
 
-    constructor() { }
+    bookList = [];
+    filteredBookList: Observable<Book[]>;
+
+    searchBookFormControl: FormControl = new FormControl('');
+    isLoading = false;
+
+    serviceAdapter: UpdateBookServiceAdapter;
+
+    constructor(public genericService: GenericService) { }
 
     ngOnInit() {
-
         this.user = DataStorage.getInstance().getUser();
+        this.serviceAdapter = new UpdateBookServiceAdapter();
+        this.serviceAdapter.initializeAdapter(this);
+        this.serviceAdapter.initializeData();
 
+        this.filteredBookList = this.searchBookFormControl.valueChanges
+        .pipe(
+            startWith(''),
+            map((value) => (typeof value === 'string' ? value : (value as any).name)),
+            map(searchedBookName => this.getFilteredBookList(searchedBookName))
+        );
     }
 
+    getFilteredBookList(searchedBookName: string): Book[] {
+        const list = this.bookList.filter(book => {
+        return this.namesMatch(book.name, searchedBookName);
+        });
+        return list;
+    }
+
+    setBookListLoading(value: boolean): void {
+        this.isLoading = value;
+    }
+
+    namesMatch(bookName: string, searchedBookName: string): boolean {
+        bookName = bookName.toLowerCase();
+        searchedBookName = searchedBookName.toLowerCase();
+        return bookName.indexOf(searchedBookName) === 0;
+    }
+
+    handleBookSelection(event: any) {
+        const selectedBook: Book = event.option.value;
+    }
+
+    displayFn(book) {
+        return book ? book.name : undefined;
+    }
 }
+
+
