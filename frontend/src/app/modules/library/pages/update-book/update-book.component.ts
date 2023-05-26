bc627da403cd3076bc77b3f3d70bbffdfa55c5b7
BuddyLongLegs <anurag10jain@gmail.com>
2023-05-26 11:46:35 +0530

    Update book done

--- frontend/src/app/modules/library/pages/update-book/update-book.component.ts
+++ frontend/src/app/modules/library/pages/update-book/update-book.component.ts
@@ -26,6 +26,12 @@ export class UpdateBookComponent implements OnInit {
 
     serviceAdapter: UpdateBookServiceAdapter;
 
+    selectedBook: Book | null = null;
+    updatedBook: Book | null = null;
+
+    backImage: any;
+    frontImage: any;
+
     constructor(public genericService: GenericService) { }
 
     ngOnInit() {
@@ -42,9 +48,127 @@ export class UpdateBookComponent implements OnInit {
         );
     }
 
+    isMobile(): boolean {
+        if (window.innerWidth > 991) {
+            return false;
+        }
+        return true;
+    }
+
+    async onImageSelect(evt: any, side: any) {
+        let image = evt.target.files[0];
+
+        if (image.type !== 'image/jpeg' && image.type !== 'image/png') {
+            alert('Image type should be either jpg, jpeg, or png');
+            return;
+        }
+
+        if (image.size === 0) {
+            alert('Image is blank. Please upload/select another image');
+            return;
+        }
+
+        image = await this.cropImage(image, [1, 1]);
+
+        while (image.size > 512000) {
+            image = await this.resizeImage(image);
+        }
+
+        if (image.size > 512000) {
+            alert('Image size should be less than 512kb');
+            return;
+        }
+
+
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            if (side === 'back') {
+                this.backImage = reader.result;
+            } else if (side === 'front') {
+                this.frontImage = reader.result;
+            }
+        };
+        reader.readAsDataURL(image);
+    }
+
+    cropImage(file: File, aspectRatio: any): Promise<Blob> {
+        return new Promise((resolve, reject) => {
+            let image = new Image();
+            image.src = URL.createObjectURL(file);
+            image.onload = () => {
+                let dx = 0;
+                let dy = 0;
+                let dw = image.width;
+                let dh = image.height;
+
+                let sx = 0;
+                let sy = 0;
+                let sw = dw;
+                let sh = dh;
+
+                if (sw > (aspectRatio[1] * sh) / aspectRatio[0]) {
+                    sx = (sw - (aspectRatio[1] * sh) / aspectRatio[0]) / 2;
+                    sw = (aspectRatio[1] * sh) / aspectRatio[0];
+                    dw = sw;
+                } else if (sh > (aspectRatio[0] * sw) / aspectRatio[1]) {
+                    sy = (sh - (aspectRatio[0] * sw) / aspectRatio[1]) / 2;
+                    sh = (aspectRatio[0] * sw) / aspectRatio[1];
+                    dh = sh;
+                }
+
+                let canvas = document.createElement('canvas');
+                canvas.width = dw;
+                canvas.height = dh;
+
+                let context = canvas.getContext('2d');
+
+                context.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+
+                canvas.toBlob(resolve, file.type);
+            };
+            image.onerror = reject;
+        });
+    }
+
+    resizeImage(file: File): Promise<Blob> {
+        return new Promise((resolve, reject) => {
+            let image = new Image();
+            image.src = URL.createObjectURL(file);
+            image.onload = () => {
+                let width = image.width;
+                let height = image.height;
+
+                let maxWidth = image.width / 2;
+                let maxHeight = image.height / 2;
+
+                let newWidth: any;
+                let newHeight: any;
+
+                if (width > height) {
+                    newHeight = height * (maxWidth / width);
+                    newWidth = maxWidth;
+                } else {
+                    newWidth = width * (maxHeight / height);
+                    newHeight = maxHeight;
+                }
+
+                let canvas = document.createElement('canvas');
+                canvas.width = newWidth;
+                canvas.height = newHeight;
+
+                let context = canvas.getContext('2d');
+
+                context.drawImage(image, 0, 0, newWidth, newHeight);
+
+                canvas.toBlob(resolve, file.type);
+            };
+            image.onerror = reject;
+        });
+    }
+
     getFilteredBookList(searchedBookName: string): Book[] {
         const list = this.bookList.filter(book => {
-        return this.namesMatch(book.name, searchedBookName);
+            return this.namesMatch(book.name, searchedBookName);
         });
         return list;
     }
@@ -63,42 +187,61 @@ export class UpdateBookComponent implements OnInit {
     /* --------------- Matched book name highlighting logic starts -------------- */
     leftText(name: string): string {
         let text = this.searchBookFormControl.value;
-        let ind = name.toLowerCase().indexOf(text.toLowerCase());
-        if (ind == -1)
-            return name;
-        if (ind > 0)
-            return name.substring(0, ind);
+        if (typeof text === typeof "abc") {   
+            let ind = name.toLowerCase().indexOf(text.toLowerCase());
+            if (ind == -1)
+                return name;
+            if (ind > 0)
+                return name.substring(0, ind);
+        }
         return '';
     }
 
     rightText(name: string): string {
         let text = this.searchBookFormControl.value;
-        let ind = name.toLowerCase().indexOf(text.toLowerCase());
-        if (ind == -1)
-            return '';
-        let right = ind + text.length;
-        if (right < name.length)
-            return name.substring(right, name.length);
+        if (typeof text === typeof "abc") {
+            let ind = name.toLowerCase().indexOf(text.toLowerCase());
+            if (ind == -1)
+                return '';
+            let right = ind + text.length;
+            if (right < name.length)
+                return name.substring(right, name.length);
+        }
         return '';
     }
 
     highlightText(name: string): string {
         let text = this.searchBookFormControl.value;
-        let ind = name.toLowerCase().indexOf(text.toLowerCase());
-        if (ind != -1)
-            return name.substring(ind, ind + text.length);
+        if (typeof text === typeof "abc") {
+            let ind = name.toLowerCase().indexOf(text.toLowerCase());
+            if (ind != -1)
+                return name.substring(ind, ind + text.length);
+        }
         return '';
     }
     /* ---------------- Matched book name highlighting logic ends --------------- */
 
 
     handleBookSelection(event: any) {
-        const selectedBook: Book = event.option.value;
+        this.selectedBook = event.option.value;
+        this.updatedBook = event.option.value;
+        this.frontImage = this.updatedBook.frontImage;
+        this.backImage = this.updatedBook.backImage;
     }
 
     displayFn(book) {
         return book ? book.name : undefined;
     }
+
+    async updateBook() {
+        if (this.updatedBook) {
+            this.updatedBook.frontImage = this.frontImage;
+            this.updatedBook.backImage = this.backImage;
+            await this.serviceAdapter.updateBook();
+            this.updatedBook = null;
+            this.frontImage = null;
+            this.backImage = null;
+            this.searchBookFormControl.setValue('');
+        }
+    }
 }
-
-
--- frontend/src/app/modules/library/pages/update-book/update-book.service.adapter.ts
+++ frontend/src/app/modules/library/pages/update-book/update-book.service.adapter.ts
@@ -1,3 +1,4 @@
+import { CommonFunctions } from '@modules/common/common-functions';
 import { UpdateBookComponent } from "./update-book.component";
 
 export class UpdateBookServiceAdapter {
@@ -21,6 +22,86 @@ export class UpdateBookServiceAdapter {
 
         this.vm.bookList = fetchedBookList;
         this.vm.setBookListLoading(false);
+    }
+
+    dataURLtoFile(dataurl, filename) {
+        try {
+            const arr = dataurl.split(',');
+            const mime = arr[0].match(/:(.*?);/)[1];
+            const bstr = atob(arr[1]);
+            let n = bstr.length;
+            const u8arr = new Uint8Array(n);
+
+            while (n--) {
+                u8arr[n] = bstr.charCodeAt(n);
+            }
+
+            return new File([u8arr], filename, { type: mime });
+        } catch (e) {
+            return null;
+        }
+    }
+
+    async updateBook() {
+        this.vm.isLoading = true;
+
+        // Check if a different book already exists in this school with the same bookNumber
+        const fetchedBook = await this.vm.genericService.getObject({ library_app: "Book" }, {
+            filter: {
+                bookNumber: this.vm.updatedBook.bookNumber,
+                parentSchool_id: this.vm.user.activeSchool.dbId
+            }
+        })
+
+        if (fetchedBook && fetchedBook.id !== this.vm.selectedBook.id) {
+            alert("Another book with the same Book No. already exists. Please choose a different Book No.");
+            return;
+        }
+
+        const book_form_data = new FormData();
+        const data = {...this.vm.updatedBook};
+
+
+        Object.keys(data).forEach((key) => {
+            if (key === 'frontImage') {
+                if (this.vm.frontImage) {
+                    const image = this.dataURLtoFile(this.vm.frontImage, 'frontImage.jpg');
+                    console.log({frontimg: image});
+                    if (image) book_form_data.append(key, image);
+                }
+            } else if (key === 'backImage') {
+                if (this.vm.backImage) {
+                    const image = this.dataURLtoFile(this.vm.backImage, 'backImage.jpg');
+                    console.log({backimg: image});
+                    if (image) book_form_data.append(key, image);
+                }
+            }
+            else {
+                if (data[key] !== null) {
+                    book_form_data.append(key, data[key]);
+                }
+            }
+        });
+
+        let parentEmployee = this.vm.user.activeSchool.employeeId;
+        let moduleName = this.vm.user.section.title;
+        let taskName = this.vm.user.section.subTitle;
+        let moduleList = this.vm.user.activeSchool.moduleList;
+        let actionString = " updated book details of" + this.vm.updatedBook.name;
+
+        try {
+            console.log({book_form_data});
+            await this.vm.genericService.updateObject({library_app: "Book"}, book_form_data);
+            alert('Book updated succesfully');
+
+            await CommonFunctions.createRecord(parentEmployee, moduleName, taskName, moduleList, actionString);
+
+            this.vm.isLoading = false;
+        }
+        catch {
+            this.vm.isLoading = false;
+        }
+
     }
 
 }
