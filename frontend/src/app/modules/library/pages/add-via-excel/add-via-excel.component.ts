eff0f8011bcb4228e790e4db42a341add6d9b1e9
BuddyLongLegs <anurag10jain@gmail.com>
2023-06-23 10:23:23 +0530

    UI structure done

--- frontend/src/app/modules/library/pages/add-via-excel/add-via-excel.component.ts
+++ frontend/src/app/modules/library/pages/add-via-excel/add-via-excel.component.ts
@@ -1,23 +1,103 @@
 import { Component, OnInit } from '@angular/core';
+import xlsx = require('xlsx');
 
 import { DataStorage } from "@classes/data-storage";
+import { GenericService } from '@services/generic/generic-service';
+import { AddViaExcelServiceAdapter } from './add-via-excel.service.adapter';
+
+interface Parameter {
+    name: string;
+    field: string;
+    filter: (any) => boolean;
+    required?: boolean;
+}
 
 @Component({
     selector: 'add-via-excel',
     templateUrl: './add-via-excel.component.html',
     styleUrls: ['./add-via-excel.component.css'],
-    providers: [ ],
+    providers: [GenericService],
 })
 
 export class AddViaExcelComponent implements OnInit {
 
     user: any;
+    isLoading: boolean = false;
 
-    constructor () { }
+    serviceAdapter: AddViaExcelServiceAdapter;
+
+    usedBookNumbers: Number[] = [];
+
+    parameters: Parameter[] = [
+        {
+            name: "Book Number",
+            field: "bookNumber",
+            filter: (num) => {
+                if (!num && num !== 0) return false;
+                if (this.usedBookNumbers.includes(num)) return false;
+                return false;
+            },
+            required: true
+        },
+        {
+            name: "Book Name",
+            field: "name",
+            filter: () => true,
+            required: true
+        },
+        {
+            name: "Author",
+            field: "author",
+            filter: () => true
+        },
+        {
+            name: "Publisher",
+            field: "publisher",
+            filter: () => true
+        },
+        {
+            name: "Date of Purchase",
+            field: "dateOfPurchase",
+            filter: () => true
+        },
+        {
+            name: "Edition",
+            field: "edition",
+            filter: () => true
+        },
+        {
+            name: "Number of Pages",
+            field: "numberOfPages",
+            filter: () => true
+        },
+        {
+            name: "Printed Cost",
+            field: "printedCost",
+            filter: () => true
+        },
+        {
+            name: "Cover Type",
+            field: "coverType",
+            filter: () => true
+        },
+        {
+            name: "Type of Book",
+            field: "typeOfBook",
+            filter: () => true
+        },
+        {
+            name: "Location",
+            field: "location",
+            filter: () => true
+        }
+    ];
+
+    constructor (public genericService: GenericService) { }
 
     ngOnInit(): void {
-
         this.user = DataStorage.getInstance().getUser();
-
+        this.serviceAdapter = new AddViaExcelServiceAdapter();
+        this.serviceAdapter.initializeAdapter(this);
+        this.serviceAdapter.initializeData();
     }
 }
---
+++ frontend/src/app/modules/library/pages/add-via-excel/add-via-excel.service.adapter.ts
@@ -0,0 +1,25 @@
+import { CommonFunctions } from '@modules/common/common-functions';
+import { AddViaExcelComponent } from "./add-via-excel.component";
+
+export class AddViaExcelServiceAdapter {
+    vm: AddViaExcelComponent;
+
+    constructor() { }
+    
+    initializeAdapter(vm: AddViaExcelComponent): void {
+        this.vm = vm;
+    }
+
+    async initializeData(): Promise<void> {
+        this.vm.isLoading = true;
+        const query = {
+            filter: {
+                parentSchool_id: this.vm.user.activeSchool.dbId
+            },
+            fields_list: ["bookNumber"]
+        };
+        const fetchUsedBookNumbers = await this.vm.genericService.getObjectList({ library_app: "Book" }, query);
+        this.vm.usedBookNumbers = fetchUsedBookNumbers.map((book) => book.bookNumber);
+        this.vm.isLoading = false;
+    }
+}
