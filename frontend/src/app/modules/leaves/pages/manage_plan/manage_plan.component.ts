8f75afdd2f9ae72996f9dfd770fe919e2843771d
Gourav Bidhuri <gourav@korangle.com>
2023-06-24 13:31:52 +0530

    remove unnecessary changes.

--- frontend/src/app/modules/leaves/pages/manage_plan/manage_plan.component.ts
+++ frontend/src/app/modules/leaves/pages/manage_plan/manage_plan.component.ts
@@ -157,12 +157,11 @@ export class ManagePlanComponent implements OnInit {
         this.appliedEmployeeChoiceList.forEach((employee) => {
             employee.name.toLowerCase().startsWith(this.filter.toLowerCase()) ? null : temporaryEmployeeChoiceList.push(employee);
         });
-        this.currentEmployeeChoiceList.forEach((employee) => {
-            temporaryEmployeeChoiceList.push(employee);
-        });
+        this.currentEmployeeChoiceList.forEach((employee) => temporaryEmployeeChoiceList.push(employee));
         temporaryEmployeeChoiceList.sort((employee1, employee2) => employee1.name.localeCompare(employee2.name));
         this.filter = "";
         this.appliedEmployeeChoiceList = this.currentEmployeeChoiceList = temporaryEmployeeChoiceList;
         this.updateEmployeeChoiceList();
     }
+    // ends :- add employee to list of selected employees.
 }
