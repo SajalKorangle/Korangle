d9bf802e70232c0425caf6a45b8f800631b82c28
BuddyLongLegs <anurag10jain@gmail.com>
2023-06-26 17:11:51 +0530

    Added download template

--- frontend/src/app/modules/library/pages/add-via-excel/add-via-excel.component.ts
+++ frontend/src/app/modules/library/pages/add-via-excel/add-via-excel.component.ts
@@ -4,6 +4,7 @@ import xlsx = require('xlsx');
 import { DataStorage } from "@classes/data-storage";
 import { GenericService } from '@services/generic/generic-service';
 import { AddViaExcelServiceAdapter } from './add-via-excel.service.adapter';
+import { Book } from '@modules/library/models/book';
 
 interface Parameter {
     name: string;
@@ -27,22 +28,37 @@ export class AddViaExcelComponent implements OnInit {
     serviceAdapter: AddViaExcelServiceAdapter;
 
     usedBookNumbers: Number[] = [];
+    bookList: Array<Array<any>> = [];
+    isUploadable: boolean = false;
+    mappedParameter: Parameter[] = [];
+    hasFileSelected: boolean = false;
+
+    reader: FileReader = new FileReader();
 
     parameters: Parameter[] = [
+        {
+            name: "None",
+            field: "none",
+            filter: () => true
+        },
         {
             name: "Book Number",
             field: "bookNumber",
             filter: (num) => {
-                if (!num && num !== 0) return false;
+                // The book number should be a number and should not been used before
+                if (!num) return false;
+                if (isNaN(num) || isNaN(parseFloat(num))) return false;
+                if(num < 0) return false;
                 if (this.usedBookNumbers.includes(num)) return false;
-                return false;
+                if (this.bookList.filter((book) => book[this.mappedParameter.indexOf(this.parameters[1])] === num).length > 1) return false;
+                return true;
             },
             required: true
         },
         {
-            name: "Book Name",
+            name: "Name",
             field: "name",
-            filter: () => true,
+            filter: (name) => !!name,
             required: true
         },
         {
@@ -58,7 +74,12 @@ export class AddViaExcelComponent implements OnInit {
         {
             name: "Date of Purchase",
             field: "dateOfPurchase",
-            filter: () => true
+            filter: (date) => {
+                // if the date is present, it should be a valid date
+                if (!date) return true;
+                if (isNaN(Date.parse(date))) return false;
+                return true;
+            }
         },
         {
             name: "Edition",
@@ -68,12 +89,22 @@ export class AddViaExcelComponent implements OnInit {
         {
             name: "Number of Pages",
             field: "numberOfPages",
-            filter: () => true
+            filter: (numPages) => {
+                // if the number of pages is present, it should be a number
+                if (!numPages) return true;
+                if (isNaN(numPages) || isNaN(parseFloat(numPages))) return false;
+                return true;
+            }
         },
         {
             name: "Printed Cost",
             field: "printedCost",
-            filter: () => true
+            filter: (cost) => {
+                // if the cost is present, it should be a number
+                if (!cost) return true;
+                if (isNaN(cost) || isNaN(parseFloat(cost))) return false;
+                return true;
+            }
         },
         {
             name: "Cover Type",
@@ -99,5 +130,80 @@ export class AddViaExcelComponent implements OnInit {
         this.serviceAdapter = new AddViaExcelServiceAdapter();
         this.serviceAdapter.initializeAdapter(this);
         this.serviceAdapter.initializeData();
+
+        this.reader.onload = (e: any) => {
+            this.isLoading = true;
+            // read workbook
+            const bstr: string = e.target.result;
+            const wb: xlsx.WorkBook = xlsx.read(bstr, { type: 'binary' });
+
+            // grab first sheet
+            const wsname: string = wb.SheetNames[0];
+            const ws: xlsx.WorkSheet = wb.Sheets[wsname];
+
+            // save data
+            this.bookList = xlsx.utils.sheet_to_json(ws, { header: 1 });
+
+            // remove empty rows
+            while(!this.bookList[this.bookList.length - 1].reduce((a, b) => a || b, false)) {
+                this.bookList.pop();
+            }
+            
+            // if blank file is uploaded
+            if (this.bookList.length === 0) {
+                this.hasFileSelected = false;
+                this.isLoading = false;
+                return;
+            }
+            
+            this.matchHeaders();
+
+            this.hasFileSelected = true;
+            this.isLoading = false;
+        }
+    }
+
+    getIndexofParameter(parameterName: string) {
+        return this.mappedParameter.findIndex((parameter) => parameter.name === parameterName);
+    }
+
+    matchHeaders() {
+        let headers = this.bookList[0];
+
+        headers.forEach((header, index) => {
+            let parameter = this.parameters.find((parameter) => parameter.name === header);
+            if (parameter) {
+                this.mappedParameter[index] = parameter;
+            } else {
+                this.mappedParameter[index] = this.parameters[0];
+            }
+        });
+    }
+
+    getAvailableParameters() {
+        return [this.parameters.filter((parameter) => parameter.name==="None" || !this.mappedParameter.includes(parameter))];
+    }
+
+    handleParameterSelection(event, index) {
+        let parameter = this.parameters.find((parameter) => parameter.name === event.value);
+        if(parameter) {
+            this.mappedParameter[index] = parameter;
+        }
+    }
+
+    handleExcelFile(event) {
+        if (event.target.files.length > 0) {
+            const file = event.target.files[0];
+            this.reader.readAsBinaryString(file);
+        }
+    }
+
+    downloadTemplate() {
+        let headerRow = this.parameters.map((parameter) => parameter.name);
+        let data = [headerRow];
+        let ws = xlsx.utils.aoa_to_sheet(data);
+        let wb = xlsx.utils.book_new();
+        xlsx.utils.book_append_sheet(wb, ws, "Sheet1");
+        xlsx.writeFile(wb, "BooksToAdd.xlsx");
     }
 }
