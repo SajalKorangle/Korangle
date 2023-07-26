e8d1dbfb9dda252e5f958ebd8130735b4258b32b
BuddyLongLegs <anurag10jain@gmail.com>
2023-07-26 21:01:16 +0530

    Fixed error

--- frontend/src/app/modules/report-card-3/pages/design-report-card/design-report-card.canvas.adapter.ts
+++ frontend/src/app/modules/report-card-3/pages/design-report-card/design-report-card.canvas.adapter.ts
@@ -84,8 +84,11 @@ export class DesignReportCardCanvasAdapter extends CanvasAdapterHTMLMixin implem
         this.vm.htmlAdapter.isSaving = true;
         this.metaDrawings = false;
         this.canvasSizing(fullHeight, fullWidth, true);
+
+        let orientation = this.canvasHeight > this.canvasWidth ? 'p' : 'l';
+        let format = [Math.max(this.canvasHeight, this.canvasWidth), Math.min(this.canvasHeight, this.canvasWidth)];
         setTimeout(() => {
-            let doc = new jsPDF({ orientation: this.actualresolution.orientation, unit: 'pt', format: [this.canvasHeight, this.canvasWidth] });
+            let doc = new jsPDF({ orientation, unit: 'pt', format });
             let dataurl = this.canvas.toDataURL('image/jpeg', 1.0);
             doc.addImage(dataurl, 'JPEG', 0, 0, this.canvasWidth, this.canvasHeight);
             doc.save(this.vm.currentLayout.name + '.pdf');
