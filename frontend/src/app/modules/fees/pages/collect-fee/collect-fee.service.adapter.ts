a359b06c44339c90a67ebccadc727c8ad15f1d19
harshal <harshalagrawal03@gmail.com>
2023-08-14 19:29:49 +0530

    corrected linting errors

--- frontend/src/app/modules/fees/pages/collect-fee/collect-fee.service.adapter.ts
+++ frontend/src/app/modules/fees/pages/collect-fee/collect-fee.service.adapter.ts
@@ -328,8 +328,8 @@ export class CollectFeeServiceAdapter {
                         tempStudent['dueFeeAmount'] = dueFeeAmount;
                         tempStudent['session'] = session;
                         tempStudent['feeReceipt'] = feeReceipt;
-                        tempStudent['feeReceiptBook'] = 
-                            this.vm.feeReceiptBookList.find(feeReceiptBook => feeReceiptBook.id==feeReceipt.parentFeeReceiptBook);
+                        tempStudent['feeReceiptBook'] =
+                            this.vm.feeReceiptBookList.find(feeReceiptBook => feeReceiptBook.id == feeReceipt.parentFeeReceiptBook);
                         studentList.push(tempStudent);
                     }
 
