9eeebcf9a871ee7b47e1adbb46201323fc0bc2a0
harshal <harshalagrawal03@gmail.com>
2023-09-10 04:55:32 +0530

    changed api version from 8.9 to 9.0

--- backend/helloworld_project/urls.py
+++ backend/helloworld_project/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from generic.generic_views import GenericView, GenericListView
 
-api_version = 'v8.9/'
+api_version = 'v9.0/'
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
