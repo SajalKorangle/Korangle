4b93382ddb9f502743b3fefc273d5c91ab885fe2
harshal <harshalagrawal03@gmail.com>
2023-09-27 16:35:26 +0530

    1. Replaced renderer functions with rxjs variables. 2. Corrected a bug where if a class is selected and it is not present when enquiry is searched with changed dates then enquiry list will not be shown initially.

---
+++ frontend/src/app/modules/enquiry/pages/view-all/view-all.stream.variables.ts
@@ -0,0 +1,61 @@
+import { BehaviorSubject, combineLatest, from, Observable } from "rxjs";
+import { ViewAllComponent } from "./view-all.component";
+import { map } from "rxjs/operators";
+
+export class ViewAllStreamVariables {
+
+    vm: ViewAllComponent;
+
+    enquiryList$ = new BehaviorSubject<any>(null);
+
+    selectedEmployee$ = new BehaviorSubject<any>(null);
+
+    selectedClass$ = new BehaviorSubject<any>(null);
+
+    initialize(vm: ViewAllComponent) {
+
+        // initialize main component instance
+        this.vm = vm;
+
+        // initialize data
+
+        // starts populate selected employee
+        this.enquiryList$.asObservable().subscribe(() => {
+            this.selectedEmployee$.next(null);
+        });
+        // ends populate selected employee
+
+        // starts populate selected class
+        this.enquiryList$.asObservable().subscribe(() => {
+            this.selectedClass$.next(null);
+        });
+        // ends populate selected class
+
+        // starts populate filtered enquiry list
+        combineLatest([
+            this.enquiryList$.asObservable(),
+            this.selectedClass$.asObservable(),
+            this.selectedEmployee$.asObservable()
+        ]).pipe(
+            map(valueList => {
+                console.log(valueList);
+                let tempList = valueList[0];
+                if (valueList[2]) {
+                    tempList = tempList.filter((enqList) => {
+                        return enqList.parentEmployee == valueList[2].id;
+                    });
+                }
+        
+                if (valueList[1]) {
+                    tempList = tempList.filter((enqList) => {
+                        return enqList.parentClass == valueList[1].id;
+                    });
+                }
+                this.vm.filteredEnquiryList = tempList;        
+            })
+        ).subscribe();
+        // ends populate filtered enquiry list
+
+    }
+
+}
